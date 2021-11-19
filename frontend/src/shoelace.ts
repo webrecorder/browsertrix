206d262f19d728010e3577b163bce53cc22b30a4
sua yoo <sua@suayoo.com>
2021-11-19 15:17:07 -0800

    convert login

--- frontend/src/shoelace.ts
+++ frontend/src/shoelace.ts
@@ -4,6 +4,5 @@
  */
 import "@shoelace-style/shoelace/dist/themes/light.css";
 import "@shoelace-style/shoelace/dist/components/button/button";
-import "@shoelace-style/shoelace/dist/components/card/card";
 import "@shoelace-style/shoelace/dist/components/form/form";
 import "@shoelace-style/shoelace/dist/components/input/input";
