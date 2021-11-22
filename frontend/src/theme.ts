8f0e8536858853e477824096db9f352258b709f6
sua yoo <sua@suayoo.com>
2021-11-22 10:21:22 -0800

    add primary theme

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,10 +1,41 @@
 /**
+ * Shoelace CSS theming variables
  * https://github.com/shoelace-style/shoelace/blob/current/src/themes/light.styles.ts
+ *
+ * To make new variables available to Tailwind, update
+ * `theme` in tailwind.cofnig.js
  */
-import { css } from "lit";
+import { css, unsafeCSS } from "lit";
+import Color from "color";
+
+// TODO generate at build time
+const PRIMARY_COLOR = "#4876ff";
+const primaryColor = Color(PRIMARY_COLOR);
 
 const theme = css`
   :root {
+    --primary: ${unsafeCSS(PRIMARY_COLOR)};
+
+    /*
+     * Theme Tokens
+     */
+    /* Primary */
+    --sl-color-primary-50: ${unsafeCSS(primaryColor.lighten(0.6))};
+    --sl-color-primary-100: ${unsafeCSS(primaryColor.lighten(0.5))};
+    --sl-color-primary-200: ${unsafeCSS(primaryColor.lighten(0.4))};
+    --sl-color-primary-300: ${unsafeCSS(primaryColor.lighten(0.3))};
+    --sl-color-primary-400: ${unsafeCSS(primaryColor.lighten(0.2))};
+    --sl-color-primary-500: ${unsafeCSS(primaryColor.lighten(0.1))};
+    --sl-color-primary-600: var(--primary);
+    --sl-color-primary-700: ${unsafeCSS(primaryColor.darken(0.1))};
+    --sl-color-primary-800: ${unsafeCSS(primaryColor.darken(0.2))};
+    --sl-color-primary-900: ${unsafeCSS(primaryColor.darken(0.3))};
+    --sl-color-primary-950: ${unsafeCSS(primaryColor.darken(0.4))};
+
+    /*
+     * Forms
+     */
+
     /* Buttons */
     --sl-button-font-size-small: var(--sl-font-size-small);
     --sl-button-font-size-medium: var(--sl-font-size-medium);
