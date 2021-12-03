1d311ab220a76868ee47f13ba414fbb27266537b
sua yoo <sua@suayoo.com>
2021-12-03 11:24:14 -0800

    opt for default optimization

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -5,18 +5,4 @@ const common = require("./webpack.config.js");
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
-
-  optimization: {
-    moduleIds: "deterministic",
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendor",
-          chunks: "all",
-        },
-      },
-    },
-  },
 });
