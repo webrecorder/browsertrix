e3c0f7411e8d6aa0be9fd2c2dbf0409d68ac4345
sua yoo <sua@suayoo.com>
2022-01-27 17:06:52 -0800

    update layout

--- frontend/src/pages/archive/crawl-detail.ts
+++ frontend/src/pages/archive/crawl-detail.ts
@@ -48,6 +48,8 @@ export class CrawlDetail extends LiteElement {
       </div>`;
     }
 
+    const isRunning = this.crawl.state === "running";
+
     return html`
       <header class="px-4 py-3 border-t border-b mb-4 text-sm">
         <dl class="grid grid-cols-2 gap-5">
@@ -73,61 +75,84 @@ export class CrawlDetail extends LiteElement {
         </dl>
       </header>
 
-      <main class="grid gap-4">
-        <div class="border rounded p-4 md:p-8">
-          <div class="aspect-video bg-slate-50 rounded"></div>
-        </div>
+      ${isRunning
+        ? html`
+            <div
+              class="flex items-center justify-between border rounded-lg bg-purple-50 border-purple-300 text-purple-800 p-4 mb-4"
+            >
+              <p class="font-medium">${msg("Crawl is currently running.")}</p>
+              <div>
+                <sl-button size="small" class="mr-2"
+                  >${msg("Cancel Crawl")}</sl-button
+                >
+                <sl-button size="small" type="danger"
+                  >${msg("Stop Crawl")}</sl-button
+                >
+              </div>
+            </div>
+          `
+        : ""}
 
-        <div class="border rounded p-4 md:p-8">
-          <dl class="grid grid-cols-2 gap-5">
-            <div>
-              <dt class="text-sm text-0-600">${msg("Status")}</dt>
-              <dd class="capitalize">${this.crawl.state.replace(/_/g, " ")}</dd>
-            </div>
-            <div>
-              <dt class="text-sm text-0-600">
-                ${this.crawl.finished ? msg("Finished") : msg("Run duration")}
-              </dt>
-              <dd>
-                ${this.crawl.finished
-                  ? html`<sl-format-date
-                      date=${`${this.crawl.finished}Z` /** Z for UTC */}
-                      month="2-digit"
-                      day="2-digit"
-                      year="2-digit"
-                      hour="numeric"
-                      minute="numeric"
-                      time-zone-name="short"
-                    ></sl-format-date>`
-                  : humanizeDuration(
-                      Date.now() - new Date(`${this.crawl.started}Z`).valueOf()
-                    )}
-              </dd>
-            </div>
-            <div>
-              <dt class="text-sm text-0-600">${msg("Reason")}</dt>
-              <dd class="truncate">
-                ${this.crawl.manual
-                  ? msg(html`Manual start by <span>${this.crawl.user}</span>`)
-                  : msg(html`Scheduled run`)}
-              </dd>
-            </div>
-            <div>
-              <dt class="text-sm text-0-600">${msg("Started")}</dt>
-              <dd>
-                <sl-format-date
-                  date=${`${this.crawl.started}Z` /** Z for UTC */}
-                  month="2-digit"
-                  day="2-digit"
-                  year="2-digit"
-                  hour="numeric"
-                  minute="numeric"
-                  time-zone-name="short"
-                ></sl-format-date>
-              </dd>
-            </div>
-          </dl>
-        </div>
+      <main class="grid gap-4">
+        <section class="grid grid-cols-2 md:grid-cols-8 gap-5">
+          <div class="col-span-8 md:col-span-5">
+            <div class="aspect-video bg-slate-50 rounded">[watch/replay]</div>
+          </div>
+
+          <div class="col-span-8 md:col-span-3 border rounded p-4 md:p-8">
+            <dl class="grid gap-5">
+              <div>
+                <dt class="text-sm text-0-600">${msg("Status")}</dt>
+                <dd class="capitalize">
+                  ${this.crawl.state.replace(/_/g, " ")}
+                </dd>
+              </div>
+              <div>
+                <dt class="text-sm text-0-600">
+                  ${this.crawl.finished ? msg("Finished") : msg("Run duration")}
+                </dt>
+                <dd>
+                  ${this.crawl.finished
+                    ? html`<sl-format-date
+                        date=${`${this.crawl.finished}Z` /** Z for UTC */}
+                        month="2-digit"
+                        day="2-digit"
+                        year="2-digit"
+                        hour="numeric"
+                        minute="numeric"
+                        time-zone-name="short"
+                      ></sl-format-date>`
+                    : humanizeDuration(
+                        Date.now() -
+                          new Date(`${this.crawl.started}Z`).valueOf()
+                      )}
+                </dd>
+              </div>
+              <div>
+                <dt class="text-sm text-0-600">${msg("Reason")}</dt>
+                <dd>
+                  ${this.crawl.manual
+                    ? msg(html`Manual start by <span>${this.crawl.user}</span>`)
+                    : msg(html`Scheduled run`)}
+                </dd>
+              </div>
+              <div>
+                <dt class="text-sm text-0-600">${msg("Started")}</dt>
+                <dd>
+                  <sl-format-date
+                    date=${`${this.crawl.started}Z` /** Z for UTC */}
+                    month="2-digit"
+                    day="2-digit"
+                    year="2-digit"
+                    hour="numeric"
+                    minute="numeric"
+                    time-zone-name="short"
+                  ></sl-format-date>
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </section>
       </main>
     `;
   }
@@ -135,6 +160,7 @@ export class CrawlDetail extends LiteElement {
   async getCrawlTemplate(): Promise<Crawl> {
     // Mock to use in dev:
     return import("../../__mocks__/api/archives/[id]/crawls").then(
+      // (module) => module.default.running[0]
       (module) => module.default.finished[0]
     );
 
