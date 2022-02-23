6eea9b1aaa06b473a67880e1d74842c077a040c1
sua yoo <sua@suayoo.com>
2022-02-23 15:42:20 -0800

    render into canvas

--- frontend/src/components/watch-crawl.ts
+++ frontend/src/components/watch-crawl.ts
@@ -1,14 +1,8 @@
 import { LitElement, html } from "lit";
 import { property, state } from "lit/decorators.js";
+import { ref, createRef } from "lit/directives/ref.js";
 import { guard } from "lit/directives/guard.js";
 import { msg, localized } from "@lit/localize";
-
-type Page = {
-  // Page URL
-  url: string;
-  // PNG dataURI
-  data: string;
-};
 
 type Message = {
   id: string; // page ID
@@ -23,6 +17,9 @@ type ScreencastMessage = Message & {
 type CloseMessage = Message & {
   msg: "close";
 };
+
+const SCREEN_WIDTH = 640;
+const SCREEN_HEIGHT = 480;
 
 /**
  * Watch page crawl
@@ -47,12 +44,19 @@ export class WatchCrawl extends LitElement {
   crawlId?: string;
 
   @state()
-  private pages: {
-    [pageId: string]: Page;
-  } = {};
+  private isConnecting: boolean = false;
 
   // Websocket connection
   private ws: WebSocket | null = null;
+
+  private canvasEl: HTMLCanvasElement | null = null;
+
+  // Canvas 2D context used to draw images
+  private canvasContext: CanvasRenderingContext2D | null = null;
+
+  // Page data
+  private pageMap: Map<string, number> = new Map();
+  private pageImages: (HTMLImageElement | null)[] = [null];
 
   connectedCallback() {
     super.connectedCallback();
@@ -80,12 +84,30 @@ export class WatchCrawl extends LitElement {
   }
 
   render() {
-    return html` ${Object.values(this.pages).map(this.renderPage)} `;
+    return html`
+      <div>
+        ${this.isConnecting ? html` <sl-spinner></sl-spinner> ` : ""}
+
+        <canvas ${ref(this.setCanvasRef)} style="outline: 1px solid red">
+        </canvas>
+      </div>
+    `;
+  }
+
+  private setCanvasRef(el: Element | undefined) {
+    if (el) {
+      this.canvasEl = el as HTMLCanvasElement;
+      this.canvasEl.width = SCREEN_WIDTH;
+      this.canvasEl.height = SCREEN_HEIGHT;
+      this.canvasContext = this.canvasEl.getContext("2d");
+    }
   }
 
   private connectWs() {
     console.log("open");
     if (!this.archiveId || !this.crawlId) return;
+
+    this.isConnecting = true;
 
     this.ws = new WebSocket(
       `${window.location.protocol === "https:" ? "wss" : "ws"}:${
@@ -95,14 +117,26 @@ export class WatchCrawl extends LitElement {
       }/watch/ws?auth_bearer=${this.authToken || ""}`
     );
 
-    // this.ws.addEventListener("open", console.debug);
-    // this.ws.addEventListener("close", console.debug);
-    // this.ws.addEventListener("error", console.error);
-    this.ws.addEventListener("message", ({ data }) => this.handleMessage(data));
+    // this.ws.addEventListener("open", () => {
+    //   this.isConnecting = false;
+    // });
+    this.ws.addEventListener("error", () => {
+      this.isConnecting = false;
+    });
+    this.ws.addEventListener("message", ({ data }) => {
+      if (this.isConnecting) {
+        this.isConnecting = false;
+      }
+
+      this.handleMessage(data);
+    });
   }
 
   private disconnectWs() {
+    this.isConnecting = false;
+
     console.log("close");
+
     if (this.ws) {
       this.ws.close();
     }
@@ -113,15 +147,54 @@ export class WatchCrawl extends LitElement {
   private handleMessage(data: string) {
     const message: ScreencastMessage | CloseMessage = JSON.parse(data);
 
-    console.log(message);
-  }
+    const id = message.id;
 
-  private renderPage(page: Page) {
-    return html`
-      <figure
-        class="aspect-4/3 rounded border"
-        aria-label=${msg("Stream of page ${page.url}")}
-      ></figure>
-    `;
+    // TODO tile pages based on screen size
+
+    if (message.msg === "screencast") {
+      let idx = this.pageMap.get(id);
+
+      if (typeof idx === "undefined" || idx === null) {
+        // Find empty image slot
+        idx = this.pageImages.indexOf(null);
+
+        if (idx === -1) {
+          idx = this.pageImages.push(null) - 1;
+        }
+
+        this.pageImages[idx] = new Image();
+        this.pageMap.set(id, idx);
+        this.canvasEl!.height = SCREEN_HEIGHT * this.pageImages.length;
+      }
+
+      const img = this.pageImages[idx]!;
+      img.src = `data:image/png;base64,${message.data}`;
+
+      const x = 0;
+      const y = idx * SCREEN_HEIGHT;
+      console.log(id, message.url, idx, y);
+
+      this.canvasContext?.drawImage(img, x, y, SCREEN_WIDTH, SCREEN_HEIGHT);
+      this.canvasContext?.fillText(message.url, x, y + 10);
+    } else if (message.msg === "close") {
+      const idx = this.pageMap.get(id);
+
+      if (idx && idx > -1) {
+        if (this.pageImages.indexOf(null) > -1) {
+          delete this.pageImages[idx];
+        } else {
+          this.pageImages[idx] = null;
+        }
+
+        // this.canvasContext?.clearRect(
+        //   0,
+        //   idx * SCREEN_HEIGHT,
+        //   SCREEN_WIDTH,
+        //   SCREEN_HEIGHT
+        // );
+        this.pageMap.delete(id);
+        this.canvasEl!.height = SCREEN_HEIGHT * this.pageImages.length;
+      }
+    }
   }
 }
