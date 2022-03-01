51a573ef1f25cf22c29f1e47ceaa781a39b59544
Ilya Kreymer <ikreymer@gmail.com>
2022-02-28 18:09:13 -0800

    backend prod settings: - set WEB_CONCURRENCY env var to configure number of backend api workers for both docker and k8s - set via 'backend_workers' in values.yaml - also add 'rwp_base_url' to values.yaml - update containers to use public webrecorder/browsertrix-backend and webrecorder/browsertrix-frontend containers - make liveness, readiness and startup health checks more tolerant

--- build-backend.sh
+++ build-backend.sh
@@ -1,1 +1,2 @@
-docker buildx build --platform linux/amd64 --push -t registry.digitalocean.com/btrix/webrecorder/browsertrix-api ./backend/
+docker buildx build --platform linux/amd64 --push -t webrecorder/browsertrix-backend ./backend/
+
