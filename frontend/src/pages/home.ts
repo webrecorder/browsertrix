305ff69f518044ea8bbf4ebfdeed71b0a9725a04
sua yoo <sua@suayoo.com>
2022-03-28 10:04:18 -0700

    add separate archive list view

--- frontend/src/pages/home.ts
+++ frontend/src/pages/home.ts
@@ -3,6 +3,7 @@ import { msg, localized } from "@lit/localize";
 
 import type { AuthState } from "../utils/AuthService";
 import type { CurrentUser } from "../types/user";
+import type { ArchiveData } from "../utils/archives";
 import LiteElement, { html } from "../utils/LiteElement";
 
 @localized()
@@ -16,6 +17,13 @@ export class Home extends LiteElement {
   @state()
   private isInviteComplete?: boolean;
 
+  @state()
+  private archiveList?: ArchiveData[];
+
+  async firstUpdated() {
+    this.archiveList = await this.getArchives();
+  }
+
   connectedCallback() {
     if (this.authState) {
       super.connectedCallback();
@@ -25,76 +33,114 @@ export class Home extends LiteElement {
   }
 
   render() {
+    let title: any;
+    let content: any;
+
     if (this.userInfo) {
       if (this.userInfo.isAdmin === true) {
-        return this.renderLoggedInAdmin();
+        title = msg("Welcome");
+        content = this.renderLoggedInAdmin();
       }
 
       if (this.userInfo.isAdmin === false) {
-        return this.renderLoggedInNonAdmin();
+        title = msg("Archives");
+        content = this.renderLoggedInNonAdmin();
       }
     }
 
-    return html`
-      <div class="bg-white" role="presentation">
-        <header
-          class="w-full max-w-screen-lg mx-auto px-3 py-4 box-border md:py-8"
-        >
-          <h1 class="text-2xl font-medium h-8"></h1>
-        </header>
-        <hr />
-      </div>
-    `;
-  }
-
-  private renderLoggedInAdmin() {
     return html`
       <div class="bg-white">
         <header
           class="w-full max-w-screen-lg mx-auto px-3 py-4 box-border md:py-8"
         >
-          <h1 class="text-2xl font-medium">${msg("Welcome")}</h1>
+          <h1 class="text-2xl font-medium">${title}</h1>
         </header>
         <hr />
       </div>
       <main class="w-full max-w-screen-lg mx-auto px-3 py-4 box-border">
-        <div class="border rounded-lg bg-white p-4 md:p-8">
-          <h2 class="text-xl font-medium mb-4">${msg("Invite a User")}</h2>
-
-          ${this.isInviteComplete
-            ? html`
-                <sl-button @click=${() => (this.isInviteComplete = false)}
-                  >${msg("Send another invite")}</sl-button
-                >
-              `
-            : html`
-                <p class="mb-2 text-neutral-600 text-sm">
-                  ${msg("Invite users to start archiving.")}
-                </p>
-                <p class="mb-4 text-neutral-600 text-sm">
-                  ${msg("Each user will manage their own archive.")}
-                </p>
-
-                <btrix-invite-form
-                  .authState=${this.authState}
-                  @success=${() => (this.isInviteComplete = true)}
-                ></btrix-invite-form>
-              `}
-        </div>
+        ${content}
       </main>
-      <btrix-archives
-        .authState="${this.authState}"
-        .userInfo="${this.userInfo}"
-      ></btrix-archives>
+    `;
+  }
+
+  private renderLoggedInAdmin() {
+    if (!this.archiveList) {
+      return;
+    }
+
+    if (this.archiveList.length) {
+      return html`
+        <div class="grid grid-cols-3 gap-8">
+          <div class="col-span-3 md:col-span-2">
+            <section>
+              <h2 class="text-xl font-medium mb-4 mt-3">
+                ${msg("All Archives")}
+              </h2>
+              <btrix-archives-list
+                .userInfo=${this.userInfo}
+                .archiveList=${this.archiveList}
+              ></btrix-archives-list>
+            </section>
+          </div>
+          <div class="col-span-3 md:col-span-1">
+            <section class="border rounded-lg bg-white p-4 md:p-8">
+              <h2 class="text-xl font-medium mb-4">${msg("Invite a User")}</h2>
+              ${this.renderInvite()}
+            </section>
+          </div>
+        </div>
+      `;
+    }
+
+    return html`
+      <section class="border rounded-lg bg-white p-4 md:p-8 mb-5">
+        <p class="text-lg mb-4 text-neutral-600">
+          ${msg("Invite users to start archiving.")}
+        </p>
+
+        ${this.renderInvite()}
+      </section>
     `;
   }
 
   private renderLoggedInNonAdmin() {
+    if (this.archiveList && !this.archiveList.length) {
+      return html`<div class="border rounded-lg bg-white p-4 md:p-8">
+        <p class="text-neutral-400 text-center">
+          ${msg("You don't have any archives.")}
+        </p>
+      </div>`;
+    }
+
     return html`
-      <btrix-archives
-        .authState="${this.authState}"
-        .userInfo="${this.userInfo}"
-      ></btrix-archives>
+      <btrix-archives-list
+        .userInfo=${this.userInfo}
+        .archiveList=${this.archiveList}
+        ?skeleton=${!this.archiveList}
+      ></btrix-archives-list>
     `;
+  }
+
+  private renderInvite() {
+    if (this.isInviteComplete) {
+      return html`
+        <sl-button @click=${() => (this.isInviteComplete = false)}
+          >${msg("Send another invite")}</sl-button
+        >
+      `;
+    }
+
+    return html`
+      <btrix-invite-form
+        .authState=${this.authState}
+        @success=${() => (this.isInviteComplete = true)}
+      ></btrix-invite-form>
+    `;
+  }
+
+  private async getArchives(): Promise<ArchiveData[]> {
+    const data = await this.apiFetch("/archives", this.authState!);
+
+    return data.archives;
   }
 }
