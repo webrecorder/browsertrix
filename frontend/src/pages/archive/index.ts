0f080ffdc1792de2f29b285e69228f0cf30963b6
sua yoo <sua@suayoo.com>
2022-03-28 10:54:31 -0700

    add detail page

--- frontend/src/pages/archive/index.ts
+++ frontend/src/pages/archive/index.ts
@@ -41,6 +41,9 @@ export class Archive extends LiteElement {
   archiveTab: ArchiveTab = defaultTab;
 
   @property({ type: String })
+  browserProfileId?: string;
+
+  @property({ type: String })
   crawlId?: string;
 
   @property({ type: String })
@@ -259,28 +262,55 @@ export class Archive extends LiteElement {
   private renderBrowserProfiles() {
     const mock = [
       {
+        id: "1",
         name: "Twitter Example",
-        username: "user@example.com",
       },
       {
+        id: "2",
         name: "Twitter Webrecorder",
-        username: "dev@webrecorder.com",
       },
     ];
+
+    if (this.browserProfileId) {
+      const profile = mock.find(({ id }) => id === this.browserProfileId)!;
+
+      return html`
+        <div class="mb-7">
+          <a
+            class="text-neutral-500 hover:text-neutral-600 text-sm font-medium"
+            href=${`/archives/${this.archiveId}/browser-profiles`}
+            @click=${this.navLink}
+          >
+            <sl-icon
+              name="arrow-left"
+              class="inline-block align-middle"
+            ></sl-icon>
+            <span class="inline-block align-middle"
+              >${msg("Back to Browser Profiles")}</span
+            >
+          </a>
+        </div>
+
+        <header>
+          <h2 class="text-2xl font-medium mb-3 md:h-8">${profile.name}</h2>
+        </header>
+      `;
+    }
 
     return html`
       <ul class="border rounded">
         ${mock.map(
           (data) => html`
             <li
-              class="grid grid-cols-12 gap-2 p-4 leading-none hover:bg-zinc-50 hover:text-primary border-t first:border-t-0 transition-colors"
+              class="p-4 leading-none hover:bg-zinc-50 hover:text-primary border-t first:border-t-0 transition-colors"
               role="button"
-              @click=${console.log}
+              @click=${() =>
+                this.navTo(
+                  `/archives/${this.archiveId}/browser-profiles/profile/${data.id}`
+                )}
               title=${data.name}
             >
-              <div class="col-span-12 md:col-span-5">
-                <div class="font-medium mb-1">${data.name}</div>
-              </div>
+              <div class="font-medium">${data.name}</div>
             </li>
           `
         )}
