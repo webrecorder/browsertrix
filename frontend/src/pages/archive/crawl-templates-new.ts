e44855e657b55aa868b78eb62f3fd09bb8f13941
sua yoo <sua@suayoo.com>
2022-03-30 18:42:01 -0700

    switch to new component for new templates

--- frontend/src/pages/archive/crawl-templates-new.ts
+++ frontend/src/pages/archive/crawl-templates-new.ts
@@ -73,7 +73,7 @@ export class CrawlTemplatesNew extends LiteElement {
     };
 
   @state()
-  private isConfigCodeView: boolean = true;
+  private isConfigCodeView: boolean = false;
 
   @state()
   private configCode: string = "";
