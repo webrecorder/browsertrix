8828681e8ef35d2ceb17cb7d78c152d9d022b8cf
sua yoo <sua@suayoo.com>
2022-04-11 13:13:53 -0700

    hotfix: fix crawl sort control alignment

--- frontend/src/pages/archive/crawls-list.ts
+++ frontend/src/pages/archive/crawls-list.ts
@@ -175,7 +175,7 @@ export class CrawlsList extends LiteElement {
             <sl-icon name="search" slot="prefix"></sl-icon>
           </sl-input>
         </div>
-        <div class="col-span-12 md:col-span-1 flex justify-end">
+        <div class="col-span-12 md:col-span-1 flex items-center justify-end">
           <div class="whitespace-nowrap text-sm text-0-500 mr-2">
             ${msg("Sort by")}
           </div>
