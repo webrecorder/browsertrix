c42d5f7159dc4e15678e27f8fd64548d993af365
sua yoo <sua@suayoo.com>
2022-04-11 15:50:25 -0700

    account for no browser profiles

--- frontend/src/pages/archive/browser-profiles-list.ts
+++ frontend/src/pages/archive/browser-profiles-list.ts
@@ -4,6 +4,14 @@ import { msg, localized, str } from "@lit/localize";
 import type { AuthState } from "../../utils/AuthService";
 import LiteElement, { html } from "../../utils/LiteElement";
 
+type BrowserProfile = {
+  id: string;
+  name: string;
+  description: string;
+  last_updated: string;
+  domains: string[];
+};
+
 /**
  * Usage:
  * ```ts
@@ -19,22 +27,22 @@ export class BrowserProfilesList extends LiteElement {
   archiveId?: string;
 
   @state()
-  browserProfiles = [
-    {
-      id: "1",
-      name: "Twitter Example",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      last_updated: new Date().toUTCString(),
-      domains: ["https://twitter.com"],
-    },
-    {
-      id: "2",
-      name: "Twitter Webrecorder",
-      description: "Et netus et malesuada fames.",
-      last_updated: new Date().toUTCString(),
-      domains: ["https://twitter.com", "https://twitter.com/webrecorder_io"],
-    },
+  browserProfiles: BrowserProfile[] = [
+    // {
+    //   id: "1",
+    //   name: "Twitter Example",
+    //   description:
+    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    //   last_updated: new Date().toUTCString(),
+    //   domains: ["https://twitter.com"],
+    // },
+    // {
+    //   id: "2",
+    //   name: "Twitter Webrecorder",
+    //   description: "Et netus et malesuada fames.",
+    //   last_updated: new Date().toUTCString(),
+    //   domains: ["https://twitter.com", "https://twitter.com/webrecorder_io"],
+    // },
   ];
 
   render() {
@@ -60,34 +68,44 @@ export class BrowserProfilesList extends LiteElement {
             </div>
           </div>
         </div>
-        <div class="border rounded" role="rowgroup">
-          ${this.browserProfiles.map(
-            (data) => html`
-              <a
-                class="block p-4 leading-none hover:bg-zinc-50 hover:text-primary border-t first:border-t-0 transition-colors"
-                href=${`/archives/${this.archiveId}/browser-profiles/profile/${data.id}`}
-                @click=${this.navLink}
-                title=${data.name}
-              >
-                <div class="grid grid-cols-8 gap-3 md:gap-5" role="row">
-                  <div class="col-span-8 md:col-span-4" role="cell">
-                    <div class="font-medium mb-1">${data.name}</div>
-                    <div class="text-sm truncate" title=${data.description}>
-                      ${data.description}
-                    </div>
-                  </div>
-                  <div class="col-span-8 md:col-span-1 text-sm" role="cell">
-                    ${new Date(data.last_updated).toLocaleDateString()}
-                  </div>
-                  <div class="col-span-8 md:col-span-3 text-sm" role="cell">
-                    ${data.domains.join(", ")}
-                  </div>
-                </div>
-              </a>
-            `
-          )}
-        </div>
+        ${this.browserProfiles && this.browserProfiles.length
+          ? html`<div class="border rounded" role="rowgroup">
+              ${this.browserProfiles.map(this.renderItem.bind(this))}
+            </div>`
+          : html`
+              <div class="border-t border-b py-5">
+                <p class="text-center text-0-500">
+                  ${msg("No browser profiles yet.")}
+                </p>
+              </div>
+            `}
       </div>
+    `;
+  }
+
+  private renderItem(data: BrowserProfile) {
+    return html`
+      <a
+        class="block p-4 leading-none hover:bg-zinc-50 hover:text-primary border-t first:border-t-0 transition-colors"
+        href=${`/archives/${this.archiveId}/browser-profiles/profile/${data.id}`}
+        @click=${this.navLink}
+        title=${data.name}
+      >
+        <div class="grid grid-cols-8 gap-3 md:gap-5" role="row">
+          <div class="col-span-8 md:col-span-4" role="cell">
+            <div class="font-medium mb-1">${data.name}</div>
+            <div class="text-sm truncate" title=${data.description}>
+              ${data.description}
+            </div>
+          </div>
+          <div class="col-span-8 md:col-span-1 text-sm" role="cell">
+            ${new Date(data.last_updated).toLocaleDateString()}
+          </div>
+          <div class="col-span-8 md:col-span-3 text-sm" role="cell">
+            ${data.domains.join(", ")}
+          </div>
+        </div>
+      </a>
     `;
   }
 }
