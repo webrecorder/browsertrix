5bc3ea8ef3903e8a362f3b59596d90c8a3791945
Ilya Kreymer <ikreymer@gmail.com>
2022-06-05 17:04:27 -0700

    check frontend and backend

--- test/setup.sh
+++ test/setup.sh
@@ -6,6 +6,7 @@ set -e
 #docker service create --name registry --publish published=5000,target=5000 registry:2
 
 export REGISTRY=localhost:5000/
+export FRONTEND_HOST=http://127.0.0.1:9871
 
 docker swarm init
 
@@ -14,11 +15,11 @@ docker stack deploy -c docker-compose.yml btrix --resolve-image changed
 count=0
 sleepfor=5
 
-until $(curl -m 3 --output /dev/null --silent --head --fail http://127.0.0.1:9871/); do
-  echo "waiting for startup... (has waited for $count seconds)"
+until $(curl -m 3 --output /dev/null --head --fail $FRONTEND_HOST/); do
+  echo "waiting for frontend startup... (has waited for $count seconds)"
   sleep $sleepfor
   count=$((count+$sleepfor))
-  if [ $count -gt 30 ]; then
+  if [ $count -gt 60 ]; then
     echo "swarm frontend startup failed, frontend & backend logs below:"
     echo ""
     echo "ps"
@@ -30,9 +31,27 @@ until $(curl -m 3 --output /dev/null --silent --head --fail http://127.0.0.1:987
     echo "backend"
     echo "--------"
     docker service logs btrix_backend 2>&1 | cat
-    exit 0
   fi
 done
 
+
+until $(curl -m 3 --output /dev/null --fail $FRONTEND_HOST/api/settings); do
+  echo "waiting for backend api startup... (has waited for $count seconds)"
+  sleep $sleepfor
+  count=$((count+$sleepfor))
+  if [ $count -gt 60 ]; then
+    echo "swarm frontend startup failed, frontend & backend logs below:"
+    echo ""
+    echo "ps"
+    echo "--------"
+    docker stack ps btrix --no-trunc
+    echo "frontend"
+    echo "--------"
+    docker service logs btrix_frontend 2>&1 | cat
+    echo "backend"
+    echo "--------"
+    docker service logs btrix_backend 2>&1 | cat
+  fi
+done
 
 
