f86812577956c84285fe3930ed9aef7d652fbbe1
Ilya Kreymer <ikreymer@gmail.com>
2022-06-13 21:50:48 +0000

    use podman-compose by default in {run,stop}-podman.sh

--- run-podman.sh
+++ run-podman.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-docker-compose build backend frontend;
-docker secret rm btrix_shared_job_config.yaml
-docker secret create btrix_shared_job_config.yaml ./configs/config.yaml
-REGISTRY="" docker-compose -f docker-compose.yml -f docker-compose.podman.yml up -d --remove-orphans
+compose=podman-compose
+
+# can optionally be used with docker-compose
+#compose=docker-compose
+
+$compose build backend frontend;
+$compose secret rm btrix_shared_job_config.yaml
+$compose secret create btrix_shared_job_config.yaml ./configs/config.yaml
+REGISTRY="" $compose -f docker-compose.yml -f docker-compose.podman.yml up -d --remove-orphans
--- stop-podman.sh
+++ stop-podman.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml -f docker-compose.podman.yml kill; docker-compose -f docker-compose.yml -f docker-compose.podman.yml rm -f;
+compose=podman-compose
+
+# can optionally be used with docker-compose
+#compose=docker-compose
+
+$compose -f docker-compose.yml -f docker-compose.podman.yml down
 
