5bc3d97db769b06dd8cc33cd944ea1ef077dfdae
Ilya Kreymer <ikreymer@gmail.com>
2022-06-14 01:48:56 -0400

    backend build: use latest podman-remote static binary from image

--- scripts/run-podman.sh
+++ scripts/run-podman.sh
@@ -5,7 +5,9 @@ compose=podman-compose
 #compose=docker-compose
 CURR=$(dirname "${BASH_SOURCE[0]}")
 
-compose build backend frontend;
+# build
+$compose build backend frontend;
+
 podman secret rm btrix_shared_job_config.yaml
 podman secret create btrix_shared_job_config.yaml $CURR/../configs/config.yaml
 REGISTRY="" $compose -f $CURR/../docker-compose.yml -f  $CURR/../configs/docker-compose.podman.yml up -d --remove-orphans
