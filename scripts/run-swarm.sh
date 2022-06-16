1653eb1d18acc603d0b13be6ec921554cfab64f6
Ilya Kreymer <ikreymer@gmail.com>
2022-06-16 13:20:26 -0700

    allow customizing swarm stack configs with env vars

--- scripts/run-swarm.sh
+++ scripts/run-swarm.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-export REGISTRY=localhost:5000/
+
+# enable to build with local registry
+# export REGISTRY=localhost:5000/
 
 CURR=$(dirname "${BASH_SOURCE[0]}")
+
+export BACKEND_TAG=latest
+export FRONTEND_TAG=latest
+
+# if using authsign
+export AUTHSIGN_PORT=80
+export AUTHSIGN_TAG=0.5.0
+
+# enable to change minio data storage location
+#export MINIO_DATA_VOLUME=/minio-data
+
+docker swarm init
 
 # without authsign
 docker stack deploy -c docker-compose.yml -c $CURR/../configs/docker-compose.swarm.yml btrix
 
 # with authsign
-# set port if proxying to authsign via a different port
-AUTHSIGN_PORT=80
-#docker stack deploy -c docker-compose.yml -c docker-compose.swarm.yml -c $CURR/../configs/docker-compose.signing.yml btrix
+#docker stack deploy -c docker-compose.yml -c $CURR/../configs/docker-compose.swarm.yml -c $CURR/../configs/docker-compose.signing.yml btrix
+
