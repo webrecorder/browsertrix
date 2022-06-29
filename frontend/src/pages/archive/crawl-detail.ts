bd4a953efd9caebc68e35d16119318d49bf3c4c7
sua yoo <sua@suayoo.com>
2022-06-29 14:29:14 -0700

    fix watch header alignment

--- frontend/src/pages/archive/crawl-detail.ts
+++ frontend/src/pages/archive/crawl-detail.ts
@@ -434,7 +434,7 @@ export class CrawlDetail extends LiteElement {
 
     return html`
       <header class="flex justify-between">
-        <h3 class="text-lg font-medium mb-2">${msg("Watch Crawl")}</h3>
+        <h3 class="text-lg font-medium my-2">${msg("Watch Crawl")}</h3>
         ${isRunning && document.fullscreenEnabled
           ? html`
               <sl-icon-button
