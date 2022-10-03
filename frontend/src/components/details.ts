ecdfaa6943e52f31c7cc4b1b1ee6faf3c8dd1cb0
sua yoo <sua@suayoo.com>
2022-10-03 15:15:50 -0700

    style details

--- frontend/src/components/details.ts
+++ frontend/src/components/details.ts
@@ -1,5 +1,7 @@
-import { LitElement, html, css } from "lit";
+import { LitElement, html, css, unsafeCSS } from "lit";
 import { property } from "lit/decorators.js";
+import caretDownFillSvg from "../assets/images/caret-down-fill.svg";
+import caretRightFillSvg from "../assets/images/caret-right-fill.svg";
 
 /**
  * Styled <details>
@@ -27,8 +29,40 @@ export class Details extends LitElement {
       padding: var(--sl-spacing-x-small) 0;
       margin-bottom: var(--sl-spacing-x-small);
       line-height: 1;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      list-style: none;
+      user-select: none;
+    }
+
+    summary::before {
+      display: block;
+      width: 1rem;
+      height: 1rem;
+      margin-right: var(--sl-spacing-2x-small);
+    }
+
+    details[open] summary::before {
+      content: url(${unsafeCSS(caretDownFillSvg)});
+    }
+
+    details:not([open]) summary::before {
+      content: url(${unsafeCSS(caretRightFillSvg)});
     }
   `;
+
+  connectedCallback() {
+    // Preload icon for other state
+    const img = new Image();
+    if (this.open) {
+      img.src = caretRightFillSvg;
+    } else {
+      img.src = caretDownFillSvg;
+    }
+
+    super.connectedCallback();
+  }
 
   render() {
     return html`
