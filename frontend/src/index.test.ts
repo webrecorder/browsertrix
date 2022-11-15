a8826c054d9544358b4740f51c2c84a81383a983
sua yoo <sua@suayoo.com>
2022-11-15 15:53:41 -0800

    update unit test

--- frontend/src/index.test.ts
+++ frontend/src/index.test.ts
@@ -27,7 +27,7 @@ describe("browsertrix-app", () => {
   });
 
   it("sets auth state from session storage", async () => {
-    stub(window.localStorage, "getItem").callsFake((key) => {
+    stub(window.sessionStorage, "getItem").callsFake((key) => {
       if (key === "btrix.auth")
         return JSON.stringify({
           username: "test-auth@example.com",
@@ -42,7 +42,7 @@ describe("browsertrix-app", () => {
   });
 
   it("sets user info", async () => {
-    stub(window.localStorage, "getItem").callsFake((key) => {
+    stub(window.sessionStorage, "getItem").callsFake((key) => {
       if (key === "btrix.auth")
         return JSON.stringify({
           username: "test-auth@example.com",
