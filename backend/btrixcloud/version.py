c6dc4eb8b12682f5af5309c8031d2e219bbfba4c
Ilya Kreymer <ikreymer@gmail.com>
2022-11-18 17:06:48 -0800

    add script for updating vesrion.txt to backend and frontend add version.py in backend

---
+++ backend/btrixcloud/version.py
@@ -0,0 +1,2 @@
+""" current version """
+__version__ = "1.1.0-beta.0"
---
+++ update-version.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+version=`cat version.txt`
+jq ".version=\"$version\"" ./frontend/package.json > ./tmp-package.json
+mv ./tmp-package.json ./frontend/package.json
+
+echo '""" current version """' > ./backend/btrixcloud/version.py
+echo "__version__ = \"$version\"" >> ./backend/btrixcloud/version.py
