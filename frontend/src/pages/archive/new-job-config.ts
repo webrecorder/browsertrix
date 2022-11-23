5ef06fa47d5c2e44e37f02bfd0c24fbe291df7d8
sua yoo <sua@suayoo.com>
2022-11-23 15:05:46 -0800

    handle navigation

--- frontend/src/pages/archive/new-job-config.ts
+++ frontend/src/pages/archive/new-job-config.ts
@@ -15,43 +15,65 @@ type State =
   | "crawlBehaviors"
   | "jobScheduling"
   | "jobInformation";
+type StepEventName =
+  | "URL_LIST"
+  | "SEEDED_CRAWL"
+  | "CRAWL_BEHAVIORS"
+  | "JOB_SCHEDULING"
+  | "JOB_INFORMATION";
+
+const jobTypeStateConfig = {
+  URL_LIST: "urListSetup",
+  SEEDED_CRAWL: "seededCrawlSetup",
+} as Record<StepEventName, State>;
+const stepStateConfig: Record<StepEventName, State> = {
+  ...jobTypeStateConfig,
+  CRAWL_BEHAVIORS: "crawlBehaviors",
+  JOB_SCHEDULING: "jobScheduling",
+  JOB_INFORMATION: "jobInformation",
+};
+
 const machineConfig = {
   initial: "urListSetup",
   states: {
     chooseJobType: {
       on: {
-        SELECT_URL_LIST: "urListSetup",
-        SELECT_SEEDED_CRAWL: "seededCrawlSetup",
+        ...jobTypeStateConfig,
       },
     },
     urListSetup: {
       on: {
+        ...stepStateConfig,
         BACK: "chooseJobType",
         CONTINUE: "crawlBehaviors",
       },
     },
     seededCrawlSetup: {
       on: {
+        ...stepStateConfig,
         BACK: "chooseJobType",
         CONTINUE: "crawlBehaviors",
       },
     },
     crawlBehaviors: {
       on: {
-        // BACK: "",
+        ...stepStateConfig,
+        BACK: "urListSetup", // TODO
         CONTINUE: "jobScheduling",
       },
     },
     jobScheduling: {
       on: {
+        ...stepStateConfig,
         BACK: "crawlBehaviors",
         CONTINUE: "jobInformation",
       },
     },
     jobInformation: {
       on: {
+        ...stepStateConfig,
         BACK: "jobScheduling",
-        // CONTINUE: "",
+        // CONTINUE: "", TODO
       },
     },
   },
@@ -160,7 +182,7 @@ export class NewJobConfig extends LiteElement {
         <div
           role="button"
           class="block"
-          @click=${() => stateService.send("SELECT_URL_LIST")}
+          @click=${() => stateService.send("URL_LIST")}
         >
           <figure class="w-64 m-4">
             <img src=${urlListSvg} />
@@ -177,7 +199,7 @@ export class NewJobConfig extends LiteElement {
         <div
           role="button"
           class="block"
-          @click=${() => stateService.send("SELECT_SEEDED_CRAWL")}
+          @click=${() => stateService.send("SEEDED_CRAWL")}
         >
           <figure class="w-64 m-4">
             <img src=${seededCrawlSvg} />
@@ -202,24 +224,42 @@ export class NewJobConfig extends LiteElement {
           class="absolute top-0 bottom-0 w-1 bg-neutral-100 rounded-full shadow-inner"
         ></div>
         <ul>
-          ${this.renderNavItem(false)} ${this.renderNavItem(true)}
-          ${this.renderNavItem(false)} ${this.renderNavItem(false)}
-          ${this.renderNavItem(false)}
+          ${this.renderNavItem("URL_LIST", msg("Crawler Setup (URL)"))}
+          ${this.renderNavItem("SEEDED_CRAWL", msg("Crawler Setup (Seed)"))}
+          ${this.renderNavItem("CRAWL_BEHAVIORS", msg("Crawl Behaviors"))}
+          ${this.renderNavItem("JOB_SCHEDULING", msg("Job Scheduling"))}
+          ${this.renderNavItem("JOB_INFORMATION", msg("Job Information"))}
         </ul>
       </nav>
     `;
   }
 
-  private renderNavItem(isActive: boolean) {
+  private renderNavItem(
+    eventName: StepEventName,
+    content: TemplateResult | string
+  ) {
+    const isActive = this.stateValue === stepStateConfig[eventName];
     return html`
-      <li class="relative" role="menuitem" aria-selected=${isActive}>
+      <li
+        class="relative cursor-pointer font-medium"
+        role="menuitem"
+        aria-selected=${isActive}
+        @click=${() => {
+          stateService.send(eventName);
+        }}
+      >
         <div
           class="absolute top-0 bottom-0 rounded-full transition-all ${isActive
             ? "w-1 bg-primary"
             : "w-0"}"
           role="presentation"
         ></div>
-        <span class="inline-block px-4 py-2">item</span>
+        <span
+          class="inline-block px-4 py-2 ${isActive
+            ? "text-primary"
+            : "text-neutral-500"}"
+          >${content}</span
+        >
       </li>
     `;
   }
