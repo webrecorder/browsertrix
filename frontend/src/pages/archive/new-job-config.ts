06e9e8f01589c0728b2993e9fa655844ee864784
sua yoo <sua@suayoo.com>
2022-11-27 10:25:36 -0800

    add form state

--- frontend/src/pages/archive/new-job-config.ts
+++ frontend/src/pages/archive/new-job-config.ts
@@ -1,13 +1,18 @@
 import type { TemplateResult } from "lit";
 import { state, property, query } from "lit/decorators.js";
 import { msg, localized, str } from "@lit/localize";
-import type { StateMachine } from "@xstate/fsm";
+import merge from "lodash/fp/merge";
 
 import seededCrawlSvg from "../../assets/images/new-job-config_Seeded-Crawl.svg";
 import urlListSvg from "../../assets/images/new-job-config_URL-List.svg";
 import LiteElement, { html } from "../../utils/LiteElement";
 import type { AuthState } from "../../utils/AuthService";
 import type { Tab } from "../../components/tab-list";
+import type {
+  ExclusionRemoveEvent,
+  ExclusionChangeEvent,
+} from "../../components/queue-exclusion-table";
+import type { CrawlConfig } from "./types";
 
 type JobType = null | "urlList" | "seeded";
 type TabName =
@@ -24,6 +29,28 @@ type Tabs = Record<
   }
 >;
 type StepName = "chooseJobType" | TabName;
+type ProgressState = {
+  currentStep: StepName;
+  activeTab: TabName | null;
+  tabs: Tabs;
+};
+type FormState = {
+  exclude: CrawlConfig["exclude"];
+};
+const initialJobType: JobType = null;
+const initialProgressState: ProgressState = {
+  activeTab: "crawlerSetup",
+  currentStep: "crawlerSetup",
+  tabs: {
+    crawlerSetup: { enabled: true, error: false, completed: false },
+    crawlBehaviors: { enabled: false, error: false, completed: false },
+    jobScheduling: { enabled: false, error: false, completed: false },
+    jobInformation: { enabled: false, error: false, completed: false },
+  },
+};
+const initialFormState: FormState = {
+  exclude: [""], // Empty slots for adding exclusions
+};
 const stepOrder: StepName[] = [
   "chooseJobType",
   "crawlerSetup",
@@ -31,16 +58,8 @@ const stepOrder: StepName[] = [
   "jobScheduling",
   "jobInformation",
 ];
-const initialStep: StepName = "chooseJobType";
-const initialTabs: Tabs = {
-  crawlerSetup: { enabled: false, error: false, completed: false },
-  crawlBehaviors: { enabled: false, error: false, completed: false },
-  jobScheduling: { enabled: false, error: false, completed: false },
-  jobInformation: { enabled: false, error: false, completed: false },
-};
-const initialJobType: JobType = null;
 const orderedTabNames = stepOrder.filter(
-  (stepName) => initialTabs[stepName as TabName]
+  (stepName) => initialProgressState.tabs[stepName as TabName]
 ) as TabName[];
 
 @localized()
@@ -52,26 +71,23 @@ export class NewJobConfig extends LiteElement {
   archiveId!: string;
 
   @state()
-  private activeStep: StepName = initialStep;
+  private progressState: ProgressState = initialProgressState;
 
   @state()
-  private currentProgressStep: StepName = initialStep;
-
-  @state()
-  private tabs: Tabs = initialTabs;
+  private formState: FormState = initialFormState;
 
   @state()
   private jobType: JobType = initialJobType;
 
   private get formHasError() {
-    return Object.values(this.tabs).some(({ error }) => error);
+    return Object.values(this.progressState.tabs).some(({ error }) => error);
   }
 
   @query('form[name="newJobConfig"]')
   formElem?: HTMLFormElement;
 
   render() {
-    if (this.activeStep === "chooseJobType") {
+    if (!this.progressState.activeTab) {
       return this.renderChooseJobType();
     }
 
@@ -87,13 +103,13 @@ export class NewJobConfig extends LiteElement {
 
     return html`
       <h3 class="ml-52 text-lg font-medium mb-3">
-        ${tabLabels[this.activeStep]}
+        ${tabLabels[this.progressState.activeTab]}
       </h3>
 
       <form name="newJobConfig" @submit=${this.onSubmit}>
         <btrix-tab-list
-          activePanel="newJobConfig-${this.activeStep}"
-          progressPanel="newJobConfig-${this.currentProgressStep}"
+          activePanel="newJobConfig-${this.progressState.activeTab}"
+          progressPanel="newJobConfig-${this.progressState.currentStep}"
         >
           ${orderedTabNames.map((tabName) =>
             this.renderNavItem(tabName, tabLabels[tabName])
@@ -114,7 +130,7 @@ export class NewJobConfig extends LiteElement {
             >
               ${this.renderSeededCrawlSetup(formColClassName)}
             </div>
-            ${this.renderFooter()}
+            ${this.renderFooter({ isFirst: true })}
           </btrix-tab-panel>
           <btrix-tab-panel name="newJobConfig-crawlBehaviors">
             <div class=${contentClassName}>
@@ -132,7 +148,7 @@ export class NewJobConfig extends LiteElement {
             <div class=${contentClassName}>
               ${this.renderJobInformation(formColClassName)}
             </div>
-            ${this.renderFooter(true)}
+            ${this.renderFooter({ isLast: true })}
           </btrix-tab-panel>
         </btrix-tab-list>
       </form>
@@ -184,8 +200,8 @@ export class NewJobConfig extends LiteElement {
   }
 
   private renderNavItem(tabName: TabName, content: TemplateResult | string) {
-    const isActive = tabName === this.activeStep;
-    const { error: isInvalid, completed } = this.tabs[tabName];
+    const isActive = tabName === this.progressState.activeTab;
+    const { error: isInvalid, completed } = this.progressState.tabs[tabName];
     let icon = html`
       <sl-icon
         name="circle"
@@ -220,7 +236,7 @@ export class NewJobConfig extends LiteElement {
       <btrix-tab
         slot="nav"
         name="newJobConfig-${tabName}"
-        ?disabled=${!this.tabs[tabName].enabled}
+        ?disabled=${!this.progressState.tabs[tabName].enabled}
         @click=${this.tabClickHandler(tabName)}
       >
         ${icon}
@@ -229,14 +245,23 @@ export class NewJobConfig extends LiteElement {
     `;
   }
 
-  private renderFooter(isLastStep = false) {
+  private renderFooter({ isFirst = false, isLast = false } = {}) {
     return html`
       <div class="px-5 py-4 border-t flex justify-between">
-        <sl-button size="small" @click=${this.backStep}>
-          <sl-icon slot="prefix" name="arrow-left"></sl-icon>
-          ${msg("Previous Step")}
-        </sl-button>
-        ${isLastStep
+        ${isFirst
+          ? html`
+              <sl-button size="small" @click=${this.backStep}>
+                <sl-icon slot="prefix" name="arrow-left"></sl-icon>
+                ${msg("Start Over")}
+              </sl-button>
+            `
+          : html`
+              <sl-button size="small" @click=${this.backStep}>
+                <sl-icon slot="prefix" name="arrow-left"></sl-icon>
+                ${msg("Previous Step")}
+              </sl-button>
+            `}
+        ${isLast
           ? html`<sl-button type="submit" size="small" variant="primary">
               ${msg("Save")}
             </sl-button>`
@@ -289,6 +314,9 @@ export class NewJobConfig extends LiteElement {
         </sl-radio-group>
       </div>
       ${this.renderHelpText(html`TODO`)}
+
+      <div class="${formColClassName}">${this.renderExclusionEditor()}</div>
+      ${this.renderHelpText(html`TODO`)}
     `;
   }
 
@@ -308,60 +336,142 @@ export class NewJobConfig extends LiteElement {
     return html`TODO`;
   }
 
+  private renderExclusionEditor() {
+    return html`
+      <btrix-queue-exclusion-table
+        .exclusions=${this.formState.exclude}
+        pageSize="50"
+        editable
+        removable
+        @on-remove=${this.handleRemoveRegex}
+        @on-change=${this.handleChangeRegex}
+      ></btrix-queue-exclusion-table>
+      <sl-button
+        class="w-full mt-1"
+        @click=${() =>
+          this.updateFormState({
+            exclude: [...(this.formState.exclude || []), ""],
+          })}
+      >
+        <sl-icon slot="prefix" name="plus-lg"></sl-icon>
+        <span class="text-neutral-600">${msg("Add More")}</span>
+      </sl-button>
+    `;
+  }
+
+  private handleRemoveRegex(e: ExclusionRemoveEvent) {
+    const { index } = e.detail;
+    if (!this.formState.exclude) {
+      this.updateFormState(
+        {
+          exclude: initialFormState.exclude,
+        },
+        true
+      );
+    } else {
+      const { exclude } = this.formState;
+      this.updateFormState(
+        {
+          exclude: [...exclude.slice(0, index), ...exclude.slice(index + 1)],
+        },
+        true
+      );
+    }
+  }
+
+  private handleChangeRegex(e: ExclusionChangeEvent) {
+    const { regex, index } = e.detail;
+
+    const nextExclusions = [...this.formState.exclude!];
+    nextExclusions[index] = regex;
+    this.updateFormState(
+      {
+        exclude: nextExclusions,
+      },
+      true
+    );
+  }
+
   private onFieldChange = (e: Event) => {
     const el = e.target as HTMLInputElement & {
       invalid: boolean;
     };
-    const currentTab = this.activeStep as TabName;
+    const currentTab = this.progressState.activeTab as TabName;
     if (el.invalid) {
-      this.tabs[currentTab].error = true;
-    } else if (this.tabs[currentTab].error) {
+      const tabs = this.progressState.tabs;
+      tabs[currentTab].error = true;
+      this.updateProgressState({ tabs });
+    } else if (this.progressState.tabs[currentTab].error) {
       const hasInvalid = el
         .closest("btrix-tab-panel")
         ?.querySelector("[data-invalid]");
       if (!hasInvalid) {
-        this.tabs[currentTab].error = false;
+        const tabs = this.progressState.tabs;
+        tabs[currentTab].error = false;
+        this.updateProgressState({ tabs });
       }
     }
   };
 
-  private tabClickHandler = (step: StepName) => (e: MouseEvent) => {
+  private tabClickHandler = (step: TabName) => (e: MouseEvent) => {
     const tab = e.currentTarget as Tab;
     if (tab.disabled || tab.active) {
       e.preventDefault();
       e.stopPropagation();
       return;
     }
-
-    this.activeStep = step;
+    this.updateProgressState({ activeTab: step });
   };
 
   private selectJobType(jobType: JobType) {
     this.jobType = jobType;
-    this.activeStep = "crawlerSetup";
-    this.currentProgressStep = this.activeStep;
-    this.tabs[this.activeStep].enabled = true;
+    const activeTab = "crawlerSetup";
+    const tabs = this.progressState.tabs;
+    tabs[activeTab].enabled = true;
+    this.updateProgressState({
+      activeTab,
+      currentStep: activeTab,
+      tabs,
+    });
   }
 
   private backStep() {
-    this.activeStep = stepOrder[stepOrder.indexOf(this.activeStep) - 1];
+    const targetTabIdx = stepOrder.indexOf(this.progressState.activeTab!) - 1;
+    if (targetTabIdx) {
+      this.updateProgressState({
+        activeTab: stepOrder[
+          stepOrder.indexOf(this.progressState.activeTab!) - 1
+        ] as TabName,
+      });
+    } else {
+      // Reset to job type selection
+      this.updateProgressState(initialProgressState);
+    }
   }
 
   private nextStep() {
     const isValid = this.checkCurrentPanelValidity();
 
     if (isValid) {
-      const nextStep = stepOrder[
-        stepOrder.indexOf(this.activeStep) + 1
+      const nextTab = stepOrder[
+        stepOrder.indexOf(this.progressState.activeTab!) + 1
       ] as TabName;
-      const isFirstEnabled = !this.tabs[nextStep].enabled;
+
+      const { tabs, currentStep } = this.progressState;
+      const isFirstEnabled = !tabs[nextTab].enabled;
+      let nextTabs = tabs;
+      let nextCurrentStep = currentStep;
 
       if (isFirstEnabled) {
-        this.tabs[nextStep].enabled = true;
-        this.currentProgressStep = nextStep;
+        nextTabs[nextTab].enabled = true;
+        nextCurrentStep = nextTab;
       }
-      this.tabs[this.activeStep as TabName].completed = true;
-      this.activeStep = nextStep;
+
+      this.updateProgressState({
+        activeTab: nextTab,
+        currentStep: nextCurrentStep,
+        tabs: nextTabs,
+      });
     }
   }
 
@@ -369,7 +479,7 @@ export class NewJobConfig extends LiteElement {
     if (!this.formElem) return false;
 
     const activePanel = this.formElem.querySelector(
-      `btrix-tab-panel[name="newJobConfig-${this.activeStep}"]`
+      `btrix-tab-panel[name="newJobConfig-${this.progressState.activeTab}"]`
     );
     const invalidElems = [...activePanel!.querySelectorAll("[data-invalid]")];
 
@@ -393,6 +503,27 @@ export class NewJobConfig extends LiteElement {
     }
 
     console.log(new FormData(form));
+  }
+
+  private updateProgressState(nextState: Partial<ProgressState>) {
+    this.progressState = merge(
+      <ProgressState>this.progressState,
+      <Partial<ProgressState>>nextState
+    );
+  }
+
+  private updateFormState(nextState: Partial<FormState>, shallowMerge = false) {
+    if (shallowMerge) {
+      this.formState = {
+        ...this.formState,
+        ...nextState,
+      };
+    } else {
+      this.formState = merge(
+        <FormState>this.formState,
+        <Partial<FormState>>nextState
+      );
+    }
   }
 }
 
