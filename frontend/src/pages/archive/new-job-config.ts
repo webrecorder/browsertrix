0348064cf0d81f9325b1df8cd32def317539bcbd
sua yoo <sua@suayoo.com>
2022-11-27 09:39:44 -0800

    simplify state

--- frontend/src/pages/archive/new-job-config.ts
+++ frontend/src/pages/archive/new-job-config.ts
@@ -1,7 +1,6 @@
 import type { TemplateResult } from "lit";
 import { state, property, query } from "lit/decorators.js";
 import { msg, localized, str } from "@lit/localize";
-import { createMachine, interpret, assign } from "@xstate/fsm";
 import type { StateMachine } from "@xstate/fsm";
 
 import seededCrawlSvg from "../../assets/images/new-job-config_Seeded-Crawl.svg";
@@ -10,75 +9,39 @@ import LiteElement, { html } from "../../utils/LiteElement";
 import type { AuthState } from "../../utils/AuthService";
 import type { Tab } from "../../components/tab-list";
 
-type State =
-  | "chooseJobType"
+type JobType = null | "urlList" | "seeded";
+type TabName =
   | "crawlerSetup"
   | "crawlBehaviors"
   | "jobScheduling"
   | "jobInformation";
-type StepEventName =
-  | "CRAWLER_SETUP"
-  | "CRAWL_BEHAVIORS"
-  | "JOB_SCHEDULING"
-  | "JOB_INFORMATION";
-type JobType = null | "urlList" | "seeded";
-type JobSelectEvent = {
-  type: "CRAWLER_SETUP";
-  jobType: JobType;
-};
-type ValidityChangeEvent = {
-  type: "VALIDITY_CHANGE";
-  valid: boolean;
-};
-type Context = {
-  jobType: JobType;
-  currentProgress: State;
-  steps: Record<
-    State,
-    {
-      enabled: boolean;
-      completed: boolean;
-      error: boolean;
-    }
-  >;
-};
-const tabEventTarget: Record<StepEventName, State> = {
-  CRAWLER_SETUP: "crawlerSetup",
-  CRAWL_BEHAVIORS: "crawlBehaviors",
-  JOB_SCHEDULING: "jobScheduling",
-  JOB_INFORMATION: "jobInformation",
-};
-const tabNavStates = Object.keys(tabEventTarget).reduce((acc, eventName) => {
-  const stateValue = tabEventTarget[eventName as StepEventName];
-  return {
-    ...acc,
-    [eventName]: [
-      {
-        cond: (ctx: Context) => ctx.steps[stateValue].enabled,
-        target: stateValue,
-      },
-    ],
-  };
-}, {});
-const stepOrder = [
+type Tabs = Record<
+  TabName,
+  {
+    enabled: boolean;
+    completed: boolean;
+    error: boolean;
+  }
+>;
+type StepName = "chooseJobType" | TabName;
+const stepOrder: StepName[] = [
   "chooseJobType",
   "crawlerSetup",
   "crawlBehaviors",
   "jobScheduling",
   "jobInformation",
 ];
-const initialState = "crawlerSetup";
-const initialContext: Context = {
-  jobType: "urlList",
-  currentProgress: "crawlerSetup",
-  steps: {
-    chooseJobType: { enabled: true, error: false, completed: false },
-    crawlerSetup: { enabled: false, error: false, completed: false },
-    crawlBehaviors: { enabled: false, error: false, completed: false },
-    jobScheduling: { enabled: false, error: false, completed: false },
-    jobInformation: { enabled: false, error: false, completed: false },
-  },
+const initialStep: StepName = "chooseJobType";
+const initialTabs: Tabs = {
+  crawlerSetup: { enabled: false, error: false, completed: false },
+  crawlBehaviors: { enabled: false, error: false, completed: false },
+  jobScheduling: { enabled: false, error: false, completed: false },
+  jobInformation: { enabled: false, error: false, completed: false },
 };
+const initialJobType: JobType = null;
+const orderedTabNames = stepOrder.filter(
+  (stepName) => initialTabs[stepName as TabName]
+) as TabName[];
 
 @localized()
 export class NewJobConfig extends LiteElement {
@@ -89,252 +52,63 @@ export class NewJobConfig extends LiteElement {
   archiveId!: string;
 
   @state()
-  private stateValue: State = initialState;
+  private activeStep: StepName = initialStep;
 
   @state()
-  private stateContext: Context = initialContext;
+  private currentProgressStep: StepName = initialStep;
 
-  private stateService: StateMachine.Service<any, any, any>;
+  @state()
+  private tabs: Tabs = initialTabs;
+
+  @state()
+  private jobType: JobType = initialJobType;
 
   private get formHasError() {
-    return Object.values(this.stateContext.steps).some(({ error }) => error);
+    return Object.values(this.tabs).some(({ error }) => error);
   }
 
   @query('form[name="newJobConfig"]')
   formElem?: HTMLFormElement;
 
-  constructor() {
-    super();
-    const makeStepEntryActions = (stepState: State) =>
-      assign({
-        steps: (ctx: Pick<Context, "steps">) => ({
-          ...ctx.steps,
-          [stepState]: { ...ctx.steps[stepState], enabled: true },
-        }),
-        currentProgress: (ctx: Pick<Context, "currentProgress">) =>
-          stepOrder.indexOf(stepState) > stepOrder.indexOf(ctx.currentProgress)
-            ? stepState
-            : ctx.currentProgress,
-      });
-    const stateMachine = createMachine({
-      initial: initialState,
-      context: initialContext,
-      states: {
-        chooseJobType: {
-          on: {
-            CRAWLER_SETUP: {
-              target: "crawlerSetup",
-              actions: assign({
-                jobType: (ctx, evt: any) => evt.jobType,
-              }),
-            },
-          },
-        },
-        crawlerSetup: {
-          entry: makeStepEntryActions("crawlerSetup"),
-          on: {
-            ...tabNavStates,
-            BACK: "chooseJobType",
-            CONTINUE: [
-              {
-                cond: this.checkValidity,
-                target: "crawlBehaviors",
-                actions: assign({
-                  steps: (ctx: Pick<Context, "steps">) => ({
-                    ...ctx.steps,
-                    crawlerSetup: {
-                      ...ctx.steps["crawlerSetup"],
-                      completed: true,
-                    },
-                  }),
-                }),
-              },
-            ],
-            VALIDITY_CHANGE: {
-              actions: assign({
-                steps: (ctx: Pick<Context, "steps">, evt: any) => ({
-                  ...ctx.steps,
-                  crawlerSetup: {
-                    ...ctx.steps.crawlerSetup,
-                    error: !(evt as ValidityChangeEvent).valid,
-                  },
-                }),
-              }),
-            },
-          },
-        },
-        crawlBehaviors: {
-          entry: makeStepEntryActions("crawlBehaviors"),
-          on: {
-            ...tabNavStates,
-            BACK: "crawlerSetup",
-            CONTINUE: [
-              {
-                cond: this.checkValidity,
-                target: "jobScheduling",
-                actions: assign({
-                  steps: (ctx: Pick<Context, "steps">) => ({
-                    ...ctx.steps,
-                    crawlBehaviors: {
-                      ...ctx.steps["crawlBehaviors"],
-                      completed: true,
-                    },
-                  }),
-                }),
-              },
-            ],
-            VALIDITY_CHANGE: {
-              actions: assign({
-                steps: (ctx: Pick<Context, "steps">, evt: any) => ({
-                  ...ctx.steps,
-                  crawlBehaviors: {
-                    ...ctx.steps.crawlBehaviors,
-                    error: !(evt as ValidityChangeEvent).valid,
-                  },
-                }),
-              }),
-            },
-          },
-        },
-        jobScheduling: {
-          entry: makeStepEntryActions("jobScheduling"),
-          on: {
-            ...tabNavStates,
-            BACK: "crawlBehaviors",
-            CONTINUE: [
-              {
-                cond: this.checkValidity,
-                target: "jobInformation",
-                actions: assign({
-                  steps: (ctx: Pick<Context, "steps">) => ({
-                    ...ctx.steps,
-                    jobScheduling: {
-                      ...ctx.steps["jobScheduling"],
-                      completed: true,
-                    },
-                  }),
-                }),
-              },
-            ],
-            VALIDITY_CHANGE: {
-              actions: assign({
-                steps: (ctx: Pick<Context, "steps">, evt: any) => ({
-                  ...ctx.steps,
-                  jobScheduling: {
-                    ...ctx.steps.jobScheduling,
-                    error: !(evt as ValidityChangeEvent).valid,
-                  },
-                }),
-              }),
-            },
-          },
-        },
-        jobInformation: {
-          entry: makeStepEntryActions("jobInformation"),
-          on: {
-            ...tabNavStates,
-            BACK: "jobScheduling",
-            CONTINUE: [
-              {
-                cond: this.checkValidity,
-                target: "jobInformation",
-                actions: assign({
-                  steps: (ctx: Pick<Context, "steps">) => ({
-                    ...ctx.steps,
-                    jobInformation: {
-                      ...ctx.steps["jobInformation"],
-                      completed: true,
-                    },
-                  }),
-                }),
-              },
-            ],
-            VALIDITY_CHANGE: {
-              actions: assign({
-                steps: (ctx: Pick<Context, "steps">, evt: any) => ({
-                  ...ctx.steps,
-                  jobInformation: {
-                    ...ctx.steps.jobInformation,
-                    error: !(evt as ValidityChangeEvent).valid,
-                  },
-                }),
-              }),
-            },
-          },
-        },
-      },
-    });
-    this.stateService = interpret(stateMachine);
-  }
-
-  connectedCallback() {
-    super.connectedCallback();
-
-    this.stateService.start();
-    this.stateService.subscribe((state) => {
-      this.stateValue = state.value;
-      this.stateContext = state.context as Context;
-      console.log("state change:", state);
-    });
-  }
-
-  disconnectedCallback() {
-    this.stateService.stop();
-    super.disconnectedCallback();
-  }
-
   render() {
-    if (this.stateValue === "chooseJobType") {
+    if (this.activeStep === "chooseJobType") {
       return this.renderChooseJobType();
     }
 
-    let heading: TemplateResult | string;
-
-    switch (this.stateValue) {
-      case "crawlerSetup":
-        heading = msg("Crawler Setup");
-        break;
-      case "crawlBehaviors":
-        heading = msg("Crawl Behaviors");
-        break;
-      case "jobScheduling":
-        heading = msg("Job Scheduling");
-        break;
-      case "jobInformation":
-        heading = msg("Job Information");
-        break;
-      default:
-        heading = "";
-        break;
-    }
+    const tabLabels = {
+      crawlerSetup: msg("Crawler Setup"),
+      crawlBehaviors: msg("Crawl Behaviors"),
+      jobScheduling: msg("Job Scheduling"),
+      jobInformation: msg("Job Information"),
+    };
 
     const contentClassName = "p-5 grid grid-cols-1 md:grid-cols-5 gap-5";
     const formColClassName = "col-span-1 md:col-span-3";
 
     return html`
-      <h3 class="ml-52 text-lg font-medium mb-3">${heading}</h3>
+      <h3 class="ml-52 text-lg font-medium mb-3">
+        ${tabLabels[this.activeStep]}
+      </h3>
 
       <form name="newJobConfig" @submit=${this.onSubmit}>
         <btrix-tab-list
-          activePanel="newJobConfig-${this.stateValue}"
-          progressPanel="newJobConfig-${this.stateContext.currentProgress}"
+          activePanel="newJobConfig-${this.activeStep}"
+          progressPanel="newJobConfig-${this.currentProgressStep}"
         >
-          ${this.renderNavItem("CRAWLER_SETUP", msg("Crawler Setup"))}
-          ${this.renderNavItem("CRAWL_BEHAVIORS", msg("Crawl Behaviors"))}
-          ${this.renderNavItem("JOB_SCHEDULING", msg("Job Scheduling"))}
-          ${this.renderNavItem("JOB_INFORMATION", msg("Job Information"))}
+          ${orderedTabNames.map((tabName) =>
+            this.renderNavItem(tabName, tabLabels[tabName])
+          )}
 
           <btrix-tab-panel name="newJobConfig-crawlerSetup">
             <div
-              class="${contentClassName}${this.stateContext.jobType === "seeded"
+              class="${contentClassName}${this.jobType === "seeded"
                 ? " hidden"
                 : ""}"
             >
               ${this.renderUrlListSetup(formColClassName)}
             </div>
             <div
-              class="${contentClassName}${this.stateContext.jobType ===
-              "urlList"
+              class="${contentClassName}${this.jobType === "urlList"
                 ? " hidden"
                 : ""}"
             >
@@ -358,15 +132,7 @@ export class NewJobConfig extends LiteElement {
             <div class=${contentClassName}>
               ${this.renderJobInformation(formColClassName)}
             </div>
-            <div class="p-4 border-t flex justify-between">
-              <sl-button size="small" @click=${() => this.stateSend("BACK")}>
-                <sl-icon slot="prefix" name="arrow-left"></sl-icon>
-                ${msg("Previous Step")}
-              </sl-button>
-              <sl-button type="submit" size="small" variant="primary">
-                ${msg("Save")}
-              </sl-button>
-            </div>
+            ${this.renderFooter(true)}
           </btrix-tab-panel>
         </btrix-tab-list>
       </form>
@@ -382,12 +148,7 @@ export class NewJobConfig extends LiteElement {
         <div
           role="button"
           class="block"
-          @click=${() => {
-            this.stateSend({
-              type: "CRAWLER_SETUP",
-              jobType: "urlList",
-            });
-          }}
+          @click=${() => this.selectJobType("urlList")}
         >
           <figure class="w-64 m-4">
             <img src=${urlListSvg} />
@@ -404,12 +165,7 @@ export class NewJobConfig extends LiteElement {
         <div
           role="button"
           class="block"
-          @click=${() => {
-            this.stateSend({
-              type: "CRAWLER_SETUP",
-              jobType: "seeded",
-            });
-          }}
+          @click=${() => this.selectJobType("seeded")}
         >
           <figure class="w-64 m-4">
             <img src=${seededCrawlSvg} />
@@ -427,13 +183,9 @@ export class NewJobConfig extends LiteElement {
     `;
   }
 
-  private renderNavItem(
-    eventName: StepEventName,
-    content: TemplateResult | string
-  ) {
-    const stateValue = tabEventTarget[eventName];
-    const isActive = stateValue === this.stateValue;
-    const { error: isInvalid, completed } = this.stateContext.steps[stateValue];
+  private renderNavItem(tabName: TabName, content: TemplateResult | string) {
+    const isActive = tabName === this.activeStep;
+    const { error: isInvalid, completed } = this.tabs[tabName];
     let icon = html`
       <sl-icon
         name="circle"
@@ -467,13 +219,9 @@ export class NewJobConfig extends LiteElement {
     return html`
       <btrix-tab
         slot="nav"
-        name="newJobConfig-${stateValue}"
-        ?disabled=${!this.stateContext.steps[stateValue].enabled}
-        @click=${(e: MouseEvent) => {
-          if (!(e.target as Tab).disabled && !(e.target as Tab).active) {
-            this.stateSend(eventName);
-          }
-        }}
+        name="newJobConfig-${tabName}"
+        ?disabled=${!this.tabs[tabName].enabled}
+        @click=${this.tabClickHandler(tabName)}
       >
         ${icon}
         <span class="inline-block align-middle">${content}</span>
@@ -481,21 +229,25 @@ export class NewJobConfig extends LiteElement {
     `;
   }
 
-  private renderFooter() {
+  private renderFooter(isLastStep = false) {
     return html`
       <div class="px-5 py-4 border-t flex justify-between">
-        <sl-button size="small" @click=${() => this.stateSend("BACK")}>
+        <sl-button size="small" @click=${this.backStep}>
           <sl-icon slot="prefix" name="arrow-left"></sl-icon>
           ${msg("Previous Step")}
         </sl-button>
-        <sl-button
-          size="small"
-          variant="primary"
-          @click=${() => this.stateSend("CONTINUE")}
-        >
-          <sl-icon slot="suffix" name="arrow-right"></sl-icon>
-          ${msg("Next Step")}
-        </sl-button>
+        ${isLastStep
+          ? html`<sl-button type="submit" size="small" variant="primary">
+              ${msg("Save")}
+            </sl-button>`
+          : html`<sl-button
+              size="small"
+              variant="primary"
+              @click=${this.nextStep}
+            >
+              <sl-icon slot="suffix" name="arrow-right"></sl-icon>
+              ${msg("Next Step")}
+            </sl-button>`}
       </div>
     `;
   }
@@ -519,7 +271,7 @@ export class NewJobConfig extends LiteElement {
           label=${msg("List of URLs")}
           rows="6"
           autocomplete="off"
-          ?required=${this.stateContext.jobType === "urlList"}
+          ?required=${this.jobType === "urlList"}
           @sl-change=${this.onFieldChange}
         ></sl-textarea>
       </div>
@@ -560,40 +312,64 @@ export class NewJobConfig extends LiteElement {
     const el = e.target as HTMLInputElement & {
       invalid: boolean;
     };
+    const currentTab = this.activeStep as TabName;
     if (el.invalid) {
-      this.stateSend({
-        type: "VALIDITY_CHANGE",
-        valid: false,
-      });
-    } else if (this.stateContext.steps[this.stateValue].error) {
+      this.tabs[currentTab].error = true;
+    } else if (this.tabs[currentTab].error) {
       const hasInvalid = el
         .closest("btrix-tab-panel")
         ?.querySelector("[data-invalid]");
       if (!hasInvalid) {
-        this.stateSend({
-          type: "VALIDITY_CHANGE",
-          valid: true,
-        });
+        this.tabs[currentTab].error = false;
       }
     }
   };
 
-  private stateSend(
-    event:
-      | StepEventName
-      | JobSelectEvent
-      | ValidityChangeEvent
-      | "BACK"
-      | "CONTINUE"
-  ) {
-    this.stateService.send(event as any);
+  private tabClickHandler = (step: StepName) => (e: MouseEvent) => {
+    const tab = e.currentTarget as Tab;
+    if (tab.disabled || tab.active) {
+      e.preventDefault();
+      e.stopPropagation();
+      return;
+    }
+
+    this.activeStep = step;
+  };
+
+  private selectJobType(jobType: JobType) {
+    this.jobType = jobType;
+    this.activeStep = "crawlerSetup";
+    this.currentProgressStep = this.activeStep;
+    this.tabs[this.activeStep].enabled = true;
   }
 
-  private checkValidity = (): boolean => {
+  private backStep() {
+    this.activeStep = stepOrder[stepOrder.indexOf(this.activeStep) - 1];
+  }
+
+  private nextStep() {
+    const isValid = this.checkCurrentPanelValidity();
+
+    if (isValid) {
+      const nextStep = stepOrder[
+        stepOrder.indexOf(this.activeStep) + 1
+      ] as TabName;
+      const isFirstEnabled = !this.tabs[nextStep].enabled;
+
+      if (isFirstEnabled) {
+        this.tabs[nextStep].enabled = true;
+        this.currentProgressStep = nextStep;
+      }
+      this.tabs[this.activeStep as TabName].completed = true;
+      this.activeStep = nextStep;
+    }
+  }
+
+  private checkCurrentPanelValidity = (): boolean => {
     if (!this.formElem) return false;
 
     const activePanel = this.formElem.querySelector(
-      `btrix-tab-panel[name="newJobConfig-${this.stateValue}"]`
+      `btrix-tab-panel[name="newJobConfig-${this.activeStep}"]`
     );
     const invalidElems = [...activePanel!.querySelectorAll("[data-invalid]")];
 
