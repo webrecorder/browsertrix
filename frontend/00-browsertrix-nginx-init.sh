307d9cac0490c35c33f752f8b9083d83d0e14852
Ilya Kreymer <ikreymer@gmail.com>
2022-12-02 11:24:51 -0800

    fix download paths: - don't include bucket path after /data/, proxy to bucket in nginx - correctly set access endpoint for non-minio configs

--- frontend/00-browsertrix-nginx-init.sh
+++ frontend/00-browsertrix-nginx-init.sh
@@ -7,8 +7,9 @@ if [ -z "$LOCAL_MINIO_HOST" ]; then
   echo "no local minio, clearing out minio route"
   echo "" > /etc/nginx/includes/minio.conf
 else
-  echo "local minio: replacing \$LOCAL_MINIO_HOST with $LOCAL_MINIO_HOST"
+  echo "local minio: replacing \$LOCAL_MINIO_HOST with \"$LOCAL_MINIO_HOST\", \$LOCAL_BUCKET with \"$LOCAL_BUCKET\""
   sed -i "s/\$LOCAL_MINIO_HOST/$LOCAL_MINIO_HOST/g" /etc/nginx/includes/minio.conf
+  sed -i "s/\$LOCAL_BUCKET/$LOCAL_BUCKET/g" /etc/nginx/includes/minio.conf
 fi
 
 mkdir -p /etc/nginx/resolvers/
