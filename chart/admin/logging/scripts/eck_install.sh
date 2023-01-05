539a4556aae34588459d2db02187d7568d12b83b
DongWoo Lee <leepro@gmail.com>
2023-01-04 22:52:41 -0800

    add logging service CI with k3d

--- chart/admin/logging/scripts/eck_install.sh
+++ chart/admin/logging/scripts/eck_install.sh
@@ -4,5 +4,6 @@ kubectl create namespace btrix-admin
 kubectl create -f https://download.elastic.co/downloads/eck/2.5.0/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/2.5.0/operator.yaml
 
-kubectl label nodes docker-desktop nodeType=admin
+# kubectl label nodes docker-desktop nodeType=admin
+kubectl get nodes
 kubectl get nodes -o wide -o jsonpath='{.items[*].metadata.labels}' | jq .
