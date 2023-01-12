e655aebd204bf6bdeeb2f7594b9e8577439c65b5
Tessa Walsh <tessa@bitarchivist.net>
2023-01-12 18:22:50 -0500

    Add second fallback to account for all code paths

--- backend/btrixcloud/invites.py
+++ backend/btrixcloud/invites.py
@@ -77,6 +77,11 @@ class InviteOps:
                 status_code=403, detail="This user has already been invited"
             )
 
+        if not archive_name or not new_user_invite.aid:
+            default_org = await self.archives.find_one({"default": True})
+            new_user_invite.aid = default_org["_id"]
+            archive_name = default_org["name"]
+
         await self.invites.insert_one(new_user_invite.to_dict())
 
         self.email.send_new_user_invite(
@@ -126,8 +131,6 @@ class InviteOps:
         if allow_existing is false, don't allow invites to existing users"""
         invite_code = uuid.uuid4().hex
 
-        aid = None
-        archive_name = None
         if archive:
             aid = archive.id
             archive_name = archive.name
