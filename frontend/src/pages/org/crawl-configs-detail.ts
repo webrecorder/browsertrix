5ef543361e907eb5d4ca3ae787a975712c188a60
Henry Wilkinson <henry@wilkinson.graphics>
2023-02-06 18:40:42 -0500

    Shrinks action button size on config detail page
    Matches crawl detail page

--- frontend/src/pages/org/crawl-configs-detail.ts
+++ frontend/src/pages/org/crawl-configs-detail.ts
@@ -117,6 +117,7 @@ export class CrawlTemplatesDetail extends LiteElement {
                     this.crawlConfig!.id
                   }?edit`}
                   variant="primary"
+                  size="small"
                   class="mr-2"
                   @click=${this.navLink}
                 >
@@ -298,7 +299,7 @@ export class CrawlTemplatesDetail extends LiteElement {
 
     return html`
       <sl-dropdown placement="bottom-end" distance="4">
-        <sl-button slot="trigger" caret>${msg("Actions")}</sl-button>
+        <sl-button slot="trigger" size="small" caret>${msg("Actions")}</sl-button>
 
         <ul
           class="text-left text-sm text-neutral-800 bg-white whitespace-nowrap"
