659778ea8f60b277bc2f8249113da536847a8176
sua yoo <sua@suayoo.com>
2023-02-13 16:00:10 -0800

    remove localstorage key

--- frontend/src/utils/AuthService.ts
+++ frontend/src/utils/AuthService.ts
@@ -171,13 +171,6 @@ export default class AuthService {
       }
     );
 
-    // NOTE beforeunload being called isn't guaranteed, which
-    // may lead to subtle bugs where the value exists in
-    // localstorage but the app is not actually authenticated.
-    window.addEventListener("beforeunload", () => {
-      window.localStorage.removeItem(AuthService.storageKey);
-    });
-
     return authState;
   }
 
@@ -195,33 +188,26 @@ export default class AuthService {
    * Retrieve shared session from another tab/window
    **/
   private static async getSharedSessionAuth(): Promise<AuthState> {
-    let timeoutPromise;
     const broadcastPromise = new Promise((resolve) => {
       // Check if there's any authenticated tabs
-      const value = window.localStorage.getItem(AuthService.storageKey);
-      if (value && (JSON.parse(value) as HasAuthStorageData).auth) {
-        // Ask for auth
-        AuthService.broadcastChannel.postMessage(<AuthRequestEventData>{
-          name: "requesting_auth",
-        });
-        // Wait for another tab to respond
-        const cb = ({ data }: any) => {
-          if (data.name === "responding_auth") {
-            AuthService.broadcastChannel.removeEventListener("message", cb);
-            resolve(data.auth);
-          }
-        };
-        AuthService.broadcastChannel.addEventListener("message", cb);
-        // Ensure that `getSharedSessionAuth` is resolved within a reasonable
-        // timeframe, even if another window/tab doesn't respond:
-        timeoutPromise = new Promise((resolve) => {
-          window.setTimeout(() => {
-            resolve(null);
-          }, 10);
-        });
-      } else {
+      AuthService.broadcastChannel.postMessage(<AuthRequestEventData>{
+        name: "requesting_auth",
+      });
+      // Wait for another tab to respond
+      const cb = ({ data }: any) => {
+        if (data.name === "responding_auth") {
+          AuthService.broadcastChannel.removeEventListener("message", cb);
+          resolve(data.auth);
+        }
+      };
+      AuthService.broadcastChannel.addEventListener("message", cb);
+    });
+    // Ensure that `getSharedSessionAuth` is resolved within a reasonable
+    // timeframe, even if another window/tab doesn't respond:
+    const timeoutPromise = new Promise((resolve) => {
+      window.setTimeout(() => {
         resolve(null);
-      }
+      }, 10);
     });
 
     return Promise.race([broadcastPromise, timeoutPromise]).then(
@@ -254,16 +240,11 @@ export default class AuthService {
   }
 
   saveLogin(auth: Auth) {
-    window.localStorage.setItem(
-      AuthService.storageKey,
-      JSON.stringify(<HasAuthStorageData>{ auth: true })
-    );
     this.persist(auth);
     this.startFreshnessCheck();
   }
 
   logout() {
-    window.localStorage.removeItem(AuthService.storageKey);
     this.cancelFreshnessCheck();
     this.revoke();
   }
