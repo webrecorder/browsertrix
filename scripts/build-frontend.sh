40129fac7b4a7ba50e26e51882fb6932902261c5
DongWoo <leepro@gmail.com>
2023-02-13 18:02:56 -0800

    remove tagging

--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -4,7 +4,5 @@ CURR=$(dirname "${BASH_SOURCE[0]}")
 docker build --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" --build-arg GIT_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)" --build-arg --load -t ${REGISTRY}webrecorder/browsertrix-frontend:latest  $CURR/../frontend/
 
 if [ -n "$REGISTRY" ]; then
-    TAG=`docker images | grep ${REGISTRY}webrecorder/browsertrix-frontend | awk '{ print $3 }'`
-    docker tag $TAG ${REGISTRY}webrecorder/browsertrix-frontend:latest
     docker push ${REGISTRY}webrecorder/browsertrix-frontend
 fi
