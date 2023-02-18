c0ca933625260e7791dc59035df62c710993c006
Henry Wilkinson <henry@wilkinson.graphics>
2023-02-17 22:07:59 -0500

    Puts the dropdowns in a flexbox
    Wow terrible git dif because I added an indent, rad.
    
    This replaces the grid settings with a flexbox for the dropdowns so they are aligned to the top of the box even when the dumb multi-select gets bigger due to simply not having enough space at the smallest breakpoint.  Is how it is, should be an edge case. :\

--- frontend/src/pages/org/crawls-list.ts
+++ frontend/src/pages/org/crawls-list.ts
@@ -274,79 +274,81 @@ export class CrawlsList extends LiteElement {
           </sl-input>
         </div>
 
-        <div class="flex-column md:flex items-center row-start-2 md:row-start-1 self-start">
-          <div class="text-neutral-500 mr-2 ml-2">${msg("View:")}</div>
-          <sl-select
-            class="flex-1 md:w-[14.5rem]"
-            placement="bottom-end"
-            distance="4"
-            size="small"
-            pill
-            .value=${this.filterByState}
-            multiple
-            max-tags-visible="1"
-            placeholder=${msg("All Crawls")}
-            @sl-change=${(e: CustomEvent) => {
-              const value = (e.target as SlSelect).value as CrawlState[];
-              this.filterByState = value;
-            }}
-          >
-            ${activeCrawlStates.map(
-              (state) => html`
-                <sl-menu-item value=${state}>
-                  ${crawlState[state].label}</sl-menu-item
-                >
-              `
-            )}
-            <sl-divider></sl-divider>
-            ${inactiveCrawlStates.map(
-              (state) => html`
-                <sl-menu-item value=${state}>
-                  ${crawlState[state].label}</sl-menu-item
-                >
-              `
-            )}
-          </sl-select>
-        </div>
-
-        <div class="flex-column md:flex items-center row-start-2 md:row-start-1 self-start">
-          <div class="whitespace-nowrap text-neutral-500 mr-2 ml-2">
-            ${msg("Sort by:")}
+        <div class="flex row-start-2 md:row-start-1 col-span-2 md:col-auto">
+          <div class="flex-column grow md:flex items-center mr-2">
+            <div class="text-neutral-500 mr-2 ml-2">${msg("View:")}</div>
+            <sl-select
+              class="flex-1 md:w-[14.5rem]"
+              placement="bottom-end"
+              distance="4"
+              size="small"
+              pill
+              .value=${this.filterByState}
+              multiple
+              max-tags-visible="1"
+              placeholder=${msg("All Crawls")}
+              @sl-change=${(e: CustomEvent) => {
+                const value = (e.target as SlSelect).value as CrawlState[];
+                this.filterByState = value;
+              }}
+            >
+              ${activeCrawlStates.map(
+                (state) => html`
+                  <sl-menu-item value=${state}>
+                    ${crawlState[state].label}</sl-menu-item
+                  >
+                `
+              )}
+              <sl-divider></sl-divider>
+              ${inactiveCrawlStates.map(
+                (state) => html`
+                  <sl-menu-item value=${state}>
+                    ${crawlState[state].label}</sl-menu-item
+                  >
+                `
+              )}
+            </sl-select>
           </div>
-          <div class="flex">
-          <sl-select
-            class="flex-1 md:w-[9.2rem]"
-            placement="bottom-end"
-            distance="4"
-            size="small"
-            pill
-            value=${this.orderBy.field}
-            @sl-select=${(e: any) => {
-              const field = e.detail.item.value as SortField;
-              this.orderBy = {
-                field: field,
-                direction:
-                  sortableFields[field].defaultDirection ||
-                  this.orderBy.direction,
-              };
-            }}
-          >
-            ${Object.entries(sortableFields).map(
-              ([value, { label }]) => html`
-                <sl-menu-item value=${value}>${label}</sl-menu-item>
-              `
-            )}
-          </sl-select>
-          <sl-icon-button
-            name="arrow-down-up"
-            label=${msg("Reverse sort")}
-            @click=${() => {
-              this.orderBy = {
-                ...this.orderBy,
-                direction: this.orderBy.direction === "asc" ? "desc" : "asc",
-              };
-            }}
-          ></sl-icon-button>
+
+          <div class="flex-column grow md:flex items-center">
+            <div class="whitespace-nowrap text-neutral-500 mr-2 ml-2">
+              ${msg("Sort by:")}
+            </div>
+            <div class="flex">
+            <sl-select
+              class="flex-1 md:w-[9.2rem]"
+              placement="bottom-end"
+              distance="4"
+              size="small"
+              pill
+              value=${this.orderBy.field}
+              @sl-select=${(e: any) => {
+                const field = e.detail.item.value as SortField;
+                this.orderBy = {
+                  field: field,
+                  direction:
+                    sortableFields[field].defaultDirection ||
+                    this.orderBy.direction,
+                };
+              }}
+            >
+              ${Object.entries(sortableFields).map(
+                ([value, { label }]) => html`
+                  <sl-menu-item value=${value}>${label}</sl-menu-item>
+                `
+              )}
+            </sl-select>
+            <sl-icon-button
+              name="arrow-down-up"
+              label=${msg("Reverse sort")}
+              @click=${() => {
+                this.orderBy = {
+                  ...this.orderBy,
+                  direction: this.orderBy.direction === "asc" ? "desc" : "asc",
+                };
+              }}
+            ></sl-icon-button>
+            </div>
           </div>
         </div>
       </div>
