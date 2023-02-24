d0abf2b3248362b6a8197ec155777637e25bb57c
Henry Wilkinson <henry@wilkinson.graphics>
2023-02-24 00:10:16 -0500

    Fix crawl config grid on sm
    - Improves grid breakpoints on md

--- frontend/src/pages/org/crawl-configs-list.ts
+++ frontend/src/pages/org/crawl-configs-list.ts
@@ -302,7 +302,7 @@ export class CrawlTemplatesList extends LiteElement {
     }
 
     return html`
-      <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         ${flow(...flowFns)(this.crawlTemplates)}
       </div>
     `;
