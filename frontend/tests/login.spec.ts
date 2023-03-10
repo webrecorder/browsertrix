cfa180b207d5e408ed2685d590965ecbf740d3be
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2023-03-10 14:38:18 +0000

    Update login.spec.ts

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -2,7 +2,7 @@ import { chromium } from 'playwright';
 import { test, expect } from '@playwright/test';
 
 test('test', async ({}) => {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
 
