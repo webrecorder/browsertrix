373577f494f3d1e821d30bd695f437fc780f40b8
Ilya Kreymer <ikreymer@gmail.com>
2023-04-21 17:39:04 -0700

    simplify migration to just use update_crawl_config for schedules as well

--- backend/btrixcloud/migrations/migration_0005_operator_scheduled_jobs.py
+++ backend/btrixcloud/migrations/migration_0005_operator_scheduled_jobs.py
@@ -1,11 +1,8 @@
 """
 Migration 0005 - Updating scheduled cron jobs after Operator changes
 """
-import os
-
 from btrixcloud.crawlconfigs import CrawlConfig, UpdateCrawlConfig
 from btrixcloud.crawlmanager import CrawlManager
-from btrixcloud.k8sapi import K8sAPI
 from btrixcloud.migrations import BaseMigration
 
 
@@ -33,48 +30,35 @@ class Migration(BaseMigration):
         crawl_manager = CrawlManager()
 
         # Update configmap for crawl configs that have non-zero timeout or scale > 1
-        match_query = {"$or": [{"crawlTimeout": {"$gt": 0}}, {"scale": {"$gt": 1}}]}
+        match_query = {
+            "$or": [
+                {"crawlTimeout": {"$gt": 0}},
+                {"scale": {"$gt": 1}},
+                {"schedule": {"$nin": ["", None]}},
+            ]
+        }
         configs_to_update = [res async for res in crawl_configs.find(match_query)]
         for config_dict in configs_to_update:
             config = CrawlConfig.from_dict(config_dict)
-            crawl_manager.update_crawl_config(
+            print(
+                f"Updating Crawl Config {config.id}: schedule: {config.schedule}, "
+                + "timeout: {config.crawlTimeout}, scale: {config.scale}"
+            )
+            await crawl_manager.update_crawl_config(
                 config,
-                UpdateCrawlConfig(scale=config.scale, crawlTimeout=config.crawlTimeout),
+                UpdateCrawlConfig(
+                    scale=config.scale,
+                    crawlTimeout=config.crawlTimeout,
+                    schedule=config.schedule,
+                ),
             )
-
-        # Update scheduled jobs
-        scheduled_workflows = [
-            res async for res in crawl_configs.find({"schedule": {"$nin": ["", None]}})
-        ]
-        if not scheduled_workflows:
-            return
-
-        # Create new crawl cron jobs
-        for config_dict in scheduled_workflows:
-            cid = str(config_dict["_id"])
-            cron_job_id = f"sched-{cid[:12]}"
-            params = {
-                "id": cron_job_id,
-                "cid": cid,
-                "image": crawl_manager.job_image,
-                "image_pull_policy": crawl_manager.job_image_pull_policy,
-                "schedule": config_dict["schedule"],
-            }
-            data = crawl_manager.templates.env.get_template(
-                "crawl_cron_job.yaml"
-            ).render(params)
-            k8s_objects = await crawl_manager.create_from_yaml(
-                data, crawl_manager.cron_namespace
-            )
-            assert len(k8s_objects) == 1
 
         # Delete existing scheduled jobs from crawler namespace
-        k8s_api_instance = K8sAPI()
-        crawler_namespace = os.environ.get("CRAWLER_NAMESPACE") or "crawlers"
-        await k8s_api_instance.batch_api.delete_collection_namespaced_cron_job(
-            namespace=crawler_namespace
+        print("Deleting cronjobs from crawler namespace")
+        await crawl_manager.batch_api.delete_collection_namespaced_cron_job(
+            namespace=crawl_manager.namespace
         )
-        result = await k8s_api_instance.batch_api.list_namespaced_cron_job(
-            namespace=crawler_namespace
+        result = await crawl_manager.batch_api.list_namespaced_cron_job(
+            namespace=crawl_manager.namespace
         )
         assert len(result.items) == 0
