ebfa1849a0398f81e114a4185801c6ff3f7fab78
sua yoo <sua@suayoo.com>
2023-05-10 16:57:56 -0700

    add tab list

--- frontend/src/pages/org/collections-new.ts
+++ frontend/src/pages/org/collections-new.ts
@@ -5,6 +5,8 @@ import { when } from "lit/directives/when.js";
 import type { AuthState } from "../../utils/AuthService";
 import LiteElement, { html } from "../../utils/LiteElement";
 
+const STEPS = ["crawls", "metadata"] as const;
+type Tab = (typeof STEPS)[number];
 type Collection = any; // TODO
 
 @localized()
@@ -14,6 +16,14 @@ export class CollectionsNew extends LiteElement {
 
   @property({ type: String })
   orgId!: string;
+
+  @state()
+  private activeTab: Tab = STEPS[0];
+
+  private readonly tabLabels: Record<Tab, string> = {
+    crawls: msg("Select Crawls"),
+    metadata: msg("Metadata"),
+  };
 
   protected async willUpdate(changedProperties: Map<string, any>) {}
 
@@ -39,7 +49,59 @@ export class CollectionsNew extends LiteElement {
   `;
 
   private renderEditor() {
-    return html`TODO`;
+    return html`<btrix-tab-list
+      activePanel="newCollection-${this.activeTab}"
+      progressPanel="newCollection-${this.activeTab}"
+    >
+      <h3 slot="header" class="font-semibold">
+        ${this.tabLabels[this.activeTab]}
+      </h3>
+
+      ${STEPS.map(this.renderTab)}
+
+      <btrix-tab-panel name="newCollection-crawls">
+        TODO crawls
+      </btrix-tab-panel>
+      <btrix-tab-panel name="newCollection-metadata">
+        TODO metadata
+      </btrix-tab-panel>
+    </btrix-tab-list>`;
   }
+
+  private renderTab = (tab: Tab) => {
+    const isActive = tab === this.activeTab;
+    const completed = false; // TODO
+    const iconProps = {
+      name: "circle",
+      library: "default",
+      class: "text-neutral-400",
+    };
+    if (isActive) {
+      iconProps.name = "pencil-circle-dashed";
+      iconProps.library = "app";
+      iconProps.class = "text-base";
+    } else if (completed) {
+      iconProps.name = "check-circle";
+    }
+    return html`
+      <btrix-tab
+        slot="nav"
+        name="newCollection-${tab}"
+        class="whitespace-nowrap"
+        @click=${() => {
+          this.activeTab = tab;
+        }}
+      >
+        <sl-icon
+          name=${iconProps.name}
+          library=${iconProps.library}
+          class="inline-block align-middle mr-1 text-base ${iconProps.class}"
+        ></sl-icon>
+        <span class="inline-block align-middle whitespace-normal">
+          ${this.tabLabels[tab]}
+        </span>
+      </btrix-tab>
+    `;
+  };
 }
 customElements.define("btrix-collections-new", CollectionsNew);
