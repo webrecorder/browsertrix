dd1374256724c87ffbc2a4847dc8666f609065cd
sua yoo <sua@suayoo.com>
2023-05-16 12:36:47 -0700

    enable adding collection

--- frontend/src/pages/org/collections-new.ts
+++ frontend/src/pages/org/collections-new.ts
@@ -3,7 +3,7 @@ import { state, property } from "lit/decorators.js";
 import { msg, localized, str } from "@lit/localize";
 import { when } from "lit/directives/when.js";
 import { mergeDeep } from "immutable";
-import type { SlTextarea, SlCheckbox } from "@shoelace-style/shoelace";
+import type { SlTextarea, SlCheckbox, SlInput } from "@shoelace-style/shoelace";
 
 import type { AuthState } from "../../utils/AuthService";
 import LiteElement, { html } from "../../utils/LiteElement";
@@ -14,7 +14,7 @@ const TABS = ["crawls", "metadata"] as const;
 type Tab = (typeof TABS)[number];
 type Collection = {
   name: string;
-  description: string;
+  description: string | null;
   crawlIds: string[];
 };
 type FormState = {
@@ -46,6 +46,12 @@ export class CollectionsNew extends LiteElement {
     description: "",
     workflows: [],
   };
+
+  @state()
+  private isSubmitting = false;
+
+  @state()
+  private serverError?: string;
 
   @state()
   private isPreviewingDescription = true;
@@ -91,23 +97,25 @@ export class CollectionsNew extends LiteElement {
   `;
 
   private renderEditor() {
-    return html`<btrix-tab-list
-      activePanel="newCollection-${this.activeTab}"
-      progressPanel="newCollection-${this.activeTab}"
-    >
-      <h3 slot="header" class="font-semibold">
-        ${this.tabLabels[this.activeTab]}
-      </h3>
+    return html`<form name="newCollection" @submit=${this.onSubmit}>
+      <btrix-tab-list
+        activePanel="newCollection-${this.activeTab}"
+        progressPanel="newCollection-${this.activeTab}"
+      >
+        <h3 slot="header" class="font-semibold">
+          ${this.tabLabels[this.activeTab]}
+        </h3>
 
-      ${TABS.map(this.renderTab)}
+        ${TABS.map(this.renderTab)}
 
-      <btrix-tab-panel name="newCollection-crawls">
-        ${this.renderCrawls()}
-      </btrix-tab-panel>
-      <btrix-tab-panel name="newCollection-metadata">
-        ${this.renderMetadata()}
-      </btrix-tab-panel>
-    </btrix-tab-list>`;
+        <btrix-tab-panel name="newCollection-crawls">
+          ${this.renderCrawls()}
+        </btrix-tab-panel>
+        <btrix-tab-panel name="newCollection-metadata">
+          ${this.renderMetadata()}
+        </btrix-tab-panel>
+      </btrix-tab-list>
+    </form>`;
   }
 
   private renderTab = (tab: Tab) => {
@@ -158,7 +166,7 @@ export class CollectionsNew extends LiteElement {
         </section>
         <section class="col-span-1 flex flex-col">
           <h4 class="text-base font-semibold mb-3">
-            ${msg("Finished Crawls")}
+            ${msg("All Crawl Workflows")}
           </h4>
           <div class="border rounded-lg p-6 flex-1">
             ${this.renderCrawlsNotInCollection()}
@@ -167,7 +175,12 @@ export class CollectionsNew extends LiteElement {
         <footer
           class="col-span-2 border rounded-lg px-6 py-4 flex justify-between"
         >
-          <sl-button variant="primary" size="small" class="ml-auto">
+          <sl-button
+            variant="primary"
+            size="small"
+            class="ml-auto"
+            @click=${() => this.goToTab("metadata")}
+          >
             <sl-icon slot="suffix" name="chevron-right"></sl-icon>
             ${msg("Next Step")}
           </sl-button>
@@ -188,22 +201,28 @@ export class CollectionsNew extends LiteElement {
             <div class="p-6 flex-1">
               <sl-input
                 class="mb-4"
-                name="collectionName"
+                name="name"
                 label=${msg("Name")}
                 autocomplete="off"
                 placeholder=${msg("My Collection")}
                 value=${this.formState.name}
                 required
+                @sl-change=${(e: CustomEvent) => {
+                  const inputEl = e.target as SlInput;
+                  this.updateFormState({
+                    [inputEl.name]: inputEl.value,
+                  });
+                }}
               ></sl-input>
               <sl-textarea
-                name="collectionDescription"
+                name="description"
                 value=${this.formState.description}
                 autocomplete="off"
                 rows="10"
                 @sl-input=${(e: Event) => {
                   const inputEl = e.target as SlTextarea;
                   this.updateFormState({
-                    description: inputEl.value,
+                    [inputEl.name]: inputEl.value,
                   });
                 }}
               >
@@ -253,11 +272,17 @@ export class CollectionsNew extends LiteElement {
         </div>
 
         <footer class="border-t px-6 py-4 flex justify-between">
-          <sl-button size="small">
+          <sl-button size="small" @click=${() => this.goToTab("crawls")}>
             <sl-icon slot="prefix" name="chevron-left"></sl-icon>
             ${msg("Previous Step")}
           </sl-button>
-          <sl-button variant="primary" size="small">
+          <sl-button
+            type="submit"
+            size="small"
+            variant="primary"
+            ?disabled=${this.isSubmitting}
+            ?loading=${this.isSubmitting}
+          >
             ${msg("Save New Collection")}
           </sl-button>
         </footer>
@@ -308,6 +333,56 @@ export class CollectionsNew extends LiteElement {
 
   private updateFormState(nextState: Partial<FormState>) {
     this.formState = mergeDeep(this.formState, nextState);
+  }
+
+  private async onSubmit(event: SubmitEvent) {
+    event.preventDefault();
+    await this.updateComplete;
+
+    const form = event.target as HTMLFormElement;
+    if (form.querySelector("[data-invalid]")) {
+      return;
+    }
+
+    const params: Collection = {
+      name: this.formState.name,
+      description: this.formState.description,
+      crawlIds: [],
+    };
+    this.isSubmitting = true;
+    console.log("submit", params);
+
+    try {
+      const data = await this.apiFetch(
+        `/orgs/${this.orgId}/collections/`,
+        this.authState!,
+        {
+          method: "POST",
+          body: JSON.stringify(params),
+        }
+      );
+
+      console.log(data.added);
+
+      this.notify({
+        message: msg("Successfully created new Collection."),
+        variant: "success",
+        icon: "check2-circle",
+        duration: 8000,
+      });
+
+      this.navTo(`/orgs/${this.orgId}/collections`);
+    } catch (e: any) {
+      if (e?.isApiError) {
+        this.serverError = e?.message;
+      } else {
+        this.serverError = msg("Something unexpected went wrong");
+      }
+
+      console.log(this.serverError);
+    }
+
+    this.isSubmitting = false;
   }
 }
 customElements.define("btrix-collections-new", CollectionsNew);
