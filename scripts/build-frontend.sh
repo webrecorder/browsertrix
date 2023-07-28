2f8a13aa7da80bfcb06f288fc21ee8f5e9b95ef9
Anish Lakhwara <anish+git@lakhwara.com>
2023-07-27 20:25:14 -0700

    fix(build): use `/usr/bin/env bash` instead of `/bin/bash`

--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 CURR=$(dirname "${BASH_SOURCE[0]}")
 
 docker build --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" --build-arg GIT_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)" --build-arg --load -t ${REGISTRY}webrecorder/browsertrix-frontend:latest  $CURR/../frontend/
