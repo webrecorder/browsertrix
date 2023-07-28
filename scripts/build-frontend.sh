85584fa69cc642a027ff30aec5113c4a6804b475
Anish Lakhwara <anish+git@lakhwara.com>
2023-07-27 20:52:14 -0700

    fix(build): build frontend was missing required env vars

--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CURR=$(dirname "${BASH_SOURCE[0]}")
 
-docker build --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" --build-arg GIT_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)" --build-arg --load -t ${REGISTRY}webrecorder/browsertrix-frontend:latest  $CURR/../frontend/
+DOCKER_BUILDKIT=1 docker build --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" --build-arg GIT_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)" --build-arg --load -t ${REGISTRY}webrecorder/browsertrix-frontend:latest  $CURR/../frontend/
 
 if [ -n "$REGISTRY" ]; then
     docker push ${REGISTRY}webrecorder/browsertrix-frontend
