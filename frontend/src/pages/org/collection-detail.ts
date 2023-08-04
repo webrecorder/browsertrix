14a5be72c16f4fb4748485a0e5009c2468bbbbd2
Henry Wilkinson <henry@wilkinson.graphics>
2023-08-03 21:40:01 -0400

    Change the menu option to use eye-slash (unfilled)
    Line icons used for actions throughout the app

--- frontend/src/pages/org/collection-detail.ts
+++ frontend/src/pages/org/collection-detail.ts
@@ -320,7 +320,7 @@ export class CollectionDetail extends LiteElement {
                   </a>
                 </sl-menu-item>
                 <sl-menu-item @click=${() => this.onTogglePublic(false)}>
-                  <sl-icon name="eye-slash-fill" slot="prefix"></sl-icon>
+                  <sl-icon name="eye-slash" slot="prefix"></sl-icon>
                   ${msg("Make Private")}
                 </sl-menu-item>
               `}
