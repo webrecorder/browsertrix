1c39bad03887fd853c73015941f34ebcf329d1b7
sua yoo <sua@suayoo.com>
2023-08-08 15:05:40 -0700

    remove ls

--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 CURR=$(dirname "${BASH_SOURCE[0]}")
 
-DOCKER_BUILDKIT=1 docker build --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" --build-arg GIT_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)" --build-arg --load -t ${REGISTRY}webrecorder/browsertrix-frontend:latest  $CURR/../frontend/
+DOCKER_BUILDKIT=1 docker build --progress=plain --no-cache \
+    --build-arg GIT_COMMIT_HASH="$(git rev-parse --short HEAD)" \
+    --build-arg GIT_BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)" \
+    --build-arg --load \
+    -t ${REGISTRY}webrecorder/browsertrix-frontend:latest  $CURR/../frontend/
 
 if [ -n "$REGISTRY" ]; then
     docker push ${REGISTRY}webrecorder/browsertrix-frontend
