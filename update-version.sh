a9ab17fc61e24450bce7adc9082a94eb87bc6270
Ilya Kreymer <ikreymer@users.noreply.github.com>
2023-08-30 12:02:02 -0700

    publish helm chart on release (fixes #1114) (#1117) (#1123)
    - no longer using :latest by default in values.yaml, instead updating version with each release
    - set chart version to match app version in Chart.yaml
    - update version in helm chart and values.yaml as part of update-version.sh script
    - update test.yaml and local-config.yaml to enable using :latest tag images
    - ci: add ci script for packaging current helm chart
    - docs: updates docs to indicate deploying directly from GitHub release
    - docs: add script to fill in latest version for 'VERSION' using custom script
    - chart: set local_service_port to 30870 by default, but use only if no ingress.
    - default values.yaml set up for local deployment, local-config.yaml contains additional commented out examples
    - ci draft: add deployment info to draft with helm install command for current version
    - test: fix password check test

--- update-version.sh
+++ update-version.sh
@@ -6,3 +6,8 @@ mv ./tmp-package.json ./frontend/package.json
 
 echo '""" current version """' > ./backend/btrixcloud/version.py
 echo "__version__ = \"$version\"" >> ./backend/btrixcloud/version.py
+
+sed -E -i "" "s/^version:.*$/version: $version/" chart/Chart.yaml
+
+sed -E -i "" "s/\/browsertrix-backend:[[:alnum:].-]+/\/browsertrix-backend:$version/" chart/values.yaml
+sed -E -i "" "s/\/browsertrix-frontend:[[:alnum:].-]+/\/browsertrix-frontend:$version/" chart/values.yaml
