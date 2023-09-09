44b690c9a5642138c745d69072be50990c6e27fe
Ilya Kreymer <ikreymer@gmail.com>
2023-09-08 20:30:47 -0700

    tweak tests

--- backend/btrixcloud/k8sapi.py
+++ backend/btrixcloud/k8sapi.py
@@ -207,15 +207,11 @@ class K8sAPI:
             else:
                 del self.api_client.default_headers["Content-Type"]
 
-    async def print_pod_logs(self, pod_names, container, lines=100, previous=False):
+    async def print_pod_logs(self, pod_names, container, lines=100):
         """print pod logs"""
         for pod in pod_names:
             resp = await self.core_api.read_namespaced_pod_log(
-                pod,
-                self.namespace,
-                container=container,
-                tail_lines=lines,
-                previous=previous,
+                pod, self.namespace, container=container, tail_lines=lines
             )
             print(f"============== LOGS FOR POD: {pod} ==============")
             print(resp)
