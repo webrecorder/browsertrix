1ecbb12943a39d7e1e2bf5c9a8908d1d8acc4518
Henry Wilkinson <henry@wilkinson.graphics>
2023-09-14 12:15:28 -0400

    Reorders actions, adds tooltip
    - All copy buttons on the collection share dialog are now on the right side
    - Adds a tooltip to tell the user the button opens the link in a new tab

--- frontend/src/pages/org/collection-detail.ts
+++ frontend/src/pages/org/collection-detail.ts
@@ -218,19 +218,21 @@ export class CollectionDetail extends LiteElement {
         </p>
         <div class="flex items-center rounded border">
           <div class="text-base">
+            <sl-tooltip content="Open in New Tab">
+              <sl-icon-button
+                href=${publicReplayUrl}
+                name="box-arrow-up-right"
+                target="_blank"
+              >
+              </sl-icon-button>
+            </sl-tooltip>
+          </div>
+          <div class="flex-1 min-w-0 truncate">${publicReplayUrl}</div>
+          <div class="text-base">
             <btrix-copy-button
               .getValue=${() => publicReplayUrl}
               content=${msg("Copy Public URL")}
             ></btrix-copy-button>
-          </div>
-          <div class="flex-1 min-w-0 truncate">${publicReplayUrl}</div>
-          <div class="text-base">
-            <sl-icon-button
-              href=${publicReplayUrl}
-              name="box-arrow-up-right"
-              target="_blank"
-            >
-            </sl-icon-button>
           </div>
         </div>
       </section>
