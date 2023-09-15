fc30a32b2d7f93e3ddec08ea394b178a22e3571d
Ilya Kreymer <ikreymer@gmail.com>
2023-09-15 07:37:31 -0700

    fix tests

--- backend/test_nightly/test_crawl_timeout.py
+++ backend/test_nightly/test_crawl_timeout.py
@@ -24,4 +24,4 @@ def test_crawl_timeout(admin_auth_headers, default_org_id, timeout_crawl):
     )
     assert r.status_code == 200
     data = r.json()
-    assert data["state"] == "partial_complete"
+    assert data["state"] == "stopped"
