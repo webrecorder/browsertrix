c281ac87d1f6edc0de65bc1e15604642805b646d
Ilya Kreymer <ikreymer@gmail.com>
2023-09-15 08:18:57 -0700

    add elseif

--- backend/btrixcloud/operator.py
+++ backend/btrixcloud/operator.py
@@ -822,11 +822,11 @@ class BtrixOperator(K8sAPI):
         # check for other statuses
         else:
             new_status = None
-            if status_count.get("running") > 0:
+            if status_count.get("running"):
                 if status.state in ("generate-wacz", "uploading-wacz", "pending-wacz"):
                     new_status = "running"
 
-            if status_count.get("generate-wacz"):
+            elif status_count.get("generate-wacz"):
                 new_status = "generate-wacz"
             elif status_count.get("uploading-wacz"):
                 new_status = "uploading-wacz"
