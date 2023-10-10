017e51c4f7cde8b523998e68c197c27a925a5cc2
sua yoo <sua@suayoo.com>
2023-10-10 15:52:34 -0700

    update name

--- frontend/src/components/account-settings.ts
+++ frontend/src/components/account-settings.ts
@@ -1,6 +1,7 @@
 import { LitElement } from "lit";
 import { state, query, property } from "lit/decorators.js";
 import { msg, localized } from "@lit/localize";
+import type { SlInput } from "@shoelace-style/shoelace";
 
 import type { CurrentUser } from "../types/user";
 import LiteElement, { html } from "../utils/LiteElement";
@@ -18,6 +19,13 @@ class RequestVerify extends LitElement {
 
   @state()
   private requestSuccess: boolean = false;
+
+  willUpdate(changedProperties: Map<string, any>) {
+    if (changedProperties.has("email")) {
+      this.isRequesting = false;
+      this.requestSuccess = false;
+    }
+  }
 
   createRenderRoot() {
     return this;
@@ -82,6 +90,9 @@ export class AccountSettings extends LiteElement {
   @property({ type: Object })
   userInfo?: CurrentUser;
 
+  @state()
+  sectionSubmitting: null | "name" | "email" | "password" = null;
+
   render() {
     if (!this.userInfo) return;
     return html`
@@ -89,48 +100,61 @@ export class AccountSettings extends LiteElement {
         <h1 class="text-xl font-semibold leading-8 mb-7">
           ${msg("Account Settings")}
         </h1>
-        <form class="border rounded mb-5">
+        <form class="border rounded mb-5" @submit=${this.onSubmitName}>
           <div class="p-4">
-            <h2 class="text-lg font-semibold leading-none mb-3">
+            <h2 class="text-lg font-semibold leading-none mb-4">
               ${msg("Display Name")}
             </h2>
-            <sl-input
-              name="displayName"
-              label=${msg(
+            <p class="mb-2">
+              ${msg(
                 "Enter your full name, or another name to display in the orgs you belong to."
               )}
+            </p>
+            <sl-input
+              name="displayName"
               value=${this.userInfo.name}
               maxlength="40"
               minlength="2"
+              required
+              aria-label=${msg("Display name")}
             ></sl-input>
           </div>
           <footer class="flex items-center justify-end border-t px-4 py-3">
-            <sl-button size="small" variant="primary">${msg("Save")}</sl-button>
+            <sl-button
+              type="submit"
+              size="small"
+              variant="primary"
+              ?loading=${this.sectionSubmitting === "name"}
+              >${msg("Save")}</sl-button
+            >
           </footer>
         </form>
         <form class="border rounded mb-5">
           <div class="p-4">
-            <h2 class="text-lg font-semibold leading-none mb-3">
+            <h2 class="text-lg font-semibold leading-none mb-4">
               ${msg("Email")}
             </h2>
+            <p class="mb-2">${msg("Update the email you use to log in.")}</p>
             <sl-input
               name="email"
-              label=${msg("Update the email you use to log in.")}
               value=${this.userInfo.email}
               type="email"
               minlength="2"
+              aria-label=${msg("Email")}
             ></sl-input>
           </div>
           <footer class="flex items-center justify-between border-t px-4 py-3">
             <btrix-request-verify
               email=${this.userInfo.email}
             ></btrix-request-verify>
-            <sl-button size="small" variant="primary">${msg("Save")}</sl-button>
+            <sl-button type="submit" size="small" variant="primary"
+              >${msg("Save")}</sl-button
+            >
           </footer>
         </form>
         <form class="border rounded mb-5">
           <div class="p-4">
-            <h2 class="text-lg font-semibold leading-none mb-3">
+            <h2 class="text-lg font-semibold leading-none mb-4">
               ${msg("Password")}
             </h2>
             <sl-input
@@ -150,10 +174,54 @@ export class AccountSettings extends LiteElement {
             ></sl-input>
           </div>
           <footer class="flex items-center justify-end border-t px-4 py-3">
-            <sl-button size="small" variant="primary">${msg("Save")}</sl-button>
+            <sl-button type="submit" size="small" variant="primary"
+              >${msg("Save")}</sl-button
+            >
           </footer>
         </form>
       </div>
     `;
+  }
+
+  private async onSubmitName(e: SubmitEvent) {
+    if (!this.userInfo) return;
+    const form = e.target as HTMLFormElement;
+    const input = form.querySelector("sl-input") as SlInput;
+    if (!input.checkValidity()) {
+      return;
+    }
+    e.preventDefault();
+    const newName = input.value.trim();
+
+    if (newName === this.userInfo.name) {
+      return;
+    }
+
+    this.sectionSubmitting = "name";
+
+    try {
+      await this.apiFetch(`/users/me`, this.authState!, {
+        method: "PATCH",
+        body: JSON.stringify({
+          email: this.userInfo.email,
+          name: newName,
+        }),
+      });
+
+      this.dispatchEvent(new CustomEvent("update-user-info"));
+      this.notify({
+        message: msg("Your name has been updated."),
+        variant: "success",
+        icon: "check2-circle",
+      });
+    } catch (e) {
+      this.notify({
+        message: msg("Sorry, couldn't update name at this time."),
+        variant: "danger",
+        icon: "exclamation-octagon",
+      });
+    }
+
+    this.sectionSubmitting = null;
   }
 }
