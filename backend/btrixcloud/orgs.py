c02664577d2be565ea47fe7fd577a91a10838347
Tessa Walsh <tessa@bitarchivist.net>
2023-10-10 17:28:25 -0400

    Fix indent

--- backend/btrixcloud/orgs.py
+++ backend/btrixcloud/orgs.py
@@ -519,8 +519,9 @@ def init_orgs_api(app, mdb, user_manager, invites, user_dep):
         org.name = rename.name
         if rename.slug:
             org.slug = rename.slug
-       else:
+        else:
             org.slug = slug_from_name(rename.name)
+
         try:
             await ops.update(org)
         except DuplicateKeyError:
