262f0cb1097f946bc79e7bc3e038ac028c1c1ce4
Ilya Kreymer <ikreymer@gmail.com>
2023-10-14 23:18:41 -0700

    add missing file

---
+++ backend/btrixcloud/userdb.py
@@ -0,0 +1,68 @@
+""" mongodb wrapper for users from fastapi users """
+
+from typing import Optional, Type
+from motor.motor_asyncio import AsyncIOMotorCollection
+from pydantic import UUID4
+
+from .models import UserDB
+
+
+# pylint: disable=missing-function-docstring
+# ============================================================================
+class MongoDBUserDatabase:
+    """
+    Database adapter for MongoDB.
+
+    :param user_db_model: Pydantic model of a DB representation of a user.
+    :param collection: Collection instance from `motor`.
+    """
+
+    collection: AsyncIOMotorCollection
+
+    def __init__(
+        self,
+        user_db_model: Type[UserDB],
+        collection: AsyncIOMotorCollection,
+    ):
+        self.user_db_model = user_db_model
+        self.collection = collection
+        self.initialized = False
+
+    async def get(self, id_: UUID4) -> Optional[UserDB]:
+        await self._initialize()
+
+        user = await self.collection.find_one({"id": id_})
+        return self.user_db_model(**user) if user else None
+
+    async def get_by_email(self, email: str) -> Optional[UserDB]:
+        await self._initialize()
+
+        user = await self.collection.find_one({"email": email})
+        return self.user_db_model(**user) if user else None
+
+    async def create(self, user: UserDB) -> UserDB:
+        await self._initialize()
+
+        await self.collection.insert_one(user.dict())
+        return user
+
+    async def update(self, user: UserDB) -> UserDB:
+        await self._initialize()
+
+        await self.collection.replace_one({"id": user.id}, user.dict())
+        return user
+
+    async def delete(self, user: UserDB) -> None:
+        await self._initialize()
+
+        await self.collection.delete_one({"id": user.id})
+
+    async def _initialize(self):
+        if not self.initialized:
+            await self.collection.create_index("id", unique=True)
+            await self.collection.create_index("email", unique=True)
+            await self.collection.create_index(
+                "email",
+                name="case_insensitive_email_index",
+            )
+            self.initialized = True
