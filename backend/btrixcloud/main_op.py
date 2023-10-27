dc718e966a4dce1f872b62cb16cefc697e8edac7
Ilya Kreymer <ikreymer@gmail.com>
2023-10-27 09:22:37 -0700

    fix arg typo

--- backend/btrixcloud/main_op.py
+++ backend/btrixcloud/main_op.py
@@ -81,8 +81,8 @@ def main():
         crawl_config_ops,
         coll_ops,
         storage_ops,
-        background_job_ops,
         event_webhook_ops,
+        background_job_ops,
     )
 
     return init_operator_api(
