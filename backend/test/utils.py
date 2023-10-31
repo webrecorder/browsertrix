2147a0a33e4c905af1c7dba0297ad87046e956da
Tessa Walsh <tessa@bitarchivist.net>
2023-10-30 22:36:57 -0400

    Modify return types

--- backend/test/utils.py
+++ backend/test/utils.py
@@ -43,7 +43,7 @@ def download_file_and_return_hash(bucket_name: str, file_path: str) -> str:
         return None
 
 
-def verify_file_replicated(file_path: str) -> bool:
+def verify_file_replicated(file_path: str):
     file_path_minus_bucket = file_path.split("/")[1]
     primary_file_hash = download_file_and_return_hash(
         "btrix-test-data", file_path_minus_bucket
@@ -53,7 +53,7 @@ def verify_file_replicated(file_path: str) -> bool:
     )
     assert primary_file_hash == replica_file_hash
 
-def verify_file_and_replica_deleted(file_path: str) -> None:
+def verify_file_and_replica_deleted(file_path: str):
     file_path_minus_bucket = file_path.split("/")[1]
     assert download_file_and_return_hash(
         "btrix-test-data", file_path_minus_bucket
