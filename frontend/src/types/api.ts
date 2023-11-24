fe63aee9d2ee4c9acecf0f3b1c4931e5f80aa1b4
emma <hi@emma.cafe>
2023-11-23 21:35:50 -0500

    ensure `items` isn't present unless generic type exists

--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -1,9 +1,18 @@
-export type APIPaginatedList<T = never> = {
-  items: T[];
-  total: number;
-  page: number;
-  pageSize: number;
-};
+/**
+ * If no generic type is specified, `items` cannot exist.
+ */
+export type APIPaginatedList<T = never> = never extends T
+  ? {
+      total: number;
+      page: number;
+      pageSize: number;
+    }
+  : {
+      items: T[];
+      total: number;
+      page: number;
+      pageSize: number;
+    };
 
 export type APIPaginationQuery = {
   page?: number;
