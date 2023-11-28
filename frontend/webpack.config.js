webrecorder/browsertrix

commit b15c5ccddd26d4ced5e9f9a7a4abdfc18bd91b31

ESLint & Typescript fixes (#1407)
Closes #1405

- Properly uses `typescript-eslint`: we were missing the preset from it,
so some of the default `eslint` rules (that don't properly work with
typescript) were being applied and causing false positives
- I also moved the `eslint` config into its own file, and enabled
`typescript-eslint`'s type-awareness, so that we can enable more
type-aware rules in the future if we like
- Adds `ts-lit-plugin` to the typescript config, which _hopefully_ will
allow us to catch issues during build (in CI)
- It looks like `ts-lit-plugin` is sort of abandonware at the moment,
and unfortunately _doesn't_ actually work for this purpose right now,
but the lit team is working on a replacement here:
https://www.npmjs.com/package/@lit-labs/analyzer
- Adds `fork-ts-checker-webpack-plugin`, which allows the typescript
checking process to be run on a separate forked thread in Webpack, which
can help speed up builds & checking
- Enables incremental type checking for better speed
- Fixes a whole bunch of `eslint`-auto-fixable issues (unused imports
and variables, some type issues, etc)
- Fixes a bunch of `lit-analyzer` issues (mostly attribute naming, some
type issues as well)
- Fixes various other type issues:
- Improves type safety in a bunch of places, notably anywhere `apiFetch`
and `APIPaginatedList` are used
  - Removes some `any`s

frontend/webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require("webpack");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const childProcess = require("child_process");
 const packageJSON = require("./package.json");
 const fs = require("fs");
@@ -19,7 +20,7 @@ require("dotenv").config({
 });
 
 // for testing: for prod, the Dockerfile should have the official prod version used
-const RWP_BASE_URL = process.env.RWP_BASE_URL || "https://replayweb.page/";
+const _RWP_BASE_URL = process.env.RWP_BASE_URL || "https://replayweb.page/";
 
 const WEBSOCKET_HOST =
   isDevServer && process.env.API_BASE_URL
@@ -82,6 +83,7 @@ const main = {
         exclude: /node_modules/,
         options: {
           onlyCompileBundledFiles: true,
+          transpileOnly: true,
         },
       },
       {
@@ -122,6 +124,8 @@ const main = {
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 12,
     }),
+
+    new ForkTsCheckerWebpackPlugin(),
 
     new HtmlWebpackPlugin({
       template: "src/index.ejs",

commit f272c608b8dfbd1c7263d8b60cea7c85d5f05c49

Fix failing builds on main (btrix-microk8s-test) (#1413)
Because tests aren't meant to be run on the CI builder, testing
dependencies aren't installed. `fork-ts-checker-webpack-plugin` still
was trying to type-check the test files though, and wasn't finding the
types for them. This changes the `fork-ts-checker-webpack-plugin` config
to ignore test files entirely.

Tested by manually in Docker with `scripts/build-frontend.sh`.

Thanks for your help @tw4l!

Fixes build regression introduced in
https://github.com/webrecorder/browsertrix-cloud/pull/1407

frontend/webpack.config.js
@@ -65,6 +65,7 @@ const version = (() => {
   return packageJSON.version;
 })();
 
+/** @type {import('webpack').Configuration} */
 const main = {
   entry: "./src/index.ts",
   output: {
@@ -125,7 +126,9 @@ const main = {
       maxChunks: 12,
     }),
 
-    new ForkTsCheckerWebpackPlugin(),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: { configOverwrite: { exclude: ["**/*.test.ts"] } },
+    }),
 
     new HtmlWebpackPlugin({
       template: "src/index.ejs",