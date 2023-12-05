e46ff0b9f270b2b01aaaaf10af34b6bc277983d0
emma <hi@emma.cafe>
2023-12-05 17:54:34 -0500

    clean up tailwindelement and rewrite note about styles

--- frontend/src/classes/TailwindElement.ts
+++ frontend/src/classes/TailwindElement.ts
@@ -4,25 +4,11 @@ import { css } from "lit";
 
 export class TailwindElement extends LitElement {
   /**
-   * Additional styles to be added.
-   *
-   * Styles here get added in {@linkcode TailwindElement}'s `styles` after the `@tailwind` directives.
-   */
-  static additionalStyles?: CSSResultGroup;
-
-  /**
-   * Use {@linkcode additionalStyles} instead.
-   *
    * Setting this will remove Tailwind from this component, unless you include the `@tailwind` directives at the start of your css.
-   *
-   * @deprecated
    */
-  static styles = [
-    css`
-      @tailwind base;
-      @tailwind components;
-      @tailwind utilities;
-    `,
-    TailwindElement.additionalStyles ?? [],
-  ];
+  static styles: CSSResultGroup = css`
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+  `;
 }
