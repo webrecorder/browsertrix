ecbb189dfef457c7c7fda29a054e1267a05cd103
sua yoo <sua@suayoo.com>
2023-12-12 08:36:15 -0800

    fix screen width

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -106,7 +106,7 @@ function makeTheme() {
       "4/3": "4 / 3", // For Browsertrix watch/replay
     },
     screens: {
-      lg: "var(--btrix-screen-lg)",
+      lg: "1124px", // Should match --btrix-screen-lg
       // Override default of:
       // => @media (min-width: 1024px) { ... }
     },
