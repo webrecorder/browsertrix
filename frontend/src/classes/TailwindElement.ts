88cc0974946833d18820c67a14450fea7178af96
emma <hi@emma.cafe>
2023-12-12 19:37:06 -0500

    import full theme css into tailwind elements

--- frontend/src/classes/TailwindElement.ts
+++ frontend/src/classes/TailwindElement.ts
@@ -1,14 +1,12 @@
 import type { CSSResultGroup } from "lit";
-import { LitElement } from "lit";
-import { css } from "lit";
+import { LitElement, unsafeCSS } from "lit";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const themeCSS = require("!!raw-loader!postcss-loader!../theme.css").default;
 
 export class TailwindElement extends LitElement {
   /**
    * Setting this will remove Tailwind from this component, unless you include the `@tailwind` directives at the start of your css.
    */
-  static styles: CSSResultGroup = css`
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
-  `;
+  static styles: CSSResultGroup = unsafeCSS(themeCSS);
 }
