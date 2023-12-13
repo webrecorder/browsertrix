d9b8c05483f1272d3621dad208555dea0368da60
sua yoo <sua@suayoo.com>
2023-12-12 18:42:47 -0800

    add comment

--- frontend/src/types/events.d.ts
+++ frontend/src/types/events.d.ts
@@ -3,6 +3,10 @@ import { type NavigateEventMap } from "@/controllers/navigate";
 import { type NotifyEventMap } from "@/controllers/notify";
 import { type AuthEventMap } from "@/utils/AuthService";
 
+/**
+ * Declare custom events here so that typescript can find them.
+ * Custom event names should be prefixed with `btrix-`.
+ */
 declare global {
   interface GlobalEventHandlersEventMap
     extends NavigateEventMap,
