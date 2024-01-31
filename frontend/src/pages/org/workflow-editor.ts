f2ac3a2aa8e1e73e1ca94ed731ae52c8702f6871
Henry Wilkinson <henry@wilkinson.graphics>
2024-01-31 14:09:24 -0500

    Updates browser profile selector help text

--- frontend/src/pages/org/workflow-editor.ts
+++ frontend/src/pages/org/workflow-editor.ts
@@ -1629,7 +1629,7 @@ https://archiveweb.page/images/${"logo.svg"}`}
       `)}
       ${this.renderHelpTextCol(
         msg(`Choose a custom profile to make use of saved cookies and logged-in
-        accounts.`)
+        accounts. Note that websites may log profiles out after a period of time.`)
       )}
       ${this.renderFormCol(html`
         <btrix-select-crawler
