ea494fa6e688a1b533e9925a1bc5e7e328d5fe31
Ilya Kreymer <ikreymer@users.noreply.github.com>
2024-03-06 15:38:22 -0800

    Merge V1.9.3 changes into main (#1583)
    - Fix execution time checking by keeping lastUpdatedTime in db by
    @ikreymer in https://github.com/webrecorder/browsertrix-cloud/pull/1573
    - disable postcss-lit for var css
    - Prevent closing tooltips from closing collection share dialog by
    @SuaYoo in https://github.com/webrecorder/browsertrix-cloud/pull/1579
    - Fix pending exclusion pagination by @SuaYoo in
    https://github.com/webrecorder/browsertrix-cloud/pull/1578
    - Fix regex escape in exclusion editor text match by @SuaYoo in
    https://github.com/webrecorder/browsertrix-cloud/pull/1577
    
    ---------
    Co-authored-by: emma <hi@emma.cafe>
    Co-authored-by: sua yoo <sua@webrecorder.org>

--- backend/btrixcloud/operator/crawls.py
+++ backend/btrixcloud/operator/crawls.py
@@ -232,7 +232,9 @@ class CrawlOperator(BaseOperator):
 
         else:
             status.scale = crawl.scale
-            status.lastUpdatedTime = to_k8s_date(dt_now())
+            now = dt_now()
+            await self.crawl_ops.inc_crawl_exec_time(crawl_id, 0, now)
+            status.lastUpdatedTime = to_k8s_date(now)
 
         children = self._load_redis(params, status, data.children)
 
@@ -828,11 +830,14 @@ class CrawlOperator(BaseOperator):
         """inc exec time tracking"""
         now = dt_now()
 
-        if not status.lastUpdatedTime:
+        update_start_time = await self.crawl_ops.get_crawl_exec_last_update_time(
+            crawl_id
+        )
+
+        if not update_start_time:
+            await self.crawl_ops.inc_crawl_exec_time(crawl_id, 0, now)
             status.lastUpdatedTime = to_k8s_date(now)
             return
-
-        update_start_time = from_k8s_date(status.lastUpdatedTime)
 
         reason = None
         update_duration = (now - update_start_time).total_seconds()
@@ -907,16 +912,6 @@ class CrawlOperator(BaseOperator):
                 max_duration = max(duration, max_duration)
 
         if exec_time:
-            if not await self.crawl_ops.inc_crawl_exec_time(
-                crawl_id, exec_time, status.lastUpdatedTime
-            ):
-                # if lastUpdatedTime is same as previous, something is wrong, don't update!
-                print(
-                    "Already updated for lastUpdatedTime, skipping execTime update!",
-                    flush=True,
-                )
-                return
-
             await self.org_ops.inc_org_time_stats(oid, exec_time, True)
             status.crawlExecTime += exec_time
             status.elapsedCrawlTime += max_duration
@@ -926,6 +921,7 @@ class CrawlOperator(BaseOperator):
             flush=True,
         )
 
+        await self.crawl_ops.inc_crawl_exec_time(crawl_id, exec_time, now)
         status.lastUpdatedTime = to_k8s_date(now)
 
     def should_mark_waiting(self, state, started):
