0b8c3f2c944de1b4fb909c97dbebd4681e7d6f7a
emma <hi@emma.cafe>
2024-03-12 00:29:50 -0400

    rework cache, weak ref map, and tests for them

--- frontend/src/utils/weakCache.test.ts
+++ frontend/src/utils/weakCache.test.ts
@@ -1,35 +1,98 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-base-to-string */
-/* eslint-disable @typescript-eslint/no-unnecessary-condition */
 import { fake } from "sinon";
-import { expect } from "@esm-bundle/chai";
-// import { WeakRefMap, cached, innerCache } from "./weakCache";
-import { memoize } from "proxy-memoize";
-// describe("WeakRefMap", () => {
-//   it("allows saving and retrieving from cache", () => {
-//     const cache = new WeakRefMap<object, object>();
-//     const objs = { key: { a: 1 }, value: { b: 2 } } as {
-//       key: { a: 1 };
-//       value?: { b: 2 };
-//     };
-//     cache.add(objs.key, objs.value!);
-//     expect(cache.get(objs.key) === objs.value);
-//     delete objs.value;
-//     expect(cache.get(objs.key) === objs.value);
-//   });
-// });
+import { WeakRefMap, cached, WeakRefMapInnerValue } from "./weakCache";
+import { expect } from "@open-wc/testing";
+describe("WeakRefMap", () => {
+  it("works with objects", () => {
+    const cache = new WeakRefMap<object, object>();
+    const objs = { key: { a: 1 }, value: { b: 2 } } as {
+      key: { a: 1 };
+      value?: { b: 2 };
+    };
+    cache.set(objs.key, objs.value!);
+    expect(cache.get(objs.key)).to.equal(objs.value);
+  });
+
+  it("works with strings", () => {
+    const cache = new WeakRefMap<string, string>();
+    cache.set("a", "b");
+    expect(cache.get("a")).to.equal("b");
+    expect(cache.cacheMap.get("a")?.deref()?.[WeakRefMapInnerValue]).to.equal(
+      "b",
+    );
+  });
+
+  it("works with booleans", () => {
+    const cache = new WeakRefMap<boolean, boolean>();
+    cache.set(true, false);
+    cache.set(false, true);
+    expect(cache.get(true)).to.equal(false);
+    expect(cache.get(false)).to.equal(true);
+  });
+
+  it("works with nulls, undefined values, and symbols", () => {
+    const cache = new WeakRefMap<
+      null | undefined | symbol,
+      null | undefined | symbol
+    >();
+    cache.set(null, undefined);
+    cache.set(undefined, Symbol.for("abc"));
+    cache.set(Symbol.for("abc"), null);
+    expect(cache.get(null)).to.be.undefined;
+    expect(cache.get(undefined)).to.equal(Symbol.for("abc"));
+    expect(cache.get(Symbol.for("abc"))).to.be.null;
+
+    // Ensure that we're getting the values from within the WeakRefs
+    expect(cache.cacheMap.get(null)!.deref()![WeakRefMapInnerValue]).to.be
+      .undefined;
+    expect(
+      cache.cacheMap.get(Symbol.for("abc"))!.deref()![WeakRefMapInnerValue],
+    ).to.be.null;
+  });
+
+  it("removes entries when memory is cleared", function (done) {
+    this.timeout(20);
+
+    // Adapted from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/FinalizationRegistry#callbacks_never_called_synchronously
+    const cache = new WeakRefMap<string, object>();
+    let counter = 0;
+    cache.set("a", { b: 1 });
+    (function allocateMemory() {
+      // Allocate 50000 functions — a lot of memory!
+      Array.from({ length: 50000 }, () => () => {});
+      const memoryFreed = !cache.cacheMap.has("a");
+      if (counter === 1) {
+        expect(memoryFreed).to.be.false;
+        console.log("Cache retained at 1 iteration");
+      }
+      if (counter > 5000 || memoryFreed) {
+        console.log(
+          `${memoryFreed ? "Memory freed" : "Reached counter limit"} at ${counter} iterations`,
+        );
+        expect(memoryFreed).to.be.true;
+
+        done();
+        return;
+      }
+      counter++;
+      setTimeout(allocateMemory);
+    })();
+    expect(cache.get("a")).property("b").to.equal(1);
+  });
+});
 
 describe("cached helper function", () => {
-  it.only("caches the result of a long computation", () => {
+  it("caches the result of a long computation", () => {
     let b = 0;
     const expensiveCalculation = fake((obj: object) => ({
       ...obj,
       b: ++b,
     }));
-    const cachedFn = memoize(expensiveCalculation);
-    const key = { a: 1 };
-    const res = [cachedFn(key), cachedFn(key), cachedFn(key)];
-    console.log(res);
+    const cachedFn = cached(expensiveCalculation);
+
+    cachedFn({ a: 1 });
+    cachedFn({ a: 1 });
+    cachedFn({ a: 1 });
+
     expect(expensiveCalculation.callCount).to.equal(1);
   });
 });
--- frontend/src/utils/weakCache.ts
+++ frontend/src/utils/weakCache.ts
@@ -1,14 +1,40 @@
-export class WeakRefMap<K, V extends object | string | null> {
-  private readonly cacheMap = new Map<
-    K,
-    WeakRef<NonNullable<V>> | null | string
-  >();
+export const WeakRefMapInnerValue = Symbol("inner value");
+
+type WeakRefValue<T> = T extends object
+  ? T & object
+  : { [WeakRefMapInnerValue]: T };
+
+const isWrapped = <V>(
+  cache: (V & object) | { [WeakRefMapInnerValue]: V } | undefined,
+): cache is { [WeakRefMapInnerValue]: V } => {
+  return Object.prototype.hasOwnProperty.call(
+    cache ?? {},
+    WeakRefMapInnerValue,
+  );
+};
+
+const isObj = (v: unknown): v is object => typeof v === "object" && v !== null;
+
+const wrapValue = <V>(value: V) =>
+  (isObj(value) ? value : { [WeakRefMapInnerValue]: value }) as WeakRefValue<V>;
+
+const unwrapValue = <V>(val: WeakRef<WeakRefValue<V>> | undefined) => {
+  const cache = val?.deref();
+  return isWrapped(cache) ? cache[WeakRefMapInnerValue] : (cache as V);
+};
+
+/**
+ * Maps arbitrary keys to values, using {@linkcode WeakRef}s for the values and a {@linkcode FinalizationRegistry} so that data is deleted when a cached value is garbage-collected. This is sort of the inverse of a {@linkcode WeakMap}.
+ *
+ * Adapted from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Memory_management#weakrefs_and_finalizationregistry and https://stackoverflow.com/a/72896692
+ */
+export class WeakRefMap<K, V> {
+  readonly cacheMap = new Map<K, WeakRef<WeakRefValue<V>>>();
   private readonly finalizer = new FinalizationRegistry((key: K) => {
-    // this.cacheMap.delete(key);
-    console.log("deleting key", key);
+    this.cacheMap.delete(key);
   });
 
-  add(key: K, value: V): V {
+  set(key: K, value: V): V {
     const cache = this.get(key);
     if (cache) {
       if (cache === value) return value;
@@ -16,46 +42,48 @@ export class WeakRefMap<K, V extends object | string | null> {
         this.finalizer.unregister(cache);
       }
     }
-    this.cacheMap.set(
-      key,
-      typeof value === "object" && value !== null ? new WeakRef(value) : value,
-    );
-    if (typeof value === "object" && value !== null) {
-      this.finalizer.register(value, key, value);
-    }
+    const objVal = wrapValue(value);
+    const ref = new WeakRef(objVal);
+    this.cacheMap.set(key, ref);
+    this.finalizer.register(objVal, key, objVal);
 
     return value;
   }
 
-  get(key: K): V | null {
-    const cache = this.cacheMap.get(key);
-    if (typeof cache === "string" || cache === null) {
-      return cache as V;
-    } else {
-      const val = cache?.deref() ?? null;
-      return val;
-    }
+  get(key: K): V | undefined {
+    return unwrapValue(this.cacheMap.get(key));
+  }
+
+  has(key: K): boolean {
+    return this.cacheMap.has(key);
   }
 }
 
-export const innerCache = Symbol("Inner cache");
+export const InnerCache = Symbol("Inner cache");
 
-export function cached<K extends unknown[], V extends {} | null>(
-  getter: (...keys: K) => V,
+export function cached<
+  Args extends unknown[],
+  Result,
+  Serializer extends (args: Args) => unknown = (args: Args) => string,
+>(
+  fn: (...args: Args) => Result,
+  serializer: Serializer = JSON.stringify as Serializer,
 ) {
-  const cache = new WeakMap<K, V>();
-  const fn: { (...keys: K): V; [innerCache]: WeakMap<K, V> } = (...keys: K) => {
-    if (cache.has(keys)) {
-      console.log("found key");
-      return cache.get(keys)!;
+  type Key = ReturnType<Serializer>;
+  const cache = new WeakRefMap<Key, Result>();
+  const cachedFn: {
+    (...args: Args): Result;
+    [InnerCache]: WeakRefMap<Key, Result>;
+  } = (...args: Args) => {
+    const k = serializer(args) as Key;
+    if (cache.has(k)) {
+      return cache.get(k)!;
     } else {
-      const value = getter(...keys);
-      // console.log("computed value");
-      cache.set(keys, value);
-      // console.log("set keys");
+      const value = fn(...args);
+      cache.set(k, value);
       return value;
     }
   };
-  fn[innerCache] = cache;
-  return fn;
+  cachedFn[InnerCache] = cache;
+  return cachedFn;
 }
