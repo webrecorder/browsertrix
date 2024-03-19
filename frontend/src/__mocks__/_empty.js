dcd2efcd3b9a55d04835477a148dcaf0cdb484ed
sua yoo <sua@webrecorder.org>
2024-03-19 13:06:07 -0700

    Fix asset imports in tests (#1611)
    Addresses failing test in
    https://github.com/webrecorder/browsertrix-cloud/pull/1592 by fixing
    asset imports in unit tests. Unit tests now import an empty string for
    all assets--note: if we want to test actual asset content, will need to
    update this config.

--- frontend/src/__mocks__/_empty.js
+++ frontend/src/__mocks__/_empty.js
@@ -1,10 +1,10 @@
 /**
- * Use to mock css files in tests.
+ * Use to mock files in tests.
  *
  * Usage in web-test-runner.config.mjs:
  *   importMap: {
  *     imports: {
- *       'styles.css': '/src/__mocks__/css.js'
+ *       'styles.css': '/src/__mocks__/_empty.js'
  *     },
  *   },
  */
