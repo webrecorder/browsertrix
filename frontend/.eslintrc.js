webrecorder/browsertrix

commit b1e2f1b32506c97cd1e3d1839f5b5e014c10e0be

Add ESLint rules for import ordering (#1608)
Follow-up from
https://github.com/webrecorder/browsertrix-cloud/pull/1546#discussion_r1529001599
(cc @SuaYoo)

- Adds `eslint-plugin-import-x` and
`@ianvs/prettier-plugin-sort-imports` and configures rules for them both
so imports get sorted on format & on lint.
- Runs both on everything!

frontend/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:import-x/recommended",
     "plugin:wc/recommended",
     "plugin:lit/recommended",
     "prettier",
@@ -97,8 +98,46 @@ module.exports = {
     "@typescript-eslint/restrict-template-expressions": "warn",
     "@typescript-eslint/unbound-method": "off",
     "@typescript-eslint/method-signature-style": "error",
+    /* end recommended rules */
+
+    /* start import rules */
+    "import-x/no-duplicates": ["error", { "prefer-inline": true }],
+    "import-x/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "@/*",
+            group: "internal",
+          },
+          {
+            pattern: "~assets/*",
+            group: "internal",
+          },
+        ],
+        distinctGroup: false,
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "import-x/no-relative-packages": "error",
+    "import-x/no-useless-path-segments": [
+      "error",
+      {
+        noUselessIndex: true,
+      },
+    ],
+    "import-x/no-cycle": "error",
   },
   reportUnusedDisableDirectives: true,
+  settings: {
+    "import-x/resolver": {
+      typescript: true,
+    },
+  },
   ignorePatterns: ["__generated__", "__mocks__", "dist"],
   overrides: [
     {

frontend/src/pages/org/index.ts
@@ -1,43 +1,41 @@
-import { state, property, customElement } from "lit/decorators.js";
-import { msg, localized, str } from "@lit/localize";
-import { when } from "lit/directives/when.js";
+import { localized, msg, str } from "@lit/localize";
+import { type TemplateResult } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
 import { ifDefined } from "lit/directives/if-defined.js";
+import { when } from "lit/directives/when.js";
 
-import type { ViewState } from "@/utils/APIRouter";
-import type { AuthState } from "@/utils/AuthService";
-import type { CurrentUser } from "@/types/user";
+import type { QATab } from "./archived-item-qa";
+import type { Tab as CollectionTab } from "./collection-detail";
+import type {
+  Member,
+  OrgInfoChangeEvent,
+  OrgRemoveMemberEvent,
+  UserRoleChangeEvent,
+} from "./settings";
+
+import type { QuotaUpdateDetail } from "@/controllers/api";
+import type { CollectionSavedEvent } from "@/features/collections/collection-metadata-dialog";
+import type { SelectJobTypeEvent } from "@/features/crawl-workflows/new-workflow-dialog";
 import type { Crawl, JobType } from "@/types/crawler";
-import type { OrgData } from "@/utils/orgs";
-import { isAdmin, isCrawler } from "@/utils/orgs";
-import LiteElement, { html } from "@/utils/LiteElement";
+import type { CurrentUser } from "@/types/user";
+import { isApiError } from "@/utils/api";
+import type { ViewState } from "@/utils/APIRouter";
 import { needLogin } from "@/utils/auth";
+import type { AuthState } from "@/utils/AuthService";
 import { DEFAULT_MAX_SCALE } from "@/utils/crawler";
+import LiteElement, { html } from "@/utils/LiteElement";
+import { isAdmin, isCrawler, type OrgData } from "@/utils/orgs";
+
 import "./workflow-detail";
 import "./workflows-list";
 import "./workflows-new";
 import "./archived-item-detail";
 import "./archived-items";
-import "./archived-item-qa";
 import "./collections-list";
-import "./collection-detail";
 import "./browser-profiles-detail";
 import "./browser-profiles-list";
 import "./browser-profiles-new";
-import "./settings";
 import "./dashboard";
-import type {
-  Member,
-  OrgInfoChangeEvent,
-  UserRoleChangeEvent,
-  OrgRemoveMemberEvent,
-} from "./settings";
-import type { Tab as CollectionTab } from "./collection-detail";
-import type { QATab } from "./archived-item-qa";
-import type { SelectJobTypeEvent } from "@/features/crawl-workflows/new-workflow-dialog";
-import type { QuotaUpdateDetail } from "@/controllers/api";
-import { type TemplateResult } from "lit";
-import { isApiError } from "@/utils/api";
-import type { CollectionSavedEvent } from "@/features/collections/collection-metadata-dialog";
 
 const RESOURCE_NAMES = ["workflow", "collection", "browser-profile", "upload"];
 type ResourceName = (typeof RESOURCE_NAMES)[number];

frontend/src/components/ui/index.ts
@@ -1,6 +1,7 @@
 import "./alert";
 import "./badge";
 import "./navigation";
+
 import("./button");
 import("./code");
 import("./combobox");

frontend/src/index.ts
@@ -1,30 +1,32 @@
-import type { TemplateResult } from "lit";
-import { render } from "lit";
-import { property, state, query, customElement } from "lit/decorators.js";
-import { when } from "lit/directives/when.js";
-import { msg, localized } from "@lit/localize";
-import { ifDefined } from "lit/directives/if-defined.js";
+import { localized, msg } from "@lit/localize";
 import type { SlDialog } from "@shoelace-style/shoelace";
-import "broadcastchannel-polyfill";
+import { render, type TemplateResult } from "lit";
+import { customElement, property, query, state } from "lit/decorators.js";
+import { ifDefined } from "lit/directives/if-defined.js";
+import { when } from "lit/directives/when.js";
 
+import "broadcastchannel-polyfill";
 import "./utils/polyfills";
-import appState, { use, AppStateService } from "./utils/state";
+
 import type { OrgTab } from "./pages/org";
+import { ROUTES } from "./routes";
+import type { CurrentUser, UserOrg } from "./types/user";
+import APIRouter, { type ViewState } from "./utils/APIRouter";
+import AuthService, {
+  type Auth,
+  type AuthEventDetail,
+  type AuthState,
+  type LoggedInEventDetail,
+  type NeedLoginEventDetail,
+} from "./utils/AuthService";
+import { DEFAULT_MAX_SCALE } from "./utils/crawler";
+import LiteElement, { html } from "./utils/LiteElement";
+import appState, { AppStateService, use } from "./utils/state";
+
 import type { NavigateEventDetail } from "@/controllers/navigate";
 import type { NotifyEventDetail } from "@/controllers/notify";
-import LiteElement, { html } from "./utils/LiteElement";
-import APIRouter from "./utils/APIRouter";
-import AuthService from "./utils/AuthService";
-import type {
-  LoggedInEventDetail,
-  NeedLoginEventDetail,
-  AuthState,
-  Auth,
-  AuthEventDetail,
-} from "./utils/AuthService";
-import type { ViewState } from "./utils/APIRouter";
-import type { CurrentUser, UserOrg } from "./types/user";
-import { ROUTES } from "./routes";
+import { theme } from "@/theme";
+
 import "./shoelace";
 import "./components";
 import "./features";
@@ -32,8 +34,6 @@ import "./pages";
 import "./assets/fonts/Inter/inter.css";
 import "./assets/fonts/Recursive/recursive.css";
 import "./styles.css";
-import { theme } from "@/theme";
-import { DEFAULT_MAX_SCALE } from "./utils/crawler";
 
 // Make theme CSS available in document
 document.adoptedStyleSheets = [theme];

commit b43f550ff38c55445e234b321eba97dee1086739

Fix missing page component imports (#1610)
Missed bug introduced in
https://github.com/webrecorder/browsertrix-cloud/pull/1608, adds back
imports and disables `import-x` rule.

frontend/.eslintrc.js
@@ -101,7 +101,7 @@ module.exports = {
     /* end recommended rules */
 
     /* start import rules */
-    "import-x/no-duplicates": ["error", { "prefer-inline": true }],
+    // "import-x/no-duplicates": ["error", { "prefer-inline": true }],
     "import-x/order": [
       "error",
       {

frontend/src/pages/org/index.ts
@@ -31,10 +31,13 @@ import "./workflows-list";
 import "./workflows-new";
 import "./archived-item-detail";
 import "./archived-items";
+import "./archived-item-qa";
 import "./collections-list";
+import "./collection-detail";
 import "./browser-profiles-detail";
 import "./browser-profiles-list";
 import "./browser-profiles-new";
+import "./settings";
 import "./dashboard";
 
 const RESOURCE_NAMES = ["workflow", "collection", "browser-profile", "upload"];