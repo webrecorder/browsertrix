f7e6800e35f1018d4b33398a9d9310e3f49729da
sua yoo <sua@suayoo.com>
2024-04-17 16:09:17 -0700

    update resource tab icon

--- frontend/src/pages/org/archived-item-qa/archived-item-qa.ts
+++ frontend/src/pages/org/archived-item-qa/archived-item-qa.ts
@@ -450,7 +450,7 @@ export class ArchivedItemQA extends TailwindElement {
               ?active=${this.tab === "resources"}
               @click=${this.onTabNavClick}
             >
-              <sl-icon name="database-down"></sl-icon>
+              <sl-icon name="database"></sl-icon>
               ${msg("Resources")}
             </btrix-navigation-button>
             <btrix-navigation-button
