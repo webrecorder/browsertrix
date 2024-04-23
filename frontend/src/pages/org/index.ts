15de04c4f0d9c5bb8adfba9b1c97c850cb42bdb2
sua yoo <sua@suayoo.com>
2024-04-23 13:21:12 -0700

    remove padding

--- frontend/src/pages/org/index.ts
+++ frontend/src/pages/org/index.ts
@@ -304,7 +304,7 @@ export class Org extends LiteElement {
         <main
           class="${noMaxWidth
             ? "w-full"
-            : "w-full max-w-screen-desktop"} mx-auto box-border flex flex-1 flex-col p-3 pt-7"
+            : "w-full max-w-screen-desktop pt-7"} mx-auto box-border flex flex-1 flex-col p-3"
           aria-labelledby="${this.orgTab}-tab"
         >
           ${tabPanelContent}
