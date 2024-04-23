017256d8deb6c7063f5691a8370505f76eb5832a
sua yoo <sua@suayoo.com>
2024-04-22 17:35:39 -0700

    update info tooltip

--- frontend/src/pages/org/archived-item-detail/ui/qa.ts
+++ frontend/src/pages/org/archived-item-detail/ui/qa.ts
@@ -530,7 +530,7 @@ export class ArchivedItemDetailQA extends TailwindElement {
             )}
             <sl-tooltip
               content=${msg(
-                "Match analysis compares pages during a crawl vs. during an analysis run. A good match indicates that the crawl is probably good, whereas a severely inconsistent may indicate a bad crawl.",
+                "Match analysis compares pages during a crawl vs. during an analysis run. A good match indicates that the crawl is probably good, whereas severe inconsistencies may indicate a bad crawl.",
               )}
             >
               <sl-icon class="text-base" name="info-circle"></sl-icon>
