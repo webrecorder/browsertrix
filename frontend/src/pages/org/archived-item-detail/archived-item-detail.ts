f45c1352b4e2eec693224e88186d33251c4ff751
Henry Wilkinson <henry@wilkinson.graphics>
2024-04-30 18:09:35 -0400

    remove truncate
    oops

--- frontend/src/pages/org/archived-item-detail/archived-item-detail.ts
+++ frontend/src/pages/org/archived-item-detail/archived-item-detail.ts
@@ -537,9 +537,7 @@ export class ArchivedItemDetail extends TailwindElement {
       <header
         class="mb-3 flex flex-col items-end gap-2 border-b pb-3 lg:flex-row"
       >
-        <h1
-          class="grid min-w-0 flex-auto truncate text-xl font-semibold leading-7"
-        >
+        <h1 class="grid min-w-0 flex-auto text-xl font-semibold leading-7">
           ${this.renderName()}
         </h1>
         <div
