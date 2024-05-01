ad46fb345dfac1c21ebed09c5043937b7f9e536b
sua yoo <sua@suayoo.com>
2024-04-30 17:21:51 -0700

    prevent replay navigation

--- frontend/src/pages/org/archived-item-qa/ui/replay.ts
+++ frontend/src/pages/org/archived-item-qa/ui/replay.ts
@@ -9,6 +9,7 @@ import { renderSpinner } from "./spinner";
 import { tw } from "@/utils/tailwind";
 
 export function renderReplay(crawlData: ReplayData) {
+  console.log("crawlData?.replayUrl", crawlData?.replayUrl);
   return html`
     <div
       class=${tw`relative h-full overflow-hidden rounded-b-lg border-x border-b bg-slate-100 p-4 shadow-inner`}
@@ -21,6 +22,17 @@ export function renderReplay(crawlData: ReplayData) {
               id="interactiveReplayFrame"
               src=${replayUrl}
               class=${tw`h-full w-full overflow-hidden rounded border bg-neutral-0 shadow-lg`}
+              @load=${(e: Event) => {
+                const iframe = e.currentTarget as HTMLIFrameElement;
+
+                /// Prevent anchor tag navigation
+                iframe.contentDocument?.querySelectorAll("a").forEach((a) => {
+                  a.addEventListener("click", (e: MouseEvent) => {
+                    e.preventDefault();
+                    console.log("click prevented");
+                  });
+                });
+              }}
             ></iframe>`,
           renderSpinner,
         ),
