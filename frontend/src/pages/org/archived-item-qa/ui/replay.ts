4096236222b9c560e11dd03089b965c232b3431a
sua yoo <sua@suayoo.com>
2024-04-30 17:50:55 -0700

    show alert

--- frontend/src/pages/org/archived-item-qa/ui/replay.ts
+++ frontend/src/pages/org/archived-item-qa/ui/replay.ts
@@ -1,3 +1,4 @@
+import { msg } from "@lit/localize";
 import { html } from "lit";
 import { guard } from "lit/directives/guard.js";
 import { when } from "lit/directives/when.js";
@@ -6,37 +7,46 @@ import type { ReplayData } from "../types";
 
 import { renderSpinner } from "./spinner";
 
+import type { Dialog } from "@/components/ui/dialog";
 import { tw } from "@/utils/tailwind";
 
 export function renderReplay(crawlData: ReplayData) {
   console.log("crawlData?.replayUrl", crawlData?.replayUrl);
   return html`
-    <div
-      class=${tw`relative h-full overflow-hidden rounded-b-lg border-x border-b bg-slate-100 p-4 shadow-inner`}
-    >
-      ${guard([crawlData], () =>
-        when(
-          crawlData?.replayUrl,
-          (replayUrl) =>
-            html`<iframe
-              id="interactiveReplayFrame"
-              src=${replayUrl}
-              class=${tw`h-full w-full overflow-hidden rounded border bg-neutral-0 shadow-lg`}
-              @load=${(e: Event) => {
-                const iframe = e.currentTarget as HTMLIFrameElement;
+    <div class="replayContainer ${tw`h-full [contain:paint]`}">
+      <div
+        class=${tw`relative h-full overflow-hidden rounded-b-lg border-x border-b bg-slate-100 p-4 shadow-inner`}
+      >
+        ${guard([crawlData], () =>
+          when(
+            crawlData?.replayUrl,
+            (replayUrl) =>
+              html`<iframe
+                id="interactiveReplayFrame"
+                src=${replayUrl}
+                class=${tw`h-full w-full overflow-hidden rounded border bg-neutral-0 shadow-lg`}
+                @load=${(e: Event) => {
+                  const iframe = e.currentTarget as HTMLIFrameElement;
 
-                /// Prevent anchor tag navigation
-                iframe.contentDocument?.querySelectorAll("a").forEach((a) => {
-                  a.addEventListener("click", (e: MouseEvent) => {
-                    e.preventDefault();
-                    console.log("click prevented");
+                  /// Prevent anchor tag navigation
+                  iframe.contentDocument?.querySelectorAll("a").forEach((a) => {
+                    a.addEventListener("click", (e: MouseEvent) => {
+                      e.preventDefault();
+                      void iframe
+                        .closest(".replayContainer")
+                        ?.querySelector<Dialog>("btrix-dialog")
+                        ?.show();
+                    });
                   });
-                });
-              }}
-            ></iframe>`,
-          renderSpinner,
-        ),
-      )}
+                }}
+              ></iframe>`,
+            renderSpinner,
+          ),
+        )}
+      </div>
+      <btrix-dialog .label=${msg("Click prevented")}>
+        ${msg("Clicking links within Replay is currently disabled.")}
+      </btrix-dialog>
     </div>
   `;
 }
