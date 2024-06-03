03940ccaa0b791695ea2dee1e3fd0ec8a8c4fecc
emma <hi@emma.cafe>
2024-06-03 18:01:41 -0400

    improve type suggestions for sort directions

--- frontend/src/components/ui/table/table-header-cell.ts
+++ frontend/src/components/ui/table/table-header-cell.ts
@@ -3,8 +3,10 @@ import { customElement, property } from "lit/decorators.js";
 
 import { TableCell } from "./table-cell";
 
+import type { SortDirection as Direction } from "@/types/utils";
+
 export type SortValues = "ascending" | "descending" | "none";
-export const SortDirection = new Map<number, SortValues>([
+export const SortDirection = new Map<Direction, SortValues>([
   [-1, "descending"],
   [1, "ascending"],
 ]);
