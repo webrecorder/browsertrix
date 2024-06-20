216c621d18238ddbd995078216f816789ede2294
Ilya Kreymer <ikreymer@gmail.com>
2024-06-20 14:02:17 -0700

    tweak status check

--- backend/btrixcloud/models.py
+++ backend/btrixcloud/models.py
@@ -1585,7 +1585,7 @@ class Page(BaseMongoModel):
             if self.mime:
                 if "html" not in self.mime:
                     self.isFile = True
-                elif self.title is None and self.status == 200:
+                elif self.title is None and (not self.status or self.status == 200):
                     self.isFile = True
 
         elif self.loadState == 0:
