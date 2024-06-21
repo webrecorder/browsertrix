a841aa00f8e7fb44638638996f1663378350ce71
emma <hi@emma.cafe>
2024-06-21 03:39:46 -0400

    translate "match" text

--- frontend/src/pages/org/archived-item-detail/ui/qa.ts
+++ frontend/src/pages/org/archived-item-detail/ui/qa.ts
@@ -665,7 +665,8 @@ export class ArchivedItemDetailQA extends TailwindElement {
                         : idx === qaStatsThresholds.length - 1
                           ? `>=${threshold ? +threshold.lowerBoundary * 100 : 0}%`
                           : `${threshold ? +threshold.lowerBoundary * 100 : 0}-${+qaStatsThresholds[idx + 1].lowerBoundary * 100 || 100}%`}
-                      match <br />
+                      ${msg("match", { desc: "label for match percentage" })}
+                      <br />
                       ${formatNumber(bar.count)} ${pluralOf("pages", bar.count)}
                     </div>
                   </div>
