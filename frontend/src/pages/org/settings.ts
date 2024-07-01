cb36e2d249ea9bce91d27db3e791a5bba44a4a62
sua yoo <sua@suayoo.com>
2024-07-01 16:01:51 -0700

    update content

--- frontend/src/pages/org/settings.ts
+++ frontend/src/pages/org/settings.ts
@@ -323,17 +323,42 @@ export class OrgSettings extends LiteElement {
         <div class="grid grid-cols-5 gap-x-4 p-4 md:gap-y-6">
           ${this.renderMainCol(html`
             <h4 class="form-label text-xs text-neutral-800">
-              ${msg("Status")}
+              ${msg("Current Plan")}
             </h4>
-            <div class="flex justify-between rounded-lg border p-4">
-              <div>TODO</div>
-              <a href="#" class="flex items-center gap-2">
-                ${msg("Manage Plan")}
-                <sl-icon name="arrow-right"></sl-icon>
-              </a>
-            </div>
+            <btrix-card>
+              <div slot="title" class="flex items-center justify-between">
+                <div class="flex items-center gap-2">
+                  <sl-icon class="text-success" name="check-circle"></sl-icon>
+                  ${msg("Active")}
+                </div>
+                <a
+                  class="transition-color flex items-center gap-2 px-2 py-1 text-sm leading-none text-primary hover:text-primary-500"
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer nofollow"
+                >
+                  ${msg("Manage Plan")}
+                  <sl-icon slot="suffix" name="arrow-right"></sl-icon>
+                </a>
+              </div>
+              <ul>
+                <li>[quota]</li>
+                <li>[quota]</li>
+                <li>[quota]</li>
+                <li>[quota]</li>
+              </ul>
+            </btrix-card>
           `)}
-          ${this.renderInfoCol(msg("TODO"))}
+          ${this.renderInfoCol(html`
+            <p class="mb-3">
+              ${msg("Hosted plan status, quotas, and add-ons, if applicable.")}
+            </p>
+            <p class="leading-normal">
+              ${msg(
+                "You can view plan details, update payment methods, and update billing information by clicking “Manage Plan”. This will redirect you to our payment processor in a new tab.",
+              )}
+            </p>
+          `)}
         </div>
       </div>
     `;
