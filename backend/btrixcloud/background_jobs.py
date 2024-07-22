webrecorder/browsertrix

commit b1ccdc4d169830c5419e12284a62fcf09b3ec0df

OpenAPI Metadata for API Endpoints (#1941)
- Updates the `/docs` and `/redoc` API endpoints to have better metadata,
including using Browsertrix favicon and our logo for the `/redoc` endpoint.
- add new logo file 'docs-logo.svg' to root

Based on info at:
https://fastapi.tiangolo.com/how-to/extending-openapi/
https://fastapi.tiangolo.com/tutorial/metadata/

---------
Co-authored-by: Henry Wilkinson <henry@wilkinson.graphics>

backend/btrixcloud/main.py
@@ -11,6 +11,9 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.routing import APIRouter
 
+from fastapi.openapi.utils import get_openapi
+from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
+
 from .db import init_db, await_db_and_migrations, update_and_prepare_db
 
 from .emailsender import EmailSender
@@ -34,16 +37,43 @@ from .subs import init_subs_api
 
 from .crawlmanager import CrawlManager
 from .utils import run_once_lock, register_exit_handler, is_bool
-
+from .version import __version__
 
 API_PREFIX = "/api"
-app_root = FastAPI(
-    docs_url=API_PREFIX + "/docs",
-    redoc_url=API_PREFIX + "/redoc",
-    openapi_url=API_PREFIX + "/openapi.json",
-)
+
+OPENAPI_URL = API_PREFIX + "/openapi.json"
+
+app_root = FastAPI(docs_url=None, redoc_url=None, OPENAPI_URL=OPENAPI_URL)
 
 db_inited = {"inited": False}
+
+
+# ============================================================================
+def make_schema():
+    """make custom openapi schema"""
+    schema = get_openapi(
+        title="Browsertrix",
+        description="""\
+The Browsertrix API provides access to all aspects of the Browsertrix app.
+
+See [https://docs.browsertrix.com/](https://docs.browsertrix.com/) for more info on deploying Browsertrix\
+        """,
+        summary="Browsertrix Crawling System API",
+        version=__version__,
+        terms_of_service="http://browsertrix.com/terms",
+        contact={
+            "name": "Browsertrix",
+            "url": "https://browsertrix.com/",
+            "email": "info@webrecorder.net",
+        },
+        license_info={
+            "name": "AGPL v3",
+            "url": "https://www.gnu.org/licenses/agpl-3.0.en.html",
+        },
+        routes=app_root.routes,
+    )
+    schema["info"]["x-logo"] = {"url": "/docs-logo.svg"}
+    return schema
 
 
 # ============================================================================
@@ -200,7 +230,6 @@ def main():
         return settings
 
     # internal routes
-
     @app.get("/openapi.json", include_in_schema=False)
     async def openapi() -> JSONResponse:
         return JSONResponse(app_root.openapi())
@@ -220,6 +249,31 @@ def main():
         return {}
 
     app_root.include_router(app, prefix=API_PREFIX)
+
+    # API Configurations -- needed to provide custom favicon
+    @app_root.get(API_PREFIX + "/docs", include_in_schema=False)
+    def overridden_swagger():
+        return get_swagger_ui_html(
+            openapi_url=OPENAPI_URL,
+            title="Browsertrix API",
+            swagger_favicon_url="/favicon.ico",
+        )
+
+    @app_root.get(API_PREFIX + "/redoc", include_in_schema=False)
+    def overridden_redoc():
+        return get_redoc_html(
+            openapi_url=OPENAPI_URL,
+            title="Browsertrix API",
+            redoc_favicon_url="/favicon.ico",
+        )
+
+    def get_api_schema():
+        if not app_root.openapi_schema:
+            app_root.openapi_schema = make_schema()
+
+        return app_root.openapi_schema
+
+    app_root.openapi = get_api_schema  # type: ignore
 
 
 # ============================================================================

backend/test/test_api.py
@@ -1,0 +1,32 @@
+import requests
+
+from .conftest import API_PREFIX
+
+
+def test_api_docs():
+    r = requests.get(f"{API_PREFIX}/docs")
+    assert r.status_code == 200
+
+    text = r.text
+    assert "<title>Browsertrix API</title>" in text
+    assert "/favicon.ico" in text
+    assert "/api/openapi.json" in text
+
+
+def test_api_redoc():
+    r = requests.get(f"{API_PREFIX}/redoc")
+    assert r.status_code == 200
+
+    text = r.text
+    assert "<title>Browsertrix API</title>" in text
+    assert "/favicon.ico" in text
+    assert "/api/openapi.json" in text
+
+
+def test_api_openapi():
+    r = requests.get(f"{API_PREFIX}/openapi.json")
+    assert r.status_code == 200
+
+    json = r.json()
+    assert json["info"]["title"] == "Browsertrix"
+    assert json["info"]["x-logo"]["url"] == "/docs-logo.svg"

frontend/src/assets/favicons/docs-logo.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2" viewBox="0 0 737 218"><path d="M788.928-638.587h778.874v237.12H788.928z" style="fill:none" transform="matrix(.84353 0 0 .57792 -625.481 409.055)"/><path d="M.347.011a.338.338 0 0 1-.17-.05l.034-.089a.3.3 0 0 0 .092.016q.033 0 .055-.015a.1.1 0 0 0 .035-.049A.396.396 0 0 0 .4-.343.1.1 0 0 0 .381-.39a.06.06 0 0 0-.029-.024.1.1 0 0 0-.04-.007.2.2 0 0 0-.054.008.13.13 0 0 0-.051.03L.174-.446a.3.3 0 0 1 .057-.055.2.2 0 0 1 .131-.043.2.2 0 0 1 .08.015q.036.015.063.047.027.033.041.085t.015.128a.4.4 0 0 1-.025.153.19.19 0 0 1-.189.127M.07 0v-.75h.156v.66L.167 0z" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 323.788 167.638)"/><path d="M.111.18V.135l.172-.148-.011-.126.096-.395h.146v.041L.296.18zm.068-.197L.024-.493v-.041h.147l.108.408.032.109z" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 353.838 167.638)"/><path d="m.165 0 .042-.147.188-.452H.46l.19.454L.688 0H.583L.428-.351.405-.42h.044l-.023.069L.268 0zM.15 0 .045-.67v-.04h.152l.076.493L.242 0zm.461 0L.58-.217.658-.71h.151v.04L.703 0z" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 394.128 167.638)"/><path d="M.322.011a.35.35 0 0 1-.155-.032.2.2 0 0 1-.095-.092.3.3 0 0 1-.033-.149q0-.09.03-.152a.22.22 0 0 1 .086-.097.26.26 0 0 1 .135-.033q.052 0 .093.015a.16.16 0 0 1 .065.043q.026.028.038.068a.3.3 0 0 1 .013.09v.103H.102v-.086h.289l-.029.049v-.087L.359-.377.352-.401.338-.418a.1.1 0 0 0-.021-.011L.29-.432a.1.1 0 0 0-.052.016.1.1 0 0 0-.034.054.34.34 0 0 0 .004.193.1.1 0 0 0 .049.051.2.2 0 0 0 .082.017.5.5 0 0 0 .126-.017l.022.101a.5.5 0 0 1-.165.028" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 436.518 167.638)"/><path d="M.347.011a.338.338 0 0 1-.17-.05l.034-.089a.3.3 0 0 0 .092.016q.033 0 .055-.015a.1.1 0 0 0 .035-.049A.396.396 0 0 0 .4-.343.1.1 0 0 0 .381-.39a.06.06 0 0 0-.029-.024.1.1 0 0 0-.04-.007.2.2 0 0 0-.054.008.13.13 0 0 0-.051.03L.174-.446a.3.3 0 0 1 .057-.055.2.2 0 0 1 .131-.043.2.2 0 0 1 .08.015q.036.015.063.047.027.033.041.085t.015.128a.4.4 0 0 1-.025.153.19.19 0 0 1-.189.127M.07 0v-.75h.156v.66L.167 0z" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 463.458 167.638)"/><path d="M.07 0v-.534h.105l.051.148V0zm.116-.293v-.1l.138-.141h.075v.176z" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 493.508 167.638)"/><path d="M.322.011a.35.35 0 0 1-.155-.032.2.2 0 0 1-.095-.092.3.3 0 0 1-.033-.149q0-.09.03-.152a.22.22 0 0 1 .086-.097.26.26 0 0 1 .135-.033q.052 0 .093.015a.16.16 0 0 1 .065.043q.026.028.038.068a.3.3 0 0 1 .013.09v.103H.102v-.086h.289l-.029.049v-.087L.359-.377.352-.401.338-.418a.1.1 0 0 0-.021-.011L.29-.432a.1.1 0 0 0-.052.016.1.1 0 0 0-.034.054.34.34 0 0 0 .004.193.1.1 0 0 0 .049.051.2.2 0 0 0 .082.017.5.5 0 0 0 .126-.017l.022.101a.5.5 0 0 1-.165.028" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 515.488 167.638)"/><path d="M.299.011a.3.3 0 0 1-.115-.019.215.215 0 0 1-.129-.144.43.43 0 0 1 .001-.234.24.24 0 0 1 .048-.086.2.2 0 0 1 .081-.054.3.3 0 0 1 .113-.018.5.5 0 0 1 .131.02L.404-.41a1 1 0 0 0-.086-.011.2.2 0 0 0-.055.01.1.1 0 0 0-.039.03.1.1 0 0 0-.024.048.27.27 0 0 0 .001.138.101.101 0 0 0 .064.074.2.2 0 0 0 .054.008.3.3 0 0 0 .09-.011l.025.113a.5.5 0 0 1-.135.022" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 542.428 167.638)"/><path d="M.289.011a.3.3 0 0 1-.133-.03.2.2 0 0 1-.087-.09.36.36 0 0 1-.03-.156q0-.096.03-.158a.2.2 0 0 1 .087-.091.3.3 0 0 1 .133-.03.3.3 0 0 1 .134.03q.056.029.086.091.03.061.03.157 0 .095-.03.156a.2.2 0 0 1-.086.091.3.3 0 0 1-.134.03m0-.12a.1.1 0 0 0 .053-.016.1.1 0 0 0 .033-.05.3.3 0 0 0 .012-.091.3.3 0 0 0-.012-.092.1.1 0 0 0-.033-.051.09.09 0 0 0-.104 0 .1.1 0 0 0-.034.051.3.3 0 0 0-.012.092q0 .056.012.091t.034.05a.1.1 0 0 0 .051.016" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 566.028 167.638)"/><path d="M.07 0v-.534h.105l.051.148V0zm.116-.293v-.1l.138-.141h.075v.176z" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 594.958 167.638)"/><path d="M.239.011a.2.2 0 0 1-.08-.015.2.2 0 0 1-.063-.048.2.2 0 0 1-.042-.085.5.5 0 0 1-.015-.129.4.4 0 0 1 .025-.152.2.2 0 0 1 .074-.094.2.2 0 0 1 .116-.032.3.3 0 0 1 .17.049L.39-.406a.2.2 0 0 0-.044-.011.2.2 0 0 0-.047-.004.1.1 0 0 0-.055.014.1.1 0 0 0-.036.048.3.3 0 0 0-.014.092q0 .046.007.076.006.03.018.047a.1.1 0 0 0 .03.025.1.1 0 0 0 .039.007.2.2 0 0 0 .055-.009.2.2 0 0 0 .051-.03l.03.063a.27.27 0 0 1-.117.087.2.2 0 0 1-.068.012M.43 0 .375-.1v-.65H.53V0z" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 616.938 167.638)"/><path d="M.322.011a.35.35 0 0 1-.155-.032.2.2 0 0 1-.095-.092.3.3 0 0 1-.033-.149q0-.09.03-.152a.22.22 0 0 1 .086-.097.26.26 0 0 1 .135-.033q.052 0 .093.015a.16.16 0 0 1 .065.043q.026.028.038.068a.3.3 0 0 1 .013.09v.103H.102v-.086h.289l-.029.049v-.087L.359-.377.352-.401.338-.418a.1.1 0 0 0-.021-.011L.29-.432a.1.1 0 0 0-.052.016.1.1 0 0 0-.034.054.34.34 0 0 0 .004.193.1.1 0 0 0 .049.051.2.2 0 0 0 .082.017.5.5 0 0 0 .126-.017l.022.101a.5.5 0 0 1-.165.028" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 646.968 167.638)"/><path d="M.07 0v-.534h.105l.051.148V0zm.116-.293v-.1l.138-.141h.075v.176z" style="fill:#14122d;fill-rule:nonzero" transform="matrix(50 0 0 49.99996 673.908 167.638)"/><path d="M146.76 75h-26.832V19.62h27.066q7.02 0 10.764 3.51 3.744 3.432 3.744 9.438 0 4.992-3.198 8.892-1.764 2.206-4.327 3.912 3.268 1.407 5.341 3.81 3.432 3.978 3.432 10.14 0 7.254-4.056 11.466T146.76 75m-14.274-45.942V43.41h12.648a12.3 12.3 0 0 0 2.562-3.276 9.76 9.76 0 0 0 1.17-4.68q0-2.886-1.482-4.602-1.404-1.794-3.9-1.794zm0 22.152v14.04h11.856q2.808 0 4.212-1.716 1.482-1.794 1.482-5.148 0-3.432-1.482-5.304-1.185-1.56-3.22-1.872zm53.339 5.304V75h-12.558V19.62h24.726q8.502 0 12.87 4.212 4.446 4.212 4.446 11.7 0 6.318-3.354 11.076-2.181 3.115-5.676 5.955l9.42 19.473V75h-12.324l-9.809-18.486zm0-26.988v18.018h11.769a11.7 11.7 0 0 0 3.519-4.056q1.56-2.808 1.56-6.396 0-3.666-1.716-5.616-1.638-1.95-4.758-1.95zM291.766 75h-9.594l-7.098-52.026V19.62h12.246l4.046 31.718 8.824-22.982h5.07l8.686 22.544 4.184-31.28h12.09v3.354L323.2 75h-9.828l-10.53-25.506-.195-.626-.195.626zm127.878 0h-34.242V19.62h34.242v11.076H397.96v12.168h19.89v8.814h-19.89v12.246h21.684zm23.352-18.486V75h-12.558V19.62h24.726q8.502 0 12.87 4.212 4.446 4.212 4.446 11.7 0 6.318-3.354 11.076-2.181 3.115-5.676 5.955l9.42 19.473V75h-12.324l-9.809-18.486zm0-26.988v18.018h11.769a11.7 11.7 0 0 0 3.519-4.056q1.56-2.808 1.56-6.396 0-3.666-1.716-5.616-1.638-1.95-4.758-1.95zm50.538 1.482h-13.728V19.62h39.858v11.388h-13.572V75h-12.558zm48.351 25.506V75h-12.558V19.62h24.726q8.502 0 12.87 4.212 4.446 4.212 4.446 11.7 0 6.318-3.354 11.076-2.181 3.115-5.676 5.955l9.42 19.473V75h-12.324l-9.809-18.486zm0-26.988v18.018h11.769a11.7 11.7 0 0 0 3.519-4.056q1.56-2.808 1.56-6.396 0-3.666-1.716-5.616-1.638-1.95-4.758-1.95zm83.545 27.507L615.939 75h-11.778v-3.354l15.193-24.905-14.335-23.767V19.62h12.324l10.531 17.233 9.359-17.233h11.622v3.354l-14.773 24.485 14.773 24.187V75h-12.402zM245.956 75.78q-7.332 0-12.324-2.808-4.914-2.808-7.488-9.048t-2.574-16.536q0-10.374 2.574-16.614 2.574-6.318 7.488-9.126 4.992-2.808 12.324-2.808t12.168 2.808q4.915 2.808 7.489 9.126 2.65 6.24 2.651 16.614 0 10.218-2.574 16.458-2.572 6.24-7.566 9.126-4.913 2.808-12.168 2.808m0-9.984q3.12 0 5.226-1.482 2.106-1.56 3.276-5.538 1.248-4.056 1.248-11.388 0-7.254-1.248-11.31-1.17-4.134-3.276-5.694-2.106-1.638-5.226-1.56-3.276.078-5.46 1.638-2.106 1.482-3.276 5.538-1.092 4.056-1.092 11.388t1.17 11.388 3.276 5.538q2.184 1.482 5.382 1.482m108.157 9.984q-4.602 0-8.892-.624a63 63 0 0 1-8.268-1.638l2.106-9.36a78 78 0 0 0 6.942 1.404q3.588.468 6.942.468 5.382 0 7.644-1.482 2.34-1.56 2.34-4.602a5 5 0 0 0-.468-2.106q-.468-1.014-1.482-1.872-.936-.858-2.34-1.638-.078-.078-.156-.078h-.156l-10.608-6.006a5 5 0 0 0-.546-.312 2 2 0 0 0-.468-.234q-4.524-2.574-7.098-5.616-2.574-3.12-2.574-8.112 0-4.602 2.496-7.956 2.574-3.432 7.41-5.304 4.914-1.872 11.778-1.872 3.978 0 7.956.468 4.056.468 7.722 1.404l-2.106 9.36a60 60 0 0 0-6.24-1.092q-3.12-.39-5.928-.39-3.354 0-5.694.624-2.262.546-3.432 1.872-1.092 1.248-1.092 3.12 0 1.638.702 2.73.78 1.014 2.574 2.028.078 0 .078.078.078.078.156.078l11.232 5.85q3.744 1.794 6.162 4.056 2.418 2.184 3.666 4.992 1.326 2.73 1.326 6.162 0 7.488-5.616 11.544t-16.068 4.056M582.361 75V19.62h12.558V75z" style="fill:#14122d" transform="translate(45 40.074)"/><path d="M6430.4 1290.72c-.37.16-.6.52-.6.91 0 .4.23.76.6.92 3.88 1.7 6.6 5.58 6.6 10.08 0 4.51-2.72 8.39-6.6 10.09-.37.16-.6.52-.6.91 0 .4.23.76.6.92 3.88 1.7 6.6 5.58 6.6 10.08 0 6.08-4.93 11-11 11-4.51 0-8.39-2.71-10.08-6.6-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a10.98 10.98 0 0 1-10.08 6.6c-4.51 0-8.39-2.71-10.08-6.6-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a10.98 10.98 0 0 1-10.08 6.6c-4.51 0-8.39-2.71-10.08-6.6-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a10.98 10.98 0 0 1-10.08 6.6c-6.07 0-11-4.92-11-11 0-4.5 2.72-8.38 6.6-10.08.37-.16.6-.52.6-.92 0-.39-.23-.75-.6-.91-3.88-1.7-6.6-5.58-6.6-10.09 0-4.5 2.72-8.38 6.6-10.08.37-.16.6-.52.6-.92 0-.39-.23-.75-.6-.91-3.88-1.7-6.6-5.58-6.6-10.09 0-6.07 4.93-11 11-11 4.51 0 8.39 2.72 10.08 6.61.16.36.52.6.92.6s.76-.24.92-.6a10.99 10.99 0 0 1 10.08-6.61c4.51 0 8.39 2.72 10.08 6.61.16.36.52.6.92.6s.76-.24.92-.6a10.99 10.99 0 0 1 10.08-6.61c4.51 0 8.39 2.72 10.08 6.61.16.36.52.6.92.6s.76-.24.92-.6a10.99 10.99 0 0 1 10.08-6.61c6.07 0 11 4.93 11 11 0 4.51-2.72 8.39-6.6 10.09m-58.48-5.69c-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a11.03 11.03 0 0 1-5.68 5.69c-.37.16-.6.52-.6.91 0 .4.23.76.6.92 2.54 1.11 4.57 3.15 5.68 5.69.16.36.52.6.92.6s.76-.24.92-.6c1.11-2.54 3.14-4.58 5.68-5.69.37-.16.6-.52.6-.92 0-.39-.23-.75-.6-.91a11.03 11.03 0 0 1-5.68-5.69m44 22c-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a11.03 11.03 0 0 1-5.68 5.69c-.37.16-.6.52-.6.91 0 .4.23.76.6.92 2.54 1.11 4.57 3.15 5.68 5.69.16.36.52.6.92.6s.76-.24.92-.6c1.11-2.54 3.14-4.58 5.68-5.69.37-.16.6-.52.6-.92 0-.39-.23-.75-.6-.91a11.03 11.03 0 0 1-5.68-5.69m0-22c-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a11.03 11.03 0 0 1-5.68 5.69c-.37.16-.6.52-.6.91 0 .4.23.76.6.92 2.54 1.11 4.57 3.15 5.68 5.69.16.36.52.6.92.6s.76-.24.92-.6c1.11-2.54 3.14-4.58 5.68-5.69.37-.16.6-.52.6-.92 0-.39-.23-.75-.6-.91a11.03 11.03 0 0 1-5.68-5.69m-22 0c-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a11.03 11.03 0 0 1-5.68 5.69c-.37.16-.6.52-.6.91 0 .4.23.76.6.92 2.54 1.11 4.57 3.15 5.68 5.69.16.36.52.6.92.6s.76-.24.92-.6c1.11-2.54 3.14-4.58 5.68-5.69.37-.16.6-.52.6-.92 0-.39-.23-.75-.6-.91a11.03 11.03 0 0 1-5.68-5.69m-22 22c-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a11.03 11.03 0 0 1-5.68 5.69c-.37.16-.6.52-.6.91 0 .4.23.76.6.92 2.54 1.11 4.57 3.15 5.68 5.69.16.36.52.6.92.6s.76-.24.92-.6c1.11-2.54 3.14-4.58 5.68-5.69.37-.16.6-.52.6-.92 0-.39-.23-.75-.6-.91a11.03 11.03 0 0 1-5.68-5.69m22 0c-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a11.03 11.03 0 0 1-5.68 5.69c-.37.16-.6.52-.6.91 0 .4.23.76.6.92 2.54 1.11 4.57 3.15 5.68 5.69.16.36.52.6.92.6s.76-.24.92-.6c1.11-2.54 3.14-4.58 5.68-5.69.37-.16.6-.52.6-.92 0-.39-.23-.75-.6-.91a11.03 11.03 0 0 1-5.68-5.69" style="fill:#4d7c0f" transform="translate(-6308.999 -1199.498)"/><path d="M6415.92 1255.03c-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a10.98 10.98 0 0 1-10.08 6.6c-4.51 0-8.39-2.71-10.08-6.6-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a10.98 10.98 0 0 1-10.08 6.6c-4.51 0-8.39-2.71-10.08-6.6-.16-.36-.52-.6-.92-.6s-.76.24-.92.6a10.98 10.98 0 0 1-10.08 6.6c-6.07 0-11-4.92-11-11 0-6.07 4.93-11 11-11 4.51 0 8.39 2.72 10.08 6.61.16.36.52.6.92.6s.76-.24.92-.6a10.99 10.99 0 0 1 10.08-6.61c4.51 0 8.39 2.72 10.08 6.61.16.36.52.6.92.6s.76-.24.92-.6a10.99 10.99 0 0 1 10.08-6.61c4.51 0 8.39 2.72 10.08 6.61.16.36.52.6.92.6s.76-.24.92-.6a10.99 10.99 0 0 1 10.08-6.61c6.07 0 11 4.93 11 11 0 6.08-4.93 11-11 11-4.51 0-8.39-2.71-10.08-6.6" style="fill:#0891b2" transform="translate(-6308.999 -1199.498)"/></svg>

commit cb909ffc955d016f4f9b649237ffca02e8977220

api docs cleanup + readd webhooks: (#1949)
- readd webhooks (regression from #1941)
- set order of tags in docs
- add missing tag to route

backend/btrixcloud/background_jobs.py
@@ -531,7 +531,9 @@ def init_background_jobs_api(
         """Retry background job"""
         return await ops.retry_background_job(job_id, org)
 
-    @app.post("/orgs/all/jobs/retryFailed", response_model=SuccessResponse)
+    @app.post(
+        "/orgs/all/jobs/retryFailed", response_model=SuccessResponse, tags=["jobs"]
+    )
     async def retry_all_failed_background_jobs(user: User = Depends(user_dep)):
         """Retry failed background jobs from all orgs"""
         if not user.is_superuser:

backend/btrixcloud/main.py
@@ -48,6 +48,26 @@ app_root = FastAPI(docs_url=None, redoc_url=None, OPENAPI_URL=OPENAPI_URL)
 db_inited = {"inited": False}
 
 
+tags = [
+    "crawlconfigs",
+    "crawls",
+    "settings",
+    "auth",
+    "users",
+    "organizations",
+    "profiles",
+    "uploads",
+    "all-crawls",
+    "qa",
+    "pages",
+    "collections",
+    "webhooks",
+    "jobs",
+    "invites",
+    "subscriptions",
+]
+
+
 # ============================================================================
 def make_schema():
     """make custom openapi schema"""
@@ -71,6 +91,8 @@ See [https://docs.browsertrix.com/](https://docs.browsertrix.com/) for more info
             "url": "https://www.gnu.org/licenses/agpl-3.0.en.html",
         },
         routes=app_root.routes,
+        webhooks=app_root.webhooks.routes,
+        tags=[{"name": tag} for tag in tags],
     )
     schema["info"]["x-logo"] = {"url": "/docs-logo.svg"}
     return schema
@@ -223,7 +245,7 @@ def main():
 
     app.include_router(org_ops.router)
 
-    @app.get("/settings")
+    @app.get("/settings", tags=["settings"])
     async def get_settings():
         if not db_inited.get("inited"):
             raise HTTPException(status_code=503, detail="not_ready_yet")