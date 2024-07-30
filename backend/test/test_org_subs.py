55923fb7474e1bcd9813d55d8225e6ee5bcb87b3
Tessa Walsh <tessa@bitarchivist.net>
2024-07-30 16:12:44 -0400

    Fix tests

--- backend/test/test_org_subs.py
+++ backend/test/test_org_subs.py
@@ -257,7 +257,7 @@ def test_update_sub(admin_auth_headers):
         "subId": "123",
         "status": "paused_payment_failed",
         "planId": "basic",
-        "futureCancelDate": "2028-12-26T01:02:03",
+        "futureCancelDate": "2028-12-26T01:02:03Z",
         "readOnlyOnCancel": False,
     }
 
@@ -462,7 +462,7 @@ def test_subscription_events_log(admin_auth_headers, non_default_org_id):
             "oid": new_subs_oid,
             "status": "paused_payment_failed",
             "planId": "basic",
-            "futureCancelDate": "2028-12-26T01:02:03",
+            "futureCancelDate": "2028-12-26T01:02:03Z",
         },
         {
             "type": "update",
@@ -521,7 +521,7 @@ def test_subscription_events_log_filter_sub_id(admin_auth_headers):
             "oid": new_subs_oid,
             "status": "paused_payment_failed",
             "planId": "basic",
-            "futureCancelDate": "2028-12-26T01:02:03",
+            "futureCancelDate": "2028-12-26T01:02:03Z",
         },
         {
             "type": "update",
@@ -573,7 +573,7 @@ def test_subscription_events_log_filter_oid(admin_auth_headers):
             "oid": new_subs_oid,
             "status": "paused_payment_failed",
             "planId": "basic",
-            "futureCancelDate": "2028-12-26T01:02:03",
+            "futureCancelDate": "2028-12-26T01:02:03Z",
         },
         {
             "type": "update",
