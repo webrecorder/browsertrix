8eb8896c09634736c82b8849804d7b7f58ecdec7
sua yoo <sua@suayoo.com>
2024-08-20 13:48:16 -0700

    update options

--- frontend/src/features/crawl-workflows/new-workflow-dialog.ts
+++ frontend/src/features/crawl-workflows/new-workflow-dialog.ts
@@ -1,7 +1,8 @@
 import { localized, msg } from "@lit/localize";
-import { css, html, LitElement } from "lit";
+import { html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
+import { TailwindElement } from "@/classes/TailwindElement";
 import seededCrawlSvg from "~assets/images/new-crawl-config_Seeded-Crawl.svg";
 import urlListSvg from "~assets/images/new-crawl-config_URL-List.svg";
 
@@ -12,78 +13,25 @@ export type SelectJobTypeEvent = CustomEvent<"url-list" | "seed-crawl">;
  */
 @localized()
 @customElement("btrix-new-workflow-dialog")
-export class NewWorkflowDialog extends LitElement {
-  // postcss-lit-disable-next-line
-  static styles = css`
-    .title,
-    .container {
-      margin: var(--sl-spacing-large) 0;
-    }
-
-    .container {
-      display: flex;
-      flex-wrap: wrap;
-      gap: var(--sl-spacing-4x-large);
-      justify-content: center;
-    }
-
-    .heading {
-      font-size: var(--sl-font-size-large);
-      font-weight: var(--sl-font-weight-semibold);
-      margin-top: 0;
-      margin-bottom: var(--sl-spacing-small);
-      line-height: 1;
-    }
-
-    .description {
-      color: var(--sl-color-neutral-500);
-      margin: 0;
-    }
-
-    .jobTypeButton {
-      padding: 0.25rem;
-      display: block;
-      width: 16.5rem;
-      cursor: pointer;
-      background: none;
-      text-align: left;
-      border: none;
-      border-radius: 0.75rem;
-    }
-
-    figure {
-      margin: 0;
-      padding: 0;
-    }
-
-    .jobTypeButton:hover .jobTypeImg {
-      transform: scale(1.05);
-    }
-
-    .jobTypeImg {
-      width: 100%;
-      max-height: 9rem;
-      transition-property: transform;
-      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-      transition-duration: 150ms;
-      margin-bottom: var(--sl-spacing-small);
-    }
-  `;
-
+export class NewWorkflowDialog extends TailwindElement {
   @property({ type: Boolean })
   open = false;
 
   render() {
     return html`
       <btrix-dialog
-        .label=${msg("Choose New Workflow Type")}
+        .label=${msg("Create a New Workflow")}
         .open=${this.open}
         style="--width: 46rem"
       >
-        <div class="container">
+        <p class="mb-7 mt-3 text-base text-neutral-700">
+          ${msg("How do you want to specify which pages to crawl?")}
+        </p>
+        <div
+          class="mb-7 flex flex-col items-center justify-center gap-7 md:flex-row md:items-start md:gap-16"
+        >
           <button
-            tabindex="2"
-            class="jobTypeButton"
+            class="group block w-[16.5rem] text-left"
             @click=${() => {
               this.dispatchEvent(
                 new CustomEvent("select-job-type", {
@@ -93,20 +41,24 @@ export class NewWorkflowDialog extends LitElement {
             }}
           >
             <figure>
-              <img class="jobTypeImg" src=${urlListSvg} />
-              <figcaption>
-                <div class="heading">${msg("URL List")}</div>
-                <p class="description">
+              <img
+                class="block transition-transform group-hover:scale-105"
+                src=${urlListSvg}
+              />
+              <figcaption class="p-1">
+                <div class="my-2 text-lg font-semibold leading-none">
+                  ${msg("Predetermined URLs")}
+                </div>
+                <p class="text-balance leading-normal text-neutral-700">
                   ${msg(
-                    "The crawler visits every URL specified in a list, and optionally every URL linked on those pages.",
+                    "Choose this option to crawl a single page, or if you already know the URL of every page you'd like to crawl.",
                   )}
                 </p>
               </figcaption>
             </figure>
           </button>
           <button
-            tabindex="1"
-            class="jobTypeButton"
+            class="group block w-[16.5rem] text-left"
             @click=${() => {
               this.dispatchEvent(
                 new CustomEvent("select-job-type", {
@@ -116,19 +68,104 @@ export class NewWorkflowDialog extends LitElement {
             }}
           >
             <figure>
-              <img class="jobTypeImg" src=${seededCrawlSvg} />
-              <figcaption>
-                <div class="heading">${msg("Seeded Crawl")}</div>
-                <p class="description">
+              <img
+                class="block transition-transform group-hover:scale-105"
+                src=${seededCrawlSvg}
+              />
+              <figcaption class="p-1">
+                <div class="my-2 text-lg font-semibold leading-none">
+                  ${msg("Automated Discovery")}
+                </div>
+                <p class="text-balance leading-normal text-neutral-700">
                   ${msg(
-                    "The crawler automatically discovers and archives pages starting from a single seed URL.",
+                    "Let the crawler automatically discover pages based on a domain or start page that you specify.",
                   )}
                 </p>
               </figcaption>
             </figure>
-          </div>
-        </button>
+          </button>
+        </div>
+        <sl-details
+          summary=${msg("Need more help deciding?")}
+          @sl-hide=${this.stopProp}
+          @sl-after-hide=${this.stopProp}
+        >
+          <p class="mb-3">
+            ${msg(
+              html`Choose <strong>Predetermined URLs</strong> (aka a "URL List"
+                crawl type) if:`,
+            )}
+          </p>
+          <ul class="mb-3 list-disc pl-5">
+            <li>${msg("You want to archive a single page on a website")}</li>
+            <li>
+              ${msg("You're archiving just a few specific pages on a website")}
+            </li>
+            <li>
+              ${msg("You have a list of URLs that you can copy-and-paste")}
+            </li>
+          </ul>
+          <p class="mb-3">
+            ${msg(
+              html`A URL list is simpler to configure, since you don't need to
+              worry about configuring the workflow to exclude parts of the
+              website that you may not want to archive.`,
+            )}
+          </p>
+          <p class="mb-3">
+            ${msg(
+              html`Choose <strong>Automated Discovery</strong> (aka a "Seeded
+                Crawl" crawl type) if:`,
+            )}
+          </p>
+          <ul class="mb-3 list-disc pl-5">
+            <li>${msg("You want to archive an entire website")}</li>
+            <li>
+              ${msg(
+                html`You're archiving a subset of a website, like everything
+                  under <em>website.com/your-username</em>`,
+              )}
+            </li>
+            <li>
+              ${msg(
+                html`You're archiving a website <em>and</em> external pages
+                  linked out of the webpage`,
+              )}
+            </li>
+          </ul>
+          <p class="mb-3">
+            ${msg(
+              html`Seeded crawls are great for advanced use cases where you
+              don't need to know every single URL that you want to archive. You
+              can configure reasonable crawl limits and page limits so that you
+              don't crawl more than you need to.`,
+            )}
+          </p>
+          <p>
+            ${msg(
+              html`Once you choose a crawl type, you can't go back and change
+                it. Check out the
+                <a
+                  class="text-blue-500 hover:text-blue-600"
+                  href="https://docs.browsertrix.com/user-guide/workflow-setup/"
+                  target="_blank"
+                  >crawl workflow setup guide</a
+                >
+                if you still need help deciding on a crawl type, and try our
+                <a
+                  class="text-blue-500 hover:text-blue-600"
+                  href="https://forum.webrecorder.net/c/help/5"
+                  target="_blank"
+                  >community help forum</a
+                >.`,
+            )}
+          </p>
+        </sl-details>
       </btrix-dialog>
     `;
+  }
+
+  private stopProp(e: Event) {
+    e.stopPropagation();
   }
 }
