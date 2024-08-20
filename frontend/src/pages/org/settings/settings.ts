f71413a5df43000d3b73829b02ae195ac252dafb
emma <hi@emma.cafe>
2024-08-20 16:30:57 -0400

    organize tailwind styles

--- frontend/src/pages/org/settings/settings.ts
+++ frontend/src/pages/org/settings/settings.ts
@@ -1,6 +1,7 @@
 import { localized, msg, str } from "@lit/localize";
 import type { SlInput } from "@shoelace-style/shoelace";
 import { serialize } from "@shoelace-style/shoelace/dist/utilities/form.js";
+import clsx from "clsx";
 import { html, type PropertyValues } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import { choose } from "lit/directives/choose.js";
@@ -425,7 +426,22 @@ export class OrgSettings extends BtrixElement {
           <sl-radio-group name="role" label="Role" value=${AccessCode.viewer}>
             <sl-radio
               value=${AccessCode.owner}
-              class="group rounded-md border border-neutral-300 p-2 transition-colors hover:border-neutral-400 attr-[aria-checked=true]:border-primary attr-[aria-checked=true]:bg-primary-50 part-[control]:col-start-1 part-[label]:col-start-1 part-[control]:col-end-2 part-[label]:col-end-3 part-[control]:row-start-1 part-[label]:row-start-1 part-[control]:row-end-2 part-[label]:row-end-3 part-[label]:ml-0 part-[base]:grid part-[label]:grid part-[label]:flex-auto part-[base]:grid-cols-[auto_minmax(0,1fr)] part-[label]:grid-cols-subgrid part-[base]:gap-x-1 part-[label]:gap-y-2"
+              class=${clsx([
+                // Base styles
+                "group rounded-md border border-neutral-300 p-2 transition-colors hover:border-neutral-400",
+
+                // When checked
+                "attr-[aria-checked=true]:border-primary attr-[aria-checked=true]:bg-primary-50",
+
+                // Part: radio control
+                "part-[control]:col-start-1 part-[control]:col-end-2  part-[control]:row-start-1  part-[control]:row-end-2",
+
+                // Part: radio base
+                "part-[base]:grid part-[base]:grid-cols-[auto_minmax(0,1fr)] part-[base]:gap-x-1",
+
+                // Part: radio label
+                "part-[label]:col-start-1 part-[label]:col-end-3 part-[label]:row-start-1 part-[label]:row-end-3 part-[label]:ml-0 part-[label]:grid part-[label]:flex-auto part-[label]:grid-cols-subgrid part-[label]:gap-y-2",
+              ])}
             >
               <div
                 class="col-start-2 flex items-baseline justify-between gap-2"
@@ -440,7 +456,9 @@ export class OrgSettings extends BtrixElement {
                 @sl-after-hide=${this.stopProp}
                 class="col-span-2 part-[content]:p-2 part-[header]:p-2 part-[content]:pt-0 group-attr-[aria-checked=true]:part-[base]:border-primary/50 group-attr-[aria-checked=true]:part-[summary-icon]:text-primary-700"
               >
-                <span slot="summary" class="text-xs">Permissions</span>
+                <span slot="summary" class="text-xs"
+                  >${msg("Permissions")}</span
+                >
                 <ul class="ms-4 list-disc text-xs text-gray-500">
                   <li class="text-warning">${msg("Manage org members")}</li>
                   ${this.appState.settings?.billingEnabled &&
