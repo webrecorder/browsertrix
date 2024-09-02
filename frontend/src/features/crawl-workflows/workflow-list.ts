25d37913e355daafbba35db2e61955e64c903ac5
Tessa Walsh <tessa@bitarchivist.net>
2024-09-02 12:05:18 -0400

    Fix display of workflow last modified date in list

--- frontend/src/features/crawl-workflows/workflow-list.ts
+++ frontend/src/features/crawl-workflows/workflow-list.ts
@@ -376,7 +376,7 @@ export class WorkflowListItem extends LitElement {
             (workflow) => html`
               <sl-format-date
                 lang=${getLocale()}
-                date="${workflow.modified.toString()}Z"
+                date="${workflow.modified}"
                 month="2-digit"
                 day="2-digit"
                 year="2-digit"
