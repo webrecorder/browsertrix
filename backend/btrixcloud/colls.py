7eb0f955893a223f7b7fd5e3203827b1005be54a
Tessa Walsh <tessa@bitarchivist.net>
2024-09-25 13:11:37 -0400

    Fix linting

--- backend/btrixcloud/colls.py
+++ backend/btrixcloud/colls.py
@@ -65,6 +65,7 @@ THUMBNAIL_MAX_SIZE = 2_000_000
 
 
 # ============================================================================
+# pylint: disable=too-many-positional-arguments
 class CollectionOps:
     """ops for working with named collections of crawls"""
 
