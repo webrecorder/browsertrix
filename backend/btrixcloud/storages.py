5bd3f665e786a0cf631b955049626bf7190fefe1
Tessa Walsh <tessa@bitarchivist.net>
2024-09-25 12:16:23 -0400

    Fix another argument

--- backend/btrixcloud/storages.py
+++ backend/btrixcloud/storages.py
@@ -259,7 +259,9 @@ class StorageOps:
 
         asyncio.create_task(
             self.run_post_storage_update_tasks(
-                OrgStorageRefs(storage=prev_storage, storageReplicas=prev_storage_refs),
+                OrgStorageRefs(
+                    storage=prev_storage, storageReplicas=prev_storage_replicas
+                ),
                 storage_refs,
                 org,
             )
