4bdca83feccff9703ed5365ac0c7e4eac88d309d
Tessa Walsh <tessa@bitarchivist.net>
2024-09-26 12:48:29 -0400

    Add provider to s3storage for rclone

--- backend/btrixcloud/models.py
+++ backend/btrixcloud/models.py
@@ -1478,6 +1478,7 @@ class S3StorageIn(BaseModel):
     bucket: str
     access_endpoint_url: Optional[str] = None
     region: str = ""
+    provider: str = "Other"
 
 
 # ============================================================================
@@ -1492,6 +1493,7 @@ class S3Storage(BaseModel):
     secret_key: str
     access_endpoint_url: str
     region: str = ""
+    provider: str = "Other"
 
 
 # ============================================================================
