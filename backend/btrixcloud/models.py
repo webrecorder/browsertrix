14d89d7d924bb446cc351fbd4e542c54134ed463
Tessa Walsh <tessa@bitarchivist.net>
2024-10-15 16:18:42 -0400

    Refactor to move updates to FastAPI background tasks

--- backend/btrixcloud/models.py
+++ backend/btrixcloud/models.py
@@ -2238,6 +2238,13 @@ class UpdatedResponse(BaseModel):
 
 
 # ============================================================================
+class UpdatedResponseId(UpdatedResponse):
+    """Response for API endpoints that return updated + id"""
+
+    id: Optional[str] = None
+
+
+# ============================================================================
 class SuccessResponse(BaseModel):
     """Response for API endpoints that return success"""
 
