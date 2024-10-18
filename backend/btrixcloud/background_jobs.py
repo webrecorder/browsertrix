d8486c73b15c154467c68680ae6392c0b758ce52
Tessa Walsh <tessa@bitarchivist.net>
2024-10-18 01:09:14 -0400

    Fix linting

--- backend/btrixcloud/background_jobs.py
+++ backend/btrixcloud/background_jobs.py
@@ -626,6 +626,7 @@ class BackgroundJobOps:
                         progress.eta = eta_list[0]
 
                 break
+            # pylint: disable=bare-except
             except:
                 continue
 
@@ -859,6 +860,7 @@ def init_background_jobs_api(
     )
     async def get_job_progress(
         job_id: str,
+        # pylint: disable=unused-argument
         org: Organization = Depends(org_crawl_dep),
     ):
         """Return progress information for background job"""
