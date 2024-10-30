55a758f342274d2bcb22e6e210398a00e47ae871
Tessa Walsh <tessa@bitarchivist.net>
2024-10-30 15:33:22 -0400

    Consolidate ops class initialization (#2117)
    Fixes #2111
    
    The background job and operator entrypoints now use a shared function
    that initalizes and returns the ops classes. This is not applied in the
    main entrypoint as that also initializes the backend API, which we don't
    want in the other entrypoints.

--- backend/btrixcloud/main_op.py
+++ backend/btrixcloud/main_op.py
@@ -5,23 +5,10 @@ import sys
 
 from fastapi import FastAPI
 
-from .crawlmanager import CrawlManager
-from .db import init_db
-from .emailsender import EmailSender
 from .operator import init_operator_api
+from .ops import init_ops
 from .utils import register_exit_handler
 
-from .invites import InviteOps
-from .users import init_user_manager
-from .orgs import OrgOps
-from .colls import CollectionOps
-from .crawlconfigs import CrawlConfigOps
-from .crawls import CrawlOps
-from .profiles import ProfileOps
-from .storages import init_storages_api
-from .webhooks import EventWebhookOps
-from .background_jobs import BackgroundJobOps
-from .pages import PageOps
 
 app_root = FastAPI()
 
@@ -29,19 +16,7 @@ app_root = FastAPI()
 # ============================================================================
 # pylint: disable=too-many-function-args, duplicate-code
 def main():
-    """main init"""
-    email = EmailSender()
-    crawl_manager = None
-
-    dbclient, mdb = init_db()
-
-    invite_ops = InviteOps(mdb, email)
-
-    user_manager = init_user_manager(mdb, email, invite_ops)
-
-    org_ops = OrgOps(mdb, invite_ops, user_manager)
-
-    event_webhook_ops = EventWebhookOps(mdb, org_ops)
+    """init operator"""
 
     # pylint: disable=import-outside-toplevel
     if not os.environ.get("KUBERNETES_SERVICE_HOST"):
@@ -51,48 +26,19 @@ def main():
         )
         sys.exit(1)
 
-    crawl_manager = CrawlManager()
-
-    storage_ops = init_storages_api(org_ops, crawl_manager)
-
-    background_job_ops = BackgroundJobOps(
-        mdb, email, user_manager, org_ops, crawl_manager, storage_ops
-    )
-
-    profile_ops = ProfileOps(
-        mdb, org_ops, crawl_manager, storage_ops, background_job_ops
-    )
-
-    crawl_config_ops = CrawlConfigOps(
-        dbclient,
-        mdb,
-        user_manager,
-        org_ops,
-        crawl_manager,
-        profile_ops,
-    )
-
-    user_manager.set_ops(org_ops, crawl_config_ops, None)
-
-    coll_ops = CollectionOps(mdb, crawl_manager, org_ops, event_webhook_ops)
-
-    crawl_ops = CrawlOps(
-        crawl_manager,
-        mdb,
-        user_manager,
+    (
         org_ops,
         crawl_config_ops,
+        _,
+        crawl_ops,
+        page_ops,
         coll_ops,
+        _,
         storage_ops,
-        event_webhook_ops,
         background_job_ops,
-    )
-
-    page_ops = PageOps(mdb, crawl_ops, org_ops, storage_ops)
-
-    crawl_ops.set_page_ops(page_ops)
-
-    background_job_ops.set_ops(crawl_ops, profile_ops)
+        event_webhook_ops,
+        _,
+    ) = init_ops()
 
     return init_operator_api(
         app_root,
