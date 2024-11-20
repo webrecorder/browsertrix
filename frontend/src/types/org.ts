47256fbde182e81bf47eb00a1bb7830a4af0e24c
sua yoo <sua@suayoo.com>
2024-11-20 12:48:18 -0800

    update type

--- frontend/src/types/org.ts
+++ frontend/src/types/org.ts
@@ -93,6 +93,7 @@ export const orgDataSchema = z.object({
   crawlingDefaults: crawlingDefaultsSchema.nullable(),
   allowSharedProxies: z.boolean(),
   allowedProxies: z.array(z.string()),
+  listPublicCollections: z.boolean(),
 });
 export type OrgData = z.infer<typeof orgDataSchema>;
 
