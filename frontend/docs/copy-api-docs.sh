b6eb9ffb27aa49dd50ad9eb02cff088a9e59bdca
Ilya Kreymer <ikreymer@gmail.com>
2024-11-23 23:59:00 -0800

    ci: add plausible to api docs if ENABLE_ANALYTICS is set

--- frontend/docs/copy-api-docs.sh
+++ frontend/docs/copy-api-docs.sh
@@ -6,3 +6,10 @@ mkdir $TARGET
 curl "$DOCS_SOURCE_URL/api/openapi.json" > $TARGET/openapi.json
 curl "$DOCS_SOURCE_URL/api/redoc" > $TARGET/index.html
 curl "$DOCS_SOURCE_URL/docs-logo.svg" > $TARGET/../docs-logo.svg
+
+if [ -n $ENABLE_ANALYTICS ]; then
+  SCRIPT_1='    <script defer data-domain=\"docs.browsertrix.com\" src=\"https://p.webrecorder.net/js/script.outbound-links.js\"></script>'
+  SCRIPT_2='    <script>window.plausible = window.plausible || function () { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>'
+  awk "1;/<head>/{ print \"$SCRIPT_1\"; print \"$SCRIPT_2\" }" $TARGET/index.html > $TARGET/index.html.new
+  mv $TARGET/index.html.new $TARGET/index.html
+fi
