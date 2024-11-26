d55cb8e6a8c82201de5bd74da7d5e47d7bce27cc
Tessa Walsh <tessa@bitarchivist.net>
2024-11-26 17:15:43 -0500

    Add list endpoint to get sorted list of URLs in collection
    Response is sorted desc by page count match and includes an array
    containing page_id, ts, and status for each snapshot with that URL.

--- backend/btrixcloud/models.py
+++ backend/btrixcloud/models.py
@@ -2240,7 +2240,7 @@ class PageWithAllQA(Page):
 class PageOut(Page):
     """Model for pages output, no QA"""
 
-    status: Optional[int] = 200
+    status: int = 200
 
 
 # ============================================================================
@@ -2264,6 +2264,24 @@ class PageNoteUpdatedResponse(BaseModel):
 
     updated: bool
     data: PageNote
+
+
+# ============================================================================
+class PageIdTimestamp(BaseModel):
+    """Simplified model for page info to include in PageUrlCount"""
+
+    pageId: UUID
+    ts: Optional[datetime] = None
+    status: int = 200
+
+
+# ============================================================================
+class PageUrlCount(BaseModel):
+    """Model for counting pages by URL"""
+
+    url: AnyHttpUrl
+    count: int = 0
+    snapshots: List[PageIdTimestamp] = []
 
 
 # ============================================================================
@@ -2512,3 +2530,10 @@ class PaginatedUserEmailsResponse(PaginatedResponse):
     """Response model for user emails with org info"""
 
     items: List[UserEmailWithOrgInfo]
+
+
+# ============================================================================
+class PaginatedPageUrlCountResponse(PaginatedResponse):
+    """Response model for page count by url"""
+
+    items: List[PageUrlCount]
