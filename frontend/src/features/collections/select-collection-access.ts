adf208d3f15c5994c69ccacaf912c19a8341962b
sua yoo <sua@suayoo.com>
2024-11-29 11:02:21 -0800

    update select styles

--- frontend/src/features/collections/select-collection-access.ts
+++ frontend/src/features/collections/select-collection-access.ts
@@ -1,5 +1,5 @@
 import { localized, msg } from "@lit/localize";
-import type { SlChangeEvent, SlIcon, SlSelect } from "@shoelace-style/shoelace";
+import type { SlIcon, SlSelectEvent } from "@shoelace-style/shoelace";
 import { html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
@@ -15,7 +15,7 @@ export class SelectCollectionAccess extends BtrixElement {
   > = {
     [CollectionAccess.Private]: {
       label: msg("Private"),
-      icon: "lock-fill",
+      icon: "lock",
       detail: msg("Only org members can view"),
     },
     [CollectionAccess.Unlisted]: {
@@ -50,27 +50,46 @@ export class SelectCollectionAccess extends BtrixElement {
     }
 
     return html`
-      <sl-select
-        label=${msg("Visibility")}
-        value=${this.value}
-        hoist
-        @sl-change=${(e: SlChangeEvent) => {
-          const { value } = e.currentTarget as SlSelect;
-          this.value = value as CollectionAccess;
-        }}
-      >
-        <sl-icon slot="prefix" name=${selected.icon}></sl-icon>
-        <span slot="suffix" class="whitespace-nowrap">${selected.detail}</span>
-        ${Object.entries(SelectCollectionAccess.Options).map(
-          ([value, { label, icon, detail }]) => html`
-            <sl-option value=${value}>
-              <sl-icon slot="prefix" name=${icon}></sl-icon>
-              ${label}
-              <span slot="suffix">${detail}</span>
-            </sl-option>
-          `,
-        )}
-      </sl-select>
+      <div>
+        <div class="form-label" id="collectionAccessLabel">
+          ${msg("Visibility")}
+        </div>
+        <sl-dropdown
+          class="block w-full"
+          aria-labelledby="collectionAccessLabel"
+          hoist
+          sync="width"
+          @sl-select=${(e: SlSelectEvent) => {
+            const { value } = e.detail.item;
+            this.value = value as CollectionAccess;
+          }}
+        >
+          <sl-button slot="trigger" size="large" class="button-card" caret>
+            <sl-icon
+              slot="prefix"
+              name=${selected.icon}
+              class="size-6"
+            ></sl-icon>
+            <span>${selected.label}</span>
+            <div class="font-normal text-neutral-500">${selected.detail}</div>
+          </sl-button>
+          <sl-menu>
+            ${Object.entries(SelectCollectionAccess.Options).map(
+              ([value, { label, icon, detail }]) => html`
+                <sl-menu-item
+                  value=${value}
+                  type="checkbox"
+                  ?checked=${label === selected.label}
+                >
+                  <sl-icon slot="prefix" name=${icon}></sl-icon>
+                  ${label}
+                  <span slot="suffix">${detail}</span>
+                </sl-menu-item>
+              `,
+            )}
+          </sl-menu>
+        </sl-dropdown>
+      </div>
     `;
   }
 }
