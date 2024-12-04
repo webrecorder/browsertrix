348659939c4573f9b29032370157abe89b261e9c
sua yoo <sua@webrecorder.org>
2024-12-03 19:45:52 -0800

    feat: Make collection public (#2208)

--- frontend/src/features/collections/index.ts
+++ frontend/src/features/collections/index.ts
@@ -2,3 +2,5 @@ import("./collections-add");
 import("./collection-items-dialog");
 import("./collection-metadata-dialog");
 import("./collection-workflow-list");
+import("./select-collection-access");
+import("./share-collection");
