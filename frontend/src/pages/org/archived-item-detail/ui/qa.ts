2d8e6a32cee4021a3b6c546fc77ff3f80d0c771c
Ilya Kreymer <ikreymer@gmail.com>
2024-12-04 21:50:38 -0800

    put 'View error logs' in separate msg()

--- frontend/src/pages/org/archived-item-detail/ui/qa.ts
+++ frontend/src/pages/org/archived-item-detail/ui/qa.ts
@@ -264,15 +264,15 @@ export class ArchivedItemDetailQA extends BtrixElement {
                     ${msg(html`
                       <span class="text-danger">${errorCount}</span>
                       Failed ${pluralOf("pages", errorCount)}
-                      ${errorCount > 0
-                        ? html`—
-                            <a
-                              class="text-primary"
-                              href=${`/orgs/${this.orgSlug}/workflows/${this.workflowId}/crawls/${this.crawlId}#logs`}
-                              >View error logs</a
-                            >`
-                        : ""}
                     `)}
+                    ${errorCount > 0
+                      ? html`—
+                          <a
+                            class="text-primary"
+                            href=${`/orgs/${this.orgSlug}/workflows/${this.workflowId}/crawls/${this.crawlId}#logs`}
+                            >${msg("View error logs")}</a
+                          >`
+                      : ""}
                   </p>
                 </div> `
               : html`
