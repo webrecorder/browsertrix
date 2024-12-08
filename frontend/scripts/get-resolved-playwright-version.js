039e882b52f0bd6fab4afaeb22d4b27f2ad2366a
emma <hi@emma.cafe>
2024-12-07 20:47:43 -0500

    rename `package` to `pkg` to fix prettier error

--- frontend/scripts/get-resolved-playwright-version.js
+++ frontend/scripts/get-resolved-playwright-version.js
@@ -5,7 +5,8 @@ const lockfile = require("@yarnpkg/lockfile");
 let file = fs.readFileSync("yarn.lock", "utf8");
 let json = lockfile.parse(file);
 
-//console.log(
-//  Object.entries(json.object).find(([package]) => package.startsWith("@playwright/test"))[1]
-//    .version,
-//);
+console.log(
+  Object.entries(json.object).find(([pkg]) =>
+    pkg.startsWith("@playwright/test"),
+  )[1].version,
+);
