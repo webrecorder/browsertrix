a98a9fd95dfdb67bbc6479e9d85ab0a9bd935be0
sua yoo <sua@suayoo.com>
2024-12-09 13:12:38 -0800

    revert merge conflict resolution

--- frontend/src/pages/org/archived-item-detail/ui/qa.ts
+++ frontend/src/pages/org/archived-item-detail/ui/qa.ts
@@ -265,6 +265,14 @@ export class ArchivedItemDetailQA extends BtrixElement {
                       <span class="text-danger">${errorCount}</span>
                       Failed ${pluralOf("pages", errorCount)}
                     `)}
+                    ${errorCount > 0
+                      ? html`—
+                          <a
+                            class="text-primary"
+                            href=${`/orgs/${this.orgSlug}/workflows/${this.workflowId}/crawls/${this.crawlId}#logs`}
+                            >${msg("View error logs")}</a
+                          >`
+                      : ""}
                   </p>
                 </div> `
               : html`
