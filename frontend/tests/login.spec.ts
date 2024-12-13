1ad1a998439b26a948c45eb77d5550db77d55c85
emma <hi@emma.cafe>
2024-12-13 17:51:25 -0500

    try super simple fetch??

--- frontend/tests/login.spec.ts
+++ frontend/tests/login.spec.ts
@@ -1,28 +1,33 @@
 import { test } from "@playwright/test";
-import { chromium } from "playwright";
+
+// import { chromium } from "playwright";
 
 test("test", async ({ baseURL }) => {
-  const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+  // const browser = await chromium.launch({ headless: true });
+  // const context = await browser.newContext();
+  // const page = await context.newPage();
 
-  try {
-    console.log({ baseURL });
-    await page.goto(baseURL!);
-    await page.waitForLoadState("load");
-    await page.waitForSelector('input[name="username"]');
-    await page.click('input[name="username"]');
-    await page.fill('input[name="username"]', "dev@webrecorder.net");
-    await page.click('input[name="password"]');
-    const devPassword = process.env.DEV_PASSWORD;
-    if (!devPassword) {
-      throw new Error(
-        "DEV_PASSWORD environment variable is not defined or null.",
-      );
-    }
-    await page.fill('input[name="password"]', devPassword);
-    await page.click('sl-button:has-text("Log In")');
-  } finally {
-    await browser.close();
-  }
+  // trying if a basic fetch works??
+  const res = await fetch(baseURL!);
+  console.log(res.status);
+
+  // try {
+  //   console.log({ baseURL });
+  //   await page.goto(baseURL!);
+  //   await page.waitForLoadState("load");
+  //   await page.waitForSelector('input[name="username"]');
+  //   await page.click('input[name="username"]');
+  //   await page.fill('input[name="username"]', "dev@webrecorder.net");
+  //   await page.click('input[name="password"]');
+  //   const devPassword = process.env.DEV_PASSWORD;
+  //   if (!devPassword) {
+  //     throw new Error(
+  //       "DEV_PASSWORD environment variable is not defined or null.",
+  //     );
+  //   }
+  //   await page.fill('input[name="password"]', devPassword);
+  //   await page.click('sl-button:has-text("Log In")');
+  // } finally {
+  //   await browser.close();
+  // }
 });
