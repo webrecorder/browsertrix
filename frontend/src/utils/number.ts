c3c12c460615a019ffdb30a6a00d85bd82b9b62b
emma <hi@emma.cafe>
2024-12-13 20:03:16 -0500

    add missing ids to ordinal suffix messages

--- frontend/src/utils/number.ts
+++ frontend/src/utils/number.ts
@@ -23,21 +23,27 @@ export function numberFormatter(
   const suffixes: Record<Intl.LDMLPluralRule, string> = {
     zero: msg("th", {
       desc: 'Ordinal suffix for "zero", e.g. 0th (the "th" part)',
+      id: "ordinal-suffix-zero",
     }),
     one: msg("st", {
       desc: 'Ordinal suffix for "one", e.g. 1st (the "st" part)',
+      id: "ordinal-suffix-one",
     }),
     two: msg("nd", {
       desc: 'Ordinal suffix for "two", e.g. 2nd (the "nd" part)',
+      id: "ordinal-suffix-two",
     }),
     few: msg("rd", {
       desc: 'Ordinal suffix for "few", e.g. 3rd (the "rd" part)',
+      id: "ordinal-suffix-few",
     }),
     other: msg("th", {
       desc: 'Ordinal suffix for "other", e.g. 4th (the "th" part)',
+      id: "ordinal-suffix-other",
     }),
     many: msg("th", {
       desc: 'Ordinal suffix for "many" (not used in English, but for example in Gujarati, the "ઠો" part of 6ઠો)',
+      id: "ordinal-suffix-many",
     }),
   };
 
