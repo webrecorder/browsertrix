38a99d4116f9e0917a8ee39783ffafae1ae589f3
sua yoo <sua@suayoo.com>
2024-12-17 12:44:17 -0800

    update not found page

--- frontend/src/components/not-found.ts
+++ frontend/src/components/not-found.ts
@@ -10,24 +10,18 @@ export class NotFound extends BtrixElement {
   render() {
     return html`
       <div class="text-center">
-        <p class="my-4 border-b py-4 text-xl leading-none text-neutral-400">
+        <p class="my-4 border-b py-4 text-xl leading-none text-neutral-500">
           ${msg("Sorry, we couldn’t find that page")}
         </p>
         <p class="text-neutral-600">
           ${msg("Check the URL to make sure you’ve entered it correctly.")}
         </p>
         <div class="my-4">
-          <sl-button
-            href="/"
-            @click=${this.navigate.link}
-            size="small"
-            variant="primary"
-            pill
-            outline
+          <sl-button href="/" @click=${this.navigate.link} size="small"
             >${msg("Go to Home")}</sl-button
           >
         </div>
-        <p class="text-neutral-400">
+        <p class="text-neutral-500">
           ${msg("Did you click a link to get here?")}
           <button
             class="text-cyan-500 transition-colors hover:text-cyan-600"
