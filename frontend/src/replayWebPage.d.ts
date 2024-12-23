5f17dc65387df289bd9ed89fa57c24c8b7de2403
emma <hi@emma.cafe>
2024-12-23 15:28:06 -0500

    add rwp package (for types)

--- frontend/src/replayWebPage.d.ts
+++ frontend/src/replayWebPage.d.ts
@@ -1,17 +1,4 @@
-/**
- * @TODO Import from replaywebpage once https://github.com/webrecorder/replayweb.page/issues/376 is addressed
- *
- * @attr {String} source
- * @attr {String} coll
- * @attr {String} config
- * @attr {String} replayBase
- * @attr {String} noSandbox
- * @attr {String} noCache
- * @attr {String} url
- */
-class ReplayWebPage {
-  fullReload(): void {}
-}
+import type { Embed as ReplayWebPage } from "replaywebpage";
 
 declare global {
   interface HTMLElementTagNameMap {
