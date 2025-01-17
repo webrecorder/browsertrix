4c958225947ebd3220d639df0cdf42a00625ddd6
Tessa Walsh <tessa@bitarchivist.net>
2025-01-16 23:22:11 -0500

    One more pylint disable

--- backend/btrixcloud/crawlconfigs.py
+++ backend/btrixcloud/crawlconfigs.py
@@ -323,7 +323,7 @@ class CrawlConfigOps:
     async def update_crawl_config(
         self, cid: UUID, org: Organization, user: User, update: UpdateCrawlConfig
     ) -> dict[str, bool | str]:
-        # pylint: disable=too-many-locals, too-many-branches
+        # pylint: disable=too-many-locals, too-many-branches, too-many-statements
         """Update name, scale, schedule, and/or tags for an existing crawl config"""
 
         orig_crawl_config = await self.get_crawl_config(cid, org.id)
