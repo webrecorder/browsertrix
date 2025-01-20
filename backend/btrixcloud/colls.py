5f178834de443ff3851a2c1f3f965168028c3cae
sua yoo <sua@suayoo.com>
2025-01-20 12:51:14 -0800

    update sort by

--- backend/btrixcloud/colls.py
+++ backend/btrixcloud/colls.py
@@ -412,7 +412,15 @@ class CollectionOps:
         aggregate = [{"$match": match_query}]
 
         if sort_by:
-            if sort_by not in ("modified", "name", "description", "totalSize"):
+            if sort_by not in (
+                "created",
+                "modified",
+                "dateEarliest",
+                "dateLatest",
+                "name",
+                "description",
+                "totalSize",
+            ):
                 raise HTTPException(status_code=400, detail="invalid_sort_by")
             if sort_direction not in (1, -1):
                 raise HTTPException(status_code=400, detail="invalid_sort_direction")
