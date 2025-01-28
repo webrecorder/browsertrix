0471ed866e9409082f2368903d8a81e995d6d248
Ilya Kreymer <ikreymer@gmail.com>
2025-01-28 00:07:23 -0800

    pause/resume initial impl: - add 'pause' crawl state - turns off crawler pods, and then redis pod when paused - add 'paused' on crawl spec to indicate when crawl is paused - /crawl/<id>/{un}pause apis to toggle 'paused' on crawl spec - ui: add pause/resume button, paused state - ui: add pausing/unpausing derivative states when crawl is running and pausing, or paused and not pausing

--- backend/btrixcloud/crawls.py
+++ backend/btrixcloud/crawls.py
@@ -769,6 +769,31 @@ class CrawlOps(BaseCrawlOps):
 
         return crawls_data
 
+    async def pause_crawl(
+        self, crawl_id: str, org: Organization, pause: bool
+    ) -> Dict[str, bool]:
+        """pause or unpause a crawl temporarily"""
+        crawl = await self.get_base_crawl(crawl_id, org)
+        if crawl and crawl.type != "crawl":
+            raise HTTPException(status_code=400, detail="not_a_crawl")
+
+        result = None
+        try:
+            result = await self.crawl_manager.pause_resume_crawl(crawl_id, pause=pause)
+
+            if result.get("success"):
+                await self.crawls.find_one_and_update(
+                    {"_id": crawl_id, "type": "crawl", "oid": org.id},
+                    {"$set": {"pausing": pause}},
+                )
+
+                return {"success": True}
+        # pylint: disable=bare-except
+        except:
+            pass
+
+        raise HTTPException(status_code=404, detail="crawl_not_found")
+
     async def shutdown_crawl(
         self, crawl_id: str, org: Organization, graceful: bool
     ) -> Dict[str, bool]:
@@ -1241,6 +1266,22 @@ def init_crawls_api(crawl_manager: CrawlManager, app, user_dep, *args):
     )
     async def crawl_graceful_stop(crawl_id, org: Organization = Depends(org_crawl_dep)):
         return await ops.shutdown_crawl(crawl_id, org, graceful=True)
+
+    @app.post(
+        "/orgs/{oid}/crawls/{crawl_id}/pause",
+        tags=["crawls"],
+        response_model=SuccessResponse,
+    )
+    async def pause_crawl(crawl_id, org: Organization = Depends(org_crawl_dep)):
+        return await ops.pause_crawl(crawl_id, org, pause=True)
+
+    @app.post(
+        "/orgs/{oid}/crawls/{crawl_id}/unpause",
+        tags=["crawls"],
+        response_model=SuccessResponse,
+    )
+    async def unpause_crawl(crawl_id, org: Organization = Depends(org_crawl_dep)):
+        return await ops.pause_crawl(crawl_id, org, pause=False)
 
     @app.post(
         "/orgs/{oid}/crawls/delete",
