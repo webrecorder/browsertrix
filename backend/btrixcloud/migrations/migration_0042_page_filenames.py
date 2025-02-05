fb153428395c8b0339572b8acc1643768770c5d2
Tessa Walsh <tessa@bitarchivist.net>
2025-02-05 11:02:11 -0500

    Add current index and total of items to migration

--- backend/btrixcloud/migrations/migration_0042_page_filenames.py
+++ backend/btrixcloud/migrations/migration_0042_page_filenames.py
@@ -27,18 +27,24 @@ class Migration(BaseMigration):
 
         if self.page_ops is None:
             print(
-                "Unable to add filename to pages, missing page_ops",
+                "Unable to add filename and other fields to pages, missing page_ops",
                 flush=True,
             )
             return
 
         crawl_ids_to_update = await pages_mdb.distinct("crawl_id", {"filename": None})
+
+        crawl_count = len(crawl_ids_to_update)
+        current_index = 1
+
         for crawl_id in crawl_ids_to_update:
+            print(f"Migrating archived item {current_index}/{crawl_count}", flush=True)
             try:
                 await self.page_ops.add_crawl_wacz_filename_to_pages(crawl_id)
             # pylint: disable=broad-exception-caught
             except Exception as err:
                 print(
-                    f"Error adding filename to pages in item {crawl_id}: {err}",
+                    f"Error adding filename and other fields to pages in item {crawl_id}: {err}",
                     flush=True,
                 )
+            current_index += 1
