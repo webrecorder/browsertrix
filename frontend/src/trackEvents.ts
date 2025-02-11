8b227a6a090208a0b1046894d82543d1d42bcc63
sua yoo <sua@suayoo.com>
2025-02-11 13:17:52 -0800

    add track calls

--- frontend/src/trackEvents.ts
+++ frontend/src/trackEvents.ts
@@ -3,7 +3,21 @@
  */
 
 export enum AnalyticsTrackEvent {
+  /**
+   * Generic
+   */
   PageView = "pageview",
+  /**
+   * Collections
+   */
   CopyShareCollectionLink = "Copy share collection link",
   DownloadPublicCollection = "Download public collection",
+  /**
+   * Workflows
+   */
+  ExpandWorkflowFormScope = 'Expand workflow form "Scope"',
+  ExpandWorkflowFormLimits = 'Expand workflow form "Limits"',
+  ExpandWorkflowFormBrowserSettings = 'Expand workflow form "Browser Settings"',
+  ExpandWorkflowFormScheduling = 'Expand workflow form "Scheduling"',
+  ExpandWorkflowFormMetadata = 'Expand workflow form "Metadata"',
 }
