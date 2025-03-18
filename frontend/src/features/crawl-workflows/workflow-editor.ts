d2601a037e772c4815fc3d0ffcd484989001da8f
sua yoo <sua@webrecorder.org>
2025-03-18 15:54:04 -0700

    feat: Show running crawl when editing workflow (#2481)
    Part of https://github.com/webrecorder/browsertrix/issues/2366
    
    ## Changes
    
    - Displays latest running crawl status when editing workflow
    - Disables "Run Now" button if crawl is currently running
    
    Currently, clicking "Run Now" will result in a preventable server error
    if the crawl is already running. The change in this PR is in preparation
    for being able to update a currently running crawl and doesn't require
    any backend changes.
    
    ## Manual testing
    
    1. Log in as crawler
    2. Go to edit crawl workflow
    3. Open same workflow in another tab
    4. Run the workflow
    5. Go back to edit tab. Verify "Starting" status is shown next to "Save"
    button and "Run Crawl" button is disabled
    
    ## Screenshots
    
    | Page | Image/video |
    | ---- | ----------- |
    | Edit Workflow | <img width="354" alt="Screenshot 2025-03-11 at 1 34
    07 PM"
    src="https://github.com/user-attachments/assets/02f7fb4a-219d-43a4-bb1f-1f2b40ac1480"
    /> |
    
    
    <!-- ## Follow-ups -->
    
    ---------
    
    Co-authored-by: emma <hi@emma.cafe>

--- frontend/src/features/crawl-workflows/workflow-editor.ts
+++ frontend/src/features/crawl-workflows/workflow-editor.ts
@@ -49,6 +49,7 @@ import {
 } from "@/controllers/observable";
 import { type SelectBrowserProfileChangeEvent } from "@/features/browser-profiles/select-browser-profile";
 import type { CollectionsChangeEvent } from "@/features/collections/collections-add";
+import type { CrawlStatusChangedEventDetail } from "@/features/crawl-workflows/live-workflow-status";
 import type {
   ExclusionChangeEvent,
   QueueExclusionTable,
@@ -224,6 +225,9 @@ export class WorkflowEditor extends BtrixElement {
 
   @state()
   private serverError?: TemplateResult | string;
+
+  @state()
+  private isCrawlRunning: boolean | null = null;
 
   // For observing panel sections position in viewport
   private readonly observable = new ObservableController(this, {
@@ -580,6 +584,7 @@ export class WorkflowEditor extends BtrixElement {
             `
           : nothing}
         ${when(this.serverError, (error) => this.renderErrorAlert(error))}
+        ${when(this.configId, this.renderCrawlStatus)}
 
         <sl-tooltip content=${msg("Save without running")}>
           <sl-button
@@ -592,14 +597,21 @@ export class WorkflowEditor extends BtrixElement {
             ${msg("Save")}
           </sl-button>
         </sl-tooltip>
-        <sl-tooltip content=${msg("Save and run with new settings")}>
+        <sl-tooltip
+          content=${this.isCrawlRunning
+            ? msg("Crawl is already running")
+            : msg("Save and run with new settings")}
+          ?disabled=${this.isCrawlRunning === null}
+        >
           <sl-button
             size="small"
             variant="primary"
             type="submit"
             ?disabled=${isArchivingDisabled(this.org, true) ||
-            this.isSubmitting}
-            ?loading=${this.isSubmitting}
+            this.isSubmitting ||
+            this.isCrawlRunning ||
+            this.isCrawlRunning === null}
+            ?loading=${this.isSubmitting || this.isCrawlRunning === null}
           >
             ${msg(html`Run Crawl`)}
           </sl-button>
@@ -607,6 +619,22 @@ export class WorkflowEditor extends BtrixElement {
       </footer>
     `;
   }
+
+  private readonly renderCrawlStatus = (workflowId: string) => {
+    if (!workflowId) return;
+
+    return html`
+      <btrix-live-workflow-status
+        class="mx-2"
+        workflowId=${workflowId}
+        @btrix-crawl-status-changed=${(
+          e: CustomEvent<CrawlStatusChangedEventDetail>,
+        ) => {
+          this.isCrawlRunning = e.detail.isCrawlRunning;
+        }}
+      ></btrix-live-workflow-status>
+    `;
+  };
 
   private renderSectionHeading(content: TemplateResult | string) {
     return html`
