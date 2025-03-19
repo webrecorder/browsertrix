webrecorder/browsertrix

commit 1bc3c35dd2e9c74dd1f7c1466a273797d7e58043

disable save & run button until crawl status has loaded

frontend/src/features/crawl-workflows/workflow-editor.ts
@@ -601,6 +601,7 @@ export class WorkflowEditor extends BtrixElement {
           content=${this.isCrawlRunning
             ? msg("Crawl is already running")
             : msg("Save and run with new settings")}
+          ?disabled=${this.isCrawlRunning === null}
         >
           <sl-button
             size="small"
@@ -608,8 +609,9 @@ export class WorkflowEditor extends BtrixElement {
             type="submit"
             ?disabled=${isArchivingDisabled(this.org, true) ||
             this.isSubmitting ||
-            this.isCrawlRunning}
-            ?loading=${this.isSubmitting}
+            this.isCrawlRunning ||
+            this.isCrawlRunning === null}
+            ?loading=${this.isSubmitting || this.isCrawlRunning === null}
           >
             ${msg(html`Run Crawl`)}
           </sl-button>

frontend/src/features/crawl-workflows/live-workflow-status.ts
@@ -48,6 +48,19 @@ export class LiveWorkflowStatus extends BtrixElement {
               ),
             );
           }
+        } else {
+          // dispatch status event on first run
+          this.dispatchEvent(
+            new CustomEvent<CrawlStatusChangedEventDetail>(
+              "btrix-crawl-status-changed",
+              {
+                detail: {
+                  isCrawlRunning: workflow.isCrawlRunning,
+                  state: workflow.lastCrawlState,
+                },
+              },
+            ),
+          );
         }
 
         return workflow;

commit b471192cbc3c0ebc361d441a96a870db177043ec

Workflow editor footer button: ensure `isCrawlRunning` is `false` if editing a new workflow (#2496)
Reported by @tw4l 

Quick fix for the bug I introduced in 1bc3c35 in #2481. I didn't
properly test on the workflow editor in a "new workflow" state, and
didn't realize that the component that fetches the workflow state for an
existing workflow wouldn't be rendered for a new workflow, so the update
to the loading state never occurred for new workflows. This fix
explicitly sets `isCrawlRunning` to `false` instead of `null` for new
workflows, so that the loading state isn't displayed.

Tested locally with both new and existing workflows (in both non-running
and running states).

frontend/src/features/crawl-workflows/workflow-editor.ts
@@ -227,7 +227,7 @@ export class WorkflowEditor extends BtrixElement {
   private serverError?: TemplateResult | string;
 
   @state()
-  private isCrawlRunning: boolean | null = null;
+  private isCrawlRunning: boolean | null = this.configId ? null : false;
 
   // For observing panel sections position in viewport
   private readonly observable = new ObservableController(this, {
@@ -330,6 +330,9 @@ export class WorkflowEditor extends BtrixElement {
       } else {
         this.initializeEditor();
       }
+    }
+    if (changedProperties.has("configId")) {
+      this.isCrawlRunning = this.configId ? null : false;
     }
   }
 