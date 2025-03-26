f09425a43fb0f406b72568989cd7c3d15ae9d0df
sua yoo <sua@suayoo.com>
2025-03-25 23:11:22 -0700

    wip inputs

--- frontend/src/features/crawl-workflows/custom-behaviors-table.ts
+++ frontend/src/features/crawl-workflows/custom-behaviors-table.ts
@@ -1,27 +1,32 @@
 import { localized, msg } from "@lit/localize";
+import type { SlChangeEvent, SlSelect } from "@shoelace-style/shoelace";
 import clsx from "clsx";
 import { html, type PropertyValues } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
+import { repeat } from "lit/directives/repeat.js";
 import { when } from "lit/directives/when.js";
+import { nanoid } from "nanoid";
 
 import { TailwindElement } from "@/classes/TailwindElement";
 import { tw } from "@/utils/tailwind";
-
-const GIT_PREFIX = "git+" as const;
 
 enum BehaviorType {
   URL = "url",
   GitRepo = "gitRepo",
 }
 
-type BehaviorURL = {
-  type: BehaviorType.URL;
+type BehaviorBase = {
+  id: string;
+  type: BehaviorType;
   url: string;
 };
 
-type BehaviorGitRepo = {
+type BehaviorURL = BehaviorBase & {
+  type: BehaviorType.URL;
+};
+
+type BehaviorGitRepo = BehaviorBase & {
   type: BehaviorType.GitRepo;
-  url: string;
   path: string;
   branch: string;
 };
@@ -30,7 +35,7 @@ type Behavior = BehaviorURL | BehaviorGitRepo;
 
 const isGitRepo = (url: string) => url.startsWith(GIT_PREFIX);
 
-const parseGitUrl = (fullUrl: string): Omit<BehaviorGitRepo, "type"> => {
+const parseGitUrl = (fullUrl: string): Omit<BehaviorGitRepo, "id" | "type"> => {
   const url = new URL(fullUrl.slice(GIT_PREFIX.length));
 
   return {
@@ -44,6 +49,7 @@ const urlToBehavior = (url: string): Behavior | null => {
   if (isGitRepo(url)) {
     try {
       return {
+        id: nanoid(),
         type: BehaviorType.GitRepo,
         ...parseGitUrl(url),
       };
@@ -53,6 +59,7 @@ const urlToBehavior = (url: string): Behavior | null => {
   }
 
   return {
+    id: nanoid(),
     type: BehaviorType.URL,
     url,
   };
@@ -62,6 +69,10 @@ const labelFor: Record<BehaviorType, string> = {
   [BehaviorType.URL]: msg("URL"),
   [BehaviorType.GitRepo]: msg("Git Repo"),
 };
+
+const inputCellStyles = tw`[--sl-input-background-color:transparent] [--sl-input-border-color-hover:transparent] [--sl-input-border-color:transparent] [--sl-input-border-radius-medium:0] [--sl-input-spacing-medium:var(--sl-spacing-small)]`;
+
+const GIT_PREFIX = "git+" as const;
 
 @customElement("btrix-custom-behaviors-table")
 @localized()
@@ -73,13 +84,16 @@ export class CustomBehaviorsTable extends TailwindElement {
   editable = false;
 
   @state()
-  private rows: Behavior[] = [];
+  private rows = new Map<string, Behavior>();
 
   protected willUpdate(changedProperties: PropertyValues): void {
     if (changedProperties.has("customBehaviors")) {
-      this.rows = this.customBehaviors
-        .map(urlToBehavior)
-        .filter((item): item is Behavior => item !== null);
+      this.rows = new Map(
+        this.customBehaviors
+          .map(urlToBehavior)
+          .filter((item): item is Behavior => item !== null)
+          .map((item) => [item.id, item]),
+      );
     }
   }
 
@@ -107,21 +121,42 @@ export class CustomBehaviorsTable extends TailwindElement {
             `,
           )}
         </btrix-table-head>
-        <btrix-table-body>${this.rows.map(this.renderRow)}</btrix-table-body>
+        <btrix-table-body>
+          ${repeat(
+            this.rows,
+            ([id]) => id,
+            ([_, row]) => this.renderRow(row),
+          )}
+        </btrix-table-body>
       </btrix-table>
+      ${when(
+        this.editable,
+        () => html`
+          <sl-button class="mt-2 w-full" @click=${() => this.addRow()}>
+            <sl-icon slot="prefix" name="plus-lg"></sl-icon>
+            <span class="text-neutral-600">${msg("Add More")}</span>
+          </sl-button>
+        `,
+      )}
     `;
   }
 
   private readonly renderRow = (row: Behavior) => {
     return html`
       <btrix-table-row class="border-t">
-        <btrix-table-cell class="items-start border-r">
+        <btrix-table-cell
+          class=${clsx(
+            this.editable
+              ? tw`h-[var(--sl-input-height-medium)] p-1`
+              : tw`items-start`,
+          )}
+        >
           ${this.renderType(row)}
         </btrix-table-cell>
         <btrix-table-cell
           class=${clsx(
-            tw`block break-all`,
-            row.type === BehaviorType.GitRepo && tw`p-0`,
+            tw`block break-all border-l`,
+            (row.type === BehaviorType.GitRepo || this.editable) && tw`p-0`,
           )}
         >
           ${row.type === BehaviorType.GitRepo
@@ -131,8 +166,12 @@ export class CustomBehaviorsTable extends TailwindElement {
         ${when(
           this.editable,
           () => html`
-            <btrix-table-cell>
-              <sl-icon-button name="trash3"></sl-icon-button>
+            <btrix-table-cell class="border-l p-1">
+              <sl-icon-button
+                class="text-base hover:text-danger"
+                name="trash3"
+                @click=${() => this.removeRow(row.id)}
+              ></sl-icon-button>
             </btrix-table-cell>
           `,
         )}
@@ -149,11 +188,36 @@ export class CustomBehaviorsTable extends TailwindElement {
       <sl-select
         placeholder=${msg("Select Source")}
         size="small"
+        class="w-[8em]"
         value=${row.type}
+        @sl-change=${(e: SlChangeEvent) => {
+          const el = e.target as SlSelect;
+
+          const { id, url } = row;
+
+          this.rows = new Map(
+            this.rows.set(
+              row.id,
+              el.value === BehaviorType.GitRepo
+                ? {
+                    id,
+                    url,
+                    type: BehaviorType.GitRepo,
+                    path: "",
+                    branch: "",
+                  }
+                : {
+                    id,
+                    url,
+                    type: BehaviorType.URL,
+                  },
+            ),
+          );
+        }}
       >
         ${Object.values(BehaviorType).map(
           (behaviorType) => html`
-            <sl-option value=${behaviorType}>
+            <sl-option value=${behaviorType} class="whitespace-nowrap">
               ${labelFor[behaviorType]}
             </sl-option>
           `,
@@ -163,21 +227,50 @@ export class CustomBehaviorsTable extends TailwindElement {
   }
 
   private renderGitRepoCell(row: BehaviorGitRepo) {
-    if (!this.editable) {
-      const labelStyle = tw`fle inline-flex items-center justify-end border-r bg-neutral-50 p-2 text-xs leading-none text-neutral-700`;
+    const subgridStyle = tw`grid grid-cols-[max-content_1fr] border-t`;
+    const labelStyle = tw`fle inline-flex items-center justify-end border-r bg-neutral-50 p-2 text-xs leading-none text-neutral-700`;
+    const pathLabel = msg("Path");
+    const branchLabel = msg("Branch");
 
+    if (!this.editable) {
       return html`
         <div class="break-all p-2">${row.url}</div>
-        <dl class="grid grid-cols-[max-content_1fr] border-t">
-          <dt class=${clsx(labelStyle, tw`border-b`)}>${msg("Path")}</dt>
+        <dl class=${subgridStyle}>
+          <dt class=${clsx(labelStyle, tw`border-b`)}>${pathLabel}</dt>
           <dd class="border-b p-2">${row.path}</dd>
-          <dt class=${labelStyle}>${msg("Branch")}</dt>
+          <dt class=${labelStyle}>${branchLabel}</dt>
           <dd class="p-2">${row.branch}</dd>
         </dl>
       `;
     }
 
-    return html`TODO`;
+    return html`<btrix-url-input
+        placeholder=${msg("Enter URL to Git repository")}
+        class=${clsx(inputCellStyles)}
+        value=${row.url}
+      ></btrix-url-input>
+      <div class=${subgridStyle}>
+        <label class=${clsx(labelStyle, tw`border-b`)}>${pathLabel}</label>
+        <div class="border-b">
+          <sl-input
+            class=${clsx(inputCellStyles)}
+            size="small"
+            value=${row.path}
+            placeholder=${msg("Optional path")}
+            spellcheck="false"
+          ></sl-input>
+        </div>
+        <label class=${labelStyle}>${branchLabel}</label>
+        <div>
+          <sl-input
+            class=${clsx(inputCellStyles)}
+            size="small"
+            value=${row.branch}
+            placeholder=${msg("Optional branch")}
+            spellcheck="false"
+          ></sl-input>
+        </div>
+      </div> `;
   }
 
   private renderUrlCell(row: BehaviorURL) {
@@ -185,6 +278,27 @@ export class CustomBehaviorsTable extends TailwindElement {
       return html`${row.url}`;
     }
 
-    return html`TODO`;
+    return html`<btrix-url-input
+      placeholder=${msg("Enter URL to JavaScript file")}
+      class=${clsx(inputCellStyles)}
+      value=${row.url}
+    ></btrix-url-input>`;
+  }
+
+  private addRow() {
+    const id = nanoid();
+
+    this.rows = new Map(
+      this.rows.set(id, {
+        id,
+        type: BehaviorType.URL,
+        url: "",
+      }),
+    );
+  }
+
+  private removeRow(id: string) {
+    this.rows.delete(id);
+    this.rows = new Map(this.rows);
   }
 }
--- frontend/src/features/crawl-workflows/workflow-editor.ts
+++ frontend/src/features/crawl-workflows/workflow-editor.ts
@@ -1331,6 +1331,7 @@ https://archiveweb.page/images/${"logo.svg"}`}
             "https://example.com/script.js",
             "git+https://git.example.com/custom-behaviors?branch=dev&path=path/to/behaviors",
           ]}
+          editable
         ></btrix-custom-behaviors-table>`,
       )}
       ${this.renderHelpTextCol(
