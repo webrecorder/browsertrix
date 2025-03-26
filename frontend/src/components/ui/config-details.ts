55c6ab443d1874583d36abefdb22c6d0c170ed83
sua yoo <sua@suayoo.com>
2025-03-25 21:19:30 -0700

    update label

--- frontend/src/components/ui/config-details.ts
+++ frontend/src/components/ui/config-details.ts
@@ -478,7 +478,7 @@ export class ConfigDetails extends BtrixElement {
     const selectors = this.crawlConfig?.config.selectLinks || [];
 
     return this.renderSetting(
-      msg("Selectors"),
+      labelFor.selectLink,
       selectors.length
         ? html`
             <div class="mb-2">
