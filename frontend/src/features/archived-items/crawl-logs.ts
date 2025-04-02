6497834ecb35ebf549c95625fe5e5917155e8eec
sua yoo <sua@suayoo.com>
2025-04-02 16:28:01 -0700

    update columns

--- frontend/src/features/archived-items/crawl-logs.ts
+++ frontend/src/features/archived-items/crawl-logs.ts
@@ -10,6 +10,9 @@ import { tw } from "@/utils/tailwind";
 
 enum LogLevel {
   Error = "error",
+  Fatal = "fatal",
+  Debug = "debug",
+  Info = "info",
 }
 
 export type CrawlLog = {
@@ -54,13 +57,13 @@ export class CrawlLogs extends TailwindElement {
   render() {
     if (!this.logs) return;
 
-    const rowClasses = tw`grid grid-cols-[10rem_4rem_20rem_1fr] leading-[1.3]`;
+    const rowClasses = tw`grid grid-cols-[9rem_3rem_20rem_1fr] leading-[1.3]`;
 
-    return html`<btrix-numbered-list class="le text-xs">
+    return html`<btrix-numbered-list class="text-xs">
         <btrix-numbered-list-header slot="header">
           <div class=${rowClasses}>
             <div class="px-2">${msg("Timestamp")}</div>
-            <div class="px-2">${msg("Severity")}</div>
+            <div>${msg("Severity")}</div>
             <div class="px-2">${msg("Message")}</div>
             <div class="px-2">${msg("Page URL")}</div>
           </div>
@@ -69,6 +72,7 @@ export class CrawlLogs extends TailwindElement {
           const selected = this.selectedLog?.index === idx;
           return html`
             <btrix-numbered-list-item
+              class="group"
               hoverable
               ?selected=${selected}
               aria-selected="${selected}"
@@ -82,19 +86,37 @@ export class CrawlLogs extends TailwindElement {
               <div slot="marker">${idx + 1}.</div>
               <div class=${rowClasses}>
                 <div>
-                  <btrix-format-date
-                    date=${log.timestamp}
-                    month="2-digit"
-                    day="2-digit"
-                    year="numeric"
-                    hour="2-digit"
-                    minute="2-digit"
-                    second="2-digit"
-                    hour-format="24"
-                  >
-                  </btrix-format-date>
+                  <sl-tooltip>
+                    <btrix-format-date
+                      date=${log.timestamp}
+                      month="2-digit"
+                      day="2-digit"
+                      year="2-digit"
+                      hour="2-digit"
+                      minute="2-digit"
+                      second="2-digit"
+                      hour-format="24"
+                    >
+                    </btrix-format-date>
+                    <btrix-format-date
+                      slot="content"
+                      date=${log.timestamp}
+                      month="long"
+                      day="numeric"
+                      year="numeric"
+                      hour="numeric"
+                      minute="numeric"
+                      second="numeric"
+                      time-zone-name="short"
+                    >
+                    </btrix-format-date>
+                  </sl-tooltip>
                 </div>
-                <div>${this.renderSeverity(log.logLevel)}</div>
+                <div class="pr-4 text-center">
+                  <sl-tooltip class="capitalize" content=${log.logLevel}>
+                    ${this.renderSeverity(log.logLevel)}
+                  </sl-tooltip>
+                </div>
                 <div class="whitespace-pre-wrap">${log.message}</div>
                 <div class="truncate" title="${log.details.page as string}">
                   <a target="_blank" href="${log.details.page as string}"
@@ -127,16 +149,37 @@ export class CrawlLogs extends TailwindElement {
   }
 
   private renderSeverity(level: LogLevel) {
-    const baseClasses = tw`size-4`;
+    const baseClasses = tw`size-4 group-hover:text-inherit`;
     switch (level) {
-      case LogLevel.Error:
+      case LogLevel.Fatal:
         return html`
           <sl-icon
-            name="exclamation-triangle-fill"
+            name="exclamation-triangle"
             class=${clsx(tw`text-danger-500`, baseClasses)}
           ></sl-icon>
         `;
+      case LogLevel.Error:
+        return html`
+          <sl-icon
+            name="exclamation-circle"
+            class=${clsx(tw`text-danger-500`, baseClasses)}
+          ></sl-icon>
+        `;
+      case LogLevel.Info:
+      case LogLevel.Debug:
+        return html`
+          <sl-icon
+            name="info-circle"
+            class=${clsx(tw`text-blue-500`, baseClasses)}
+          ></sl-icon>
+        `;
       default:
+        return html`
+          <sl-icon
+            name="question-lg"
+            class=${clsx(tw`text-neutral-500`, baseClasses)}
+          ></sl-icon>
+        `;
         break;
     }
   }
@@ -148,6 +191,9 @@ export class CrawlLogs extends TailwindElement {
       <btrix-desc-list>
         <btrix-desc-list-item label=${msg("Timestamp").toUpperCase()}>
           ${this.selectedLog.timestamp}
+        </btrix-desc-list-item>
+        <btrix-desc-list-item label=${msg("Message").toUpperCase()}>
+          ${this.selectedLog.message}
         </btrix-desc-list-item>
         ${Object.entries(details).map(
           ([key, value]) => html`
