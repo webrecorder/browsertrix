aaf58ebcc43127d6d8d0c805b95a46af7355faef
sua yoo <sua@suayoo.com>
2025-04-16 19:37:33 -0700

    clean up

--- frontend/src/stories/Button.stories.ts
+++ frontend/src/stories/Button.stories.ts
@@ -1,32 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
 import { html } from "lit";
-import { ifDefined } from "lit/directives/if-defined.js";
 
-import type { Button as ButtonComponent } from "@/components/ui/button";
-
-import "@/components/ui/button";
-
-const renderButton = ({
-  variant,
-  filled,
-  label,
-  raised,
-  loading,
-  href,
-}: ButtonComponent) => {
-  return html`
-    <btrix-button
-      variant=${ifDefined(variant)}
-      label=${ifDefined(label)}
-      href=${ifDefined(href)}
-      ?filled=${filled}
-      ?raised=${raised}
-      ?loading=${loading}
-    >
-      ${label}
-    </btrix-button>
-  `;
-};
+import { renderButton, RenderProps } from "./Button";
 
 const meta = {
   component: "btrix-button",
@@ -35,19 +10,15 @@ const meta = {
   argTypes: {
     type: {
       control: { type: "select" },
-      options: ["button", "submit"] satisfies ButtonComponent["type"][],
+      options: ["button", "submit"] satisfies RenderProps["type"][],
     },
     variant: {
       control: { type: "select" },
-      options: ["neutral", "danger"] satisfies ButtonComponent["variant"][],
+      options: ["neutral", "danger"] satisfies RenderProps["variant"][],
     },
     size: {
       control: { type: "select" },
-      options: [
-        "x-small",
-        "small",
-        "medium",
-      ] satisfies ButtonComponent["size"][],
+      options: ["x-small", "small", "medium"] satisfies RenderProps["size"][],
     },
   },
   args: {
@@ -59,10 +30,10 @@ const meta = {
       showPanel: false,
     },
   },
-} satisfies Meta<ButtonComponent>;
+} satisfies Meta<RenderProps>;
 
 export default meta;
-type Story = StoryObj<ButtonComponent>;
+type Story = StoryObj<RenderProps>;
 
 export const Raised: Story = {
   args: {
@@ -78,16 +49,32 @@ export const Loading: Story = {
 
 export const Variants: Story = {
   render: () => html`
-    <btrix-button filled variant="neutral">Neutral (Default)</btrix-button>
-    <btrix-button filled variant="danger">Danger</btrix-button>
+    ${renderButton({
+      variant: "neutral",
+      label: "Neutral (Default)",
+      filled: true,
+    })}
+    ${renderButton({ variant: "danger", label: "Danger", filled: true })}
   `,
 };
 
 export const Sizes: Story = {
   render: () => html`
-    <btrix-button filled size="x-small">X-Small</btrix-button>
-    <btrix-button filled size="small">Small</btrix-button>
-    <btrix-button filled size="medium">Medium (Default)</btrix-button>
+    ${renderButton({
+      size: "x-small",
+      label: "X-Small",
+      filled: true,
+    })}
+    ${renderButton({
+      size: "small",
+      label: "Small",
+      filled: true,
+    })}
+    ${renderButton({
+      size: "medium",
+      label: "Medium (Default",
+      filled: true,
+    })}
   `,
 };
 
--- frontend/src/stories/Table.stories.ts
+++ frontend/src/stories/Table.stories.ts
@@ -1,85 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/web-components";
-import { html, type TemplateResult } from "lit";
-import { ifDefined } from "lit/directives/if-defined.js";
+
+import { defaultArgs, RenderProps, renderTable } from "./Table";
 
 import type { Table as TableComponent } from "@/components/ui/table/table";
-
-import "@/components/ui/table";
-
-interface RenderProps {
-  columns: Record<
-    string,
-    {
-      title: string | TemplateResult;
-      classes?: string;
-      renderItem?: (data: Record<string, unknown>) => TemplateResult;
-    }
-  >;
-  rows: {
-    data: Record<string, unknown>;
-    classes?: string;
-  }[];
-}
-
-const columns = {
-  name: {
-    title: "Name",
-  },
-  email: {
-    title: "Email",
-  },
-  role: {
-    title: "Role",
-  },
-  remove: {
-    title: html`<span class="sr-only">Remove</span>`,
-    renderItem: () => html`<sl-icon name="trash3"></sl-icon>`,
-  },
-} satisfies RenderProps["columns"];
-const rows: { data: Omit<Record<keyof typeof columns, unknown>, "remove"> }[] =
-  [
-    {
-      data: {
-        name: "Alice",
-        email: "alice@example.com",
-        role: 40,
-      },
-    },
-    {
-      data: { name: "Bob", email: "bob@example.com", role: 20 },
-    },
-  ] satisfies RenderProps["rows"];
-
-const renderTable = ({ columns: headers, rows: items }: RenderProps) => {
-  return html`
-    <btrix-table>
-      <btrix-table-head>
-        ${Object.values(headers).map(
-          ({ title, classes }) => html`
-            <btrix-table-header-cell class=${ifDefined(classes)}>
-              ${title}
-            </btrix-table-header-cell>
-          `,
-        )}
-      </btrix-table-head>
-      <btrix-table-body>
-        ${items.map(
-          ({ classes, data }) => html`
-            <btrix-table-row class=${ifDefined(classes)}>
-              ${Object.entries(headers).map(
-                ([key, { renderItem }]) => html`
-                  <btrix-table-cell class=${ifDefined(classes)}>
-                    ${renderItem ? renderItem(data) : data[key]}
-                  </btrix-table-cell>
-                `,
-              )}
-            </btrix-table-row>
-          `,
-        )}
-      </btrix-table-body>
-    </btrix-table>
-  `;
-};
 
 const meta = {
   component: "btrix-table",
@@ -92,10 +15,7 @@ const meta = {
     columns: { table: { disable: true } },
     rows: { table: { disable: true } },
   },
-  args: {
-    columns: columns,
-    rows: rows,
-  },
+  args: defaultArgs,
   parameters: {
     options: {
       showPanel: false,
