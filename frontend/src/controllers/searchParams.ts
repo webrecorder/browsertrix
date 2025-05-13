e1445054e24ce6cd87f6027096eb5ff98a626a22
emma <hi@emma.cafe>
2025-05-13 18:18:30 -0400

    add set & delete methods to search params controller - renames `set` method to `update` - adds `set` and `delete` methods that mirror `URLSearchParams` methods

--- frontend/src/controllers/searchParams.ts
+++ frontend/src/controllers/searchParams.ts
@@ -12,7 +12,7 @@ export class SearchParamsController implements ReactiveController {
     return new URLSearchParams(location.search);
   }
 
-  public set(
+  public update(
     update: URLSearchParams | ((prev: URLSearchParams) => URLSearchParams),
     options: { replace?: boolean; data?: unknown } = { replace: false },
   ) {
@@ -22,6 +22,57 @@ export class SearchParamsController implements ReactiveController {
       typeof update === "function"
         ? update(this.searchParams).toString()
         : update.toString();
+
+    if (options.replace) {
+      history.replaceState(options.data, "", url);
+    } else {
+      history.pushState(options.data, "", url);
+    }
+  }
+
+  public set(
+    name: string,
+    value: string,
+    options: { replace?: boolean; data?: unknown } = { replace: false },
+  ) {
+    this.prevParams = new URLSearchParams(this.searchParams);
+    const url = new URL(location.toString());
+    const newParams = new URLSearchParams(this.searchParams);
+    newParams.set(name, value);
+    url.search = newParams.toString();
+
+    if (options.replace) {
+      history.replaceState(options.data, "", url);
+    } else {
+      history.pushState(options.data, "", url);
+    }
+  }
+
+  public delete(
+    name: string,
+    value: string,
+    options?: { replace?: boolean; data?: unknown },
+  ): void;
+  public delete(
+    name: string,
+    options?: { replace?: boolean; data?: unknown },
+  ): void;
+  public delete(
+    name: string,
+    valueOrOptions?: string | { replace?: boolean; data?: unknown },
+    options?: { replace?: boolean; data?: unknown },
+  ) {
+    this.prevParams = new URLSearchParams(this.searchParams);
+    const url = new URL(location.toString());
+    const newParams = new URLSearchParams(this.searchParams);
+    if (typeof valueOrOptions === "string") {
+      newParams.delete(name, valueOrOptions);
+    } else {
+      newParams.delete(name);
+      options = valueOrOptions;
+    }
+    options ??= { replace: false };
+    url.search = newParams.toString();
 
     if (options.replace) {
       history.replaceState(options.data, "", url);
