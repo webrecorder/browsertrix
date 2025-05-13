7b1b1ee55c8b2b913ffd26838f3eeff2d27a6bf1
sua yoo <sua@suayoo.com>
2025-05-13 14:22:03 -0700

    update tooltips

--- frontend/src/strings/archived-items/tooltips.ts
+++ frontend/src/strings/archived-items/tooltips.ts
@@ -1,10 +1,6 @@
 import { msg } from "@lit/localize";
 
 export const tooltipFor = {
-  downloadMultWacz: msg(
-    msg("This will combine all files into a single multi-WACZ file."),
-  ),
-  downloadLogs: msg(
-    "This will download the entire log file, including verbose logging levels that are hidden below.",
-  ),
+  downloadMultWacz: msg(msg("Download Files as Multi-WACZ")),
+  downloadLogs: msg("Download Entire Log File"),
 };
