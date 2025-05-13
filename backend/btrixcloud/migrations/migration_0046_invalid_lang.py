b069dc2c071277ed36546365e0db8e3a15f97bb5
Tessa Walsh <tessa@bitarchivist.net>
2025-05-13 14:30:50 -0400

    Make migration more efficient

--- backend/btrixcloud/migrations/migration_0046_invalid_lang.py
+++ backend/btrixcloud/migrations/migration_0046_invalid_lang.py
@@ -2,12 +2,197 @@
 Migration 0046 - Invalid language codes
 """
 
-from iso639 import is_language
-
 from btrixcloud.migrations import BaseMigration
 
 
 MIGRATION_VERSION = "0046"
+
+ISO_639_1_CODES = [
+    "aa",
+    "ab",
+    "af",
+    "ak",
+    "am",
+    "ar",
+    "an",
+    "as",
+    "av",
+    "ae",
+    "ay",
+    "az",
+    "ba",
+    "bm",
+    "be",
+    "bn",
+    "bi",
+    "bo",
+    "bs",
+    "br",
+    "bg",
+    "ca",
+    "cs",
+    "ch",
+    "ce",
+    "cu",
+    "cv",
+    "kw",
+    "co",
+    "cr",
+    "cy",
+    "da",
+    "de",
+    "dv",
+    "dz",
+    "el",
+    "en",
+    "eo",
+    "et",
+    "eu",
+    "ee",
+    "fo",
+    "fa",
+    "fj",
+    "fi",
+    "fr",
+    "fy",
+    "ff",
+    "gd",
+    "ga",
+    "gl",
+    "gv",
+    "gn",
+    "gu",
+    "ht",
+    "ha",
+    "sh",
+    "he",
+    "hz",
+    "hi",
+    "ho",
+    "hr",
+    "hu",
+    "hy",
+    "ig",
+    "io",
+    "ii",
+    "iu",
+    "ie",
+    "ia",
+    "id",
+    "ik",
+    "is",
+    "it",
+    "jv",
+    "ja",
+    "kl",
+    "kn",
+    "ks",
+    "ka",
+    "kr",
+    "kk",
+    "km",
+    "ki",
+    "rw",
+    "ky",
+    "kv",
+    "kg",
+    "ko",
+    "kj",
+    "ku",
+    "lo",
+    "la",
+    "lv",
+    "li",
+    "ln",
+    "lt",
+    "lb",
+    "lu",
+    "lg",
+    "mh",
+    "ml",
+    "mr",
+    "mk",
+    "mg",
+    "mt",
+    "mn",
+    "mi",
+    "ms",
+    "my",
+    "na",
+    "nv",
+    "nr",
+    "nd",
+    "ng",
+    "ne",
+    "nl",
+    "nn",
+    "nb",
+    "no",
+    "ny",
+    "oc",
+    "oj",
+    "or",
+    "om",
+    "os",
+    "pa",
+    "pi",
+    "pl",
+    "pt",
+    "ps",
+    "qu",
+    "rm",
+    "ro",
+    "rn",
+    "ru",
+    "sg",
+    "sa",
+    "si",
+    "sk",
+    "sl",
+    "se",
+    "sm",
+    "sn",
+    "sd",
+    "so",
+    "st",
+    "es",
+    "sq",
+    "sc",
+    "sr",
+    "ss",
+    "su",
+    "sw",
+    "sv",
+    "ty",
+    "ta",
+    "tt",
+    "te",
+    "tg",
+    "tl",
+    "th",
+    "ti",
+    "to",
+    "tn",
+    "ts",
+    "tk",
+    "tr",
+    "tw",
+    "ug",
+    "uk",
+    "ur",
+    "uz",
+    "ve",
+    "vi",
+    "vo",
+    "wa",
+    "wo",
+    "xh",
+    "yi",
+    "yo",
+    "za",
+    "zh",
+    "zu",
+]
 
 
 # pylint: disable=duplicate-code
@@ -25,56 +210,56 @@ class Migration(BaseMigration):
         the database with "en".
         """
         configs_mdb = self.mdb["crawl_configs"]
+        crawls_mdb = self.mdb["crawls"]
         orgs_mdb = self.mdb["organizations"]
 
-        # Fix workflows
-        async for config_raw in configs_mdb.find({"config.lang": {"$ne": None}}):
-            config_id = config_raw["_id"]
+        # Workflows
+        try:
+            result = await configs_mdb.update_many(
+                {"config.lang": {"$nin": [None, *ISO_639_1_CODES]}},
+                {"$set": {"config.lang": "en"}},
+            )
+            print(
+                f"Fixed invalid language code for {result.modified_count} workflows",
+                flush=True,
+            )
+        # pylint: disable=broad-exception-caught
+        except Exception as err:
+            print(
+                f"Unable to update invalid language codes for crawl workflows: {err}",
+                flush=True,
+            )
 
-            try:
-                lang = config_raw["config"]["lang"]
-            except KeyError:
-                continue
+        # Crawls
+        try:
+            result = await crawls_mdb.update_many(
+                {"config.lang": {"$nin": [None, *ISO_639_1_CODES]}},
+                {"$set": {"config.lang": "en"}},
+            )
+            print(
+                f"Fixed invalid language code for {result.modified_count} crawls",
+                flush=True,
+            )
+        # pylint: disable=broad-exception-caught
+        except Exception as err:
+            print(
+                f"Unable to update invalid language codes for crawls: {err}",
+                flush=True,
+            )
 
-            if not is_language(lang, "pt1"):
-                print(
-                    f"Invalid language code {lang} found for workflow {config_id}. Fixing.",
-                    flush=True,
-                )
-                try:
-                    await configs_mdb.find_one_and_update(
-                        {"_id": config_id},
-                        {"$set": {"config.lang": "en"}},
-                    )
-                # pylint: disable=broad-exception-caught
-                except Exception as err:
-                    print(
-                        f"Unable to update language code for workflow {config_id}: {err}",
-                        flush=True,
-                    )
-
-        # Fix org defaults
-        async for org_raw in orgs_mdb.find({"crawlingDefaults.lang": {"$ne": None}}):
-            oid = org_raw["_id"]
-
-            try:
-                lang = org_raw["crawlingDefaults"]["lang"]
-            except KeyError:
-                continue
-
-            if not is_language(lang, "pt1"):
-                print(
-                    f"Invalid language code {lang} found in org {oid} crawling defaults. Fixing.",
-                    flush=True,
-                )
-                try:
-                    await orgs_mdb.find_one_and_update(
-                        {"_id": oid},
-                        {"$set": {"crawlingDefaults.lang": "en"}},
-                    )
-                # pylint: disable=broad-exception-caught
-                except Exception as err:
-                    print(
-                        f"Unable to update default language code for org {oid}: {err}",
-                        flush=True,
-                    )
+        # Org crawling defaults
+        try:
+            result = await orgs_mdb.update_many(
+                {"crawlingDefaults.lang": {"$nin": [None, *ISO_639_1_CODES]}},
+                {"$set": {"crawlingDefaults.lang": "en"}},
+            )
+            print(
+                f"Fixed invalid language code for {result.modified_count} orgs",
+                flush=True,
+            )
+        # pylint: disable=broad-exception-caught
+        except Exception as err:
+            print(
+                f"Unable to update invalid language codes for org crawling defaults: {err}",
+                flush=True,
+            )
