fec9b8eaf68d20ff27e5e95eb40cc708e32fe424
sua yoo <sua@suayoo.com>
2025-05-14 15:32:08 -0700

    add pause notice

--- frontend/src/components/ui/alert.ts
+++ frontend/src/components/ui/alert.ts
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import { css, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
 
 import { TailwindElement } from "@/classes/TailwindElement";
 
@@ -18,28 +18,42 @@ export class Alert extends TailwindElement {
   @property({ type: String })
   variant: "success" | "warning" | "danger" | "info" = "info";
 
+  @state()
+  open = true;
+
   static styles = css`
     :host {
       display: block;
     }
   `;
 
+  public hide() {
+    // TODO Animate for nicer transition
+    this.open = false;
+  }
+
+  public show() {
+    // TODO Animate for nicer transition
+    this.open = true;
+  }
+
   render() {
-    return html`
-      <div
-        class="${clsx(
-          "px-3 py-2 rounded border",
-          {
-            success: "bg-success-50 text-success-800 border-success-200",
-            warning: "bg-warning-50 text-warning-800 border-warning-200",
-            danger: "bg-danger-50 text-danger-800 border-danger-200",
-            info: "bg-primary-50 text-primary-800 border-primary-200",
-          }[this.variant],
-        )}"
-        role="alert"
-      >
-        <slot></slot>
-      </div>
-    `;
+    if (!this.open) return;
+
+    return html`<div
+      class="${clsx(
+        "px-3 py-2 rounded-lg border",
+        {
+          success: "bg-success-50 text-success-800 border-success-200",
+          warning: "bg-warning-50 text-warning-800 border-warning-200",
+          danger: "bg-danger-50 text-danger-800 border-danger-200",
+          info: "bg-primary-50 text-primary-600 border-primary-100",
+        }[this.variant],
+      )}"
+      role="alert"
+      part="base"
+    >
+      <slot></slot>
+    </div>`;
   }
 }
