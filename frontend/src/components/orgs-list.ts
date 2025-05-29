65d394ac261f369801b961a72447bea6c2cfe721
emma <hi@emma.cafe>
2025-05-29 18:39:31 -0400

    surface trial cancellation dates in the past as errors

--- frontend/src/components/orgs-list.ts
+++ frontend/src/components/orgs-list.ts
@@ -822,7 +822,7 @@ export class OrgsList extends BtrixElement {
                 <sl-icon
                   class="text-base text-danger"
                   name="x-octagon-fill"
-                  label=${msg("Subscription Cancellation Scheduled")}
+                  label=${msg("Issue")}
                 ></sl-icon>`,
               description: html`${msg(
                   "Subscription Cancellation Scheduled in the Past",
@@ -861,29 +861,78 @@ export class OrgsList extends BtrixElement {
           };
           break;
         case SubscriptionStatus.TrialingCanceled:
-          subscription = {
-            icon: html`<sl-icon
-              class="text-base text-neutral-400"
-              name="x-square-fill"
-              label=${msg("Trial Cancelled")}
-            ></sl-icon>`,
-            description: html`<header class="font-medium leading-none">
-                ${msg("Subscription Cancellation Scheduled")}
-              </header>
-              <hr class="my-2" />
-              <div class="mt-2 text-xs">
-                ${msg("Cancels in")}
-                ${this.localize.humanizeDuration(
-                  new Date(org.subscription.futureCancelDate ?? 0).getTime() -
-                    new Date().getTime(),
-                  { compact: true, verbose: true },
+          if (
+            org.subscription.futureCancelDate &&
+            new Date(org.subscription.futureCancelDate).getTime() -
+              new Date().getTime() >=
+              0
+          ) {
+            subscription = {
+              icon: html`<sl-icon
+                class="text-base text-neutral-400"
+                name="x-square-fill"
+                label=${msg("Trial Cancelled")}
+              ></sl-icon>`,
+              description: html`<header class="font-medium leading-none">
+                  ${msg("Trial Cancellation Scheduled")}
+                </header>
+                <hr class="my-2" />
+                <div class="mt-2 text-xs">
+                  ${msg("Cancels in")}
+                  ${this.localize.humanizeDuration(
+                    new Date(org.subscription.futureCancelDate).getTime() -
+                      new Date().getTime(),
+                    { compact: true, verbose: true },
+                  )}
+                  (${this.localize.date(org.subscription.futureCancelDate, {
+                    timeStyle: "medium",
+                    dateStyle: "medium",
+                  })})
+                </div>`,
+            };
+          } else if (
+            org.subscription.futureCancelDate &&
+            new Date(org.subscription.futureCancelDate).getTime() -
+              new Date().getTime() <
+              0
+          ) {
+            subscription = {
+              icon: html`<sl-icon
+                  class="text-base text-neutral-400"
+                  name="x-square-fill"
+                  label=${msg("Trial Cancelled")}
+                ></sl-icon>
+                <sl-icon
+                  class="text-base text-danger"
+                  name="x-octagon-fill"
+                  label=${msg("Issue")}
+                ></sl-icon>`,
+              description: html`${msg(
+                  "Trial Cancellation Scheduled in the Past",
                 )}
-                (${this.localize.date(org.subscription.futureCancelDate, {
-                  timeStyle: "medium",
-                  dateStyle: "medium",
-                })})
-              </div>`,
-          };
+                <div class="mt-2 text-xs">
+                  ${msg("Trial was scheduled for cancellation at")}
+                  ${this.localize.date(org.subscription.futureCancelDate, {
+                    timeStyle: "medium",
+                    dateStyle: "medium",
+                  })}
+                  ${msg("but is still active.")}
+                </div>
+                <div class="my-2 font-bold text-danger-300">
+                  ${msg("This indicates something has gone wrong.")}
+                </div>`,
+            };
+          } else {
+            subscription = {
+              icon: html`<sl-icon
+                class="text-base text-neutral-400"
+                name="x-square-fill"
+                label=${msg("Trial Cancelled")}
+              ></sl-icon>`,
+              description: msg("Trial Cancelled"),
+            };
+          }
+
           break;
         case SubscriptionStatus.PausedPaymentFailed:
           subscription = {
