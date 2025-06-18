efa3b7a6e6bd89450d50adaf5417fe70dcf3a6f9
emma <hi@emma.cafe>
2025-06-18 12:58:27 -0400

    always render `<btrix-pagination>` so that page updates always occur in url

--- frontend/src/pages/org/workflows-list.ts
+++ frontend/src/pages/org/workflows-list.ts
@@ -4,6 +4,7 @@ import type {
   SlDialog,
   SlSelectEvent,
 } from "@shoelace-style/shoelace";
+import clsx from "clsx";
 import { html, type PropertyValues } from "lit";
 import { customElement, query, state } from "lit/decorators.js";
 import { ifDefined } from "lit/directives/if-defined.js";
@@ -513,27 +514,27 @@ export class WorkflowsList extends BtrixElement {
       <btrix-workflow-list>
         ${this.workflows.items.map(this.renderWorkflowItem)}
       </btrix-workflow-list>
-      ${when(
-        total > pageSize,
-        () => html`
-          <footer class="mt-6 flex justify-center">
-            <btrix-pagination
-              page=${page}
-              totalCount=${total}
-              size=${pageSize}
-              @page-change=${async (e: PageChangeEvent) => {
-                await this.fetchWorkflows({
-                  page: e.detail.page,
-                });
+      <footer
+        class=${clsx(
+          tw`mt-6 flex justify-center`,
+          total <= pageSize && tw`hidden`,
+        )}
+      >
+        <btrix-pagination
+          page=${page}
+          totalCount=${total}
+          size=${pageSize}
+          @page-change=${async (e: PageChangeEvent) => {
+            await this.fetchWorkflows({
+              page: e.detail.page,
+            });
 
-                // Scroll to top of list
-                // TODO once deep-linking is implemented, scroll to top of pushstate
-                this.scrollIntoView({ behavior: "smooth" });
-              }}
-            ></btrix-pagination>
-          </footer>
-        `,
-      )}
+            // Scroll to top of list
+            // TODO once deep-linking is implemented, scroll to top of pushstate
+            this.scrollIntoView({ behavior: "smooth" });
+          }}
+        ></btrix-pagination>
+      </footer>
     `;
   }
 
