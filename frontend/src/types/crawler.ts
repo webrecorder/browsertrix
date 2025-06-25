21575867c926a228c3c6addc3cff6a7918c5cda7
sua yoo <sua@suayoo.com>
2025-06-25 13:31:09 -0700

    convert paste to file

--- frontend/src/types/crawler.ts
+++ frontend/src/types/crawler.ts
@@ -59,6 +59,7 @@ export type WorkflowParams = {
   browserWindows: number;
   profileid: string | null;
   config: SeedConfig;
+  seedFile: string | null;
   tags: string[];
   crawlTimeout: number | null;
   maxCrawlSize: number | null;
