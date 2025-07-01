c94c064aa32fed9eb30556d0c296324865bef1ca
Tessa Walsh <tessa@bitarchivist.net>
2025-07-01 12:52:52 -0400

    Modify migration to be more efficient

--- backend/btrixcloud/migrations/migration_0049_recalculate_org_storage.py
+++ backend/btrixcloud/migrations/migration_0049_recalculate_org_storage.py
@@ -16,21 +16,43 @@ class Migration(BaseMigration):
         super().__init__(mdb, migration_version=MIGRATION_VERSION)
 
         self.org_ops = kwargs.get("org_ops")
+        self.coll_ops = kwargs.get("coll_ops")
+        self.file_ops = kwargs.get("file_ops")
 
     async def migrate_up(self):
-        """Perform migration up. Recalculate storage for each org."""
+        """Perform migration up. Add thumbnail and seed file storage to orgs."""
         # pylint: disable=duplicate-code, line-too-long
-        if self.org_ops is None:
-            print("Unable to recalculate org storage, missing org_ops", flush=True)
+        if self.org_ops is None or self.coll_ops is None or self.file_ops is None:
+            print("Unable to recalculate org storage, missing ops", flush=True)
             return
 
         orgs_db = self.mdb["organizations"]
-        async for org_dict in orgs_db.find({}):
+
+        match_query = {
+            "$or": [{"bytesStoredSeedFiles": None}, {"bytesStoredThumbnails": None}]
+        }
+
+        async for org_dict in orgs_db.find(match_query):
             oid = org_dict.get("_id")
 
             try:
                 org = await self.org_ops.get_org_by_id(oid)
-                await self.org_ops.recalculate_storage(org)
+
+                seed_file_size = await self.file_ops.calculate_seed_file_storage(oid)
+                thumbnail_size = await self.coll_ops.calculate_thumbnail_storage(oid)
+
+                await orgs_db.find_one_and_update(
+                    {"_id": oid},
+                    {
+                        "$set": {
+                            "bytesStored": org.bytesStored
+                            + seed_file_size
+                            + thumbnail_size,
+                            "bytesStoredSeedFiles": seed_file_size,
+                            "bytesStoredThumbnails": thumbnail_size,
+                        }
+                    },
+                )
             # pylint: disable=broad-exception-caught
             except Exception as err:
                 print(
