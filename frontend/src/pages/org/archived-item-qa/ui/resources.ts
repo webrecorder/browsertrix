f05f9d8781deed4654abd90b4434951e5df4a479
sua yoo <sua@suayoo.com>
2025-07-16 15:19:40 -0700

    show resources

--- frontend/src/pages/org/archived-item-qa/ui/resources.ts
+++ frontend/src/pages/org/archived-item-qa/ui/resources.ts
@@ -11,18 +11,21 @@ import { tw } from "@/utils/tailwind";
 
 const TOTAL = "Total";
 
-function renderDiff(
+function resourceTable(
   crawlResources: ResourcesPayload["resources"],
-  qaResources: ResourcesPayload["resources"],
+  qaResources?: ResourcesPayload["resources"],
 ) {
   const columns = [
     msg("Resource Type"),
     msg("Good During Crawl"),
     msg("Bad During Crawl"),
-    msg("Good During Analysis"),
-    msg("Bad During Analysis"),
   ];
-  const rows = [
+
+  if (qaResources) {
+    columns.push(msg("Good During Analysis"), msg("Bad During Analysis"));
+  }
+
+  let rows = [
     [
       html`<span class="font-semibold capitalize"
         >${msg("All Resources")}</span
@@ -33,6 +36,11 @@ function renderDiff(
       html`<span class="font-semibold"
         >${localize.number(crawlResources[TOTAL].bad)}</span
       >`,
+    ],
+  ];
+
+  if (qaResources) {
+    rows[0].push(
       html`<span
         class="${clsx(
           "font-semibold",
@@ -53,35 +61,38 @@ function renderDiff(
       >
         ${localize.number(qaResources[TOTAL].bad)}
       </span>`,
-    ],
-    ...Object.keys(qaResources)
-      .filter((key) => key !== TOTAL)
-      .map((key) => [
-        html`<span class="capitalize">${key}</span>`,
-        html`${Object.prototype.hasOwnProperty.call(crawlResources, key)
-          ? localize.number(crawlResources[key].good)
-          : 0}`,
-        html`${Object.prototype.hasOwnProperty.call(crawlResources, key)
-          ? localize.number(crawlResources[key].bad)
-          : 0}`,
-        html`<span
-          class=${Object.prototype.hasOwnProperty.call(crawlResources, key) &&
-          crawlResources[key].good === qaResources[key].good
-            ? tw`text-neutral-400`
-            : tw`text-danger`}
-        >
-          ${localize.number(qaResources[key].good)}
-        </span>`,
-        html`<span
-          class=${Object.prototype.hasOwnProperty.call(crawlResources, key) &&
-          crawlResources[key].bad === qaResources[key].bad
-            ? tw`text-neutral-400`
-            : tw`font-semibold text-danger`}
-        >
-          ${localize.number(qaResources[key].bad)}
-        </span>`,
-      ]),
-  ];
+    );
+    rows = [
+      ...rows,
+      ...Object.keys(qaResources)
+        .filter((key) => key !== TOTAL)
+        .map((key) => [
+          html`<span class="capitalize">${key}</span>`,
+          html`${Object.prototype.hasOwnProperty.call(crawlResources, key)
+            ? localize.number(crawlResources[key].good)
+            : 0}`,
+          html`${Object.prototype.hasOwnProperty.call(crawlResources, key)
+            ? localize.number(crawlResources[key].bad)
+            : 0}`,
+          html`<span
+            class=${Object.prototype.hasOwnProperty.call(crawlResources, key) &&
+            crawlResources[key].good === qaResources[key].good
+              ? tw`text-neutral-400`
+              : tw`text-danger`}
+          >
+            ${localize.number(qaResources[key].good)}
+          </span>`,
+          html`<span
+            class=${Object.prototype.hasOwnProperty.call(crawlResources, key) &&
+            crawlResources[key].bad === qaResources[key].bad
+              ? tw`text-neutral-400`
+              : tw`font-semibold text-danger`}
+          >
+            ${localize.number(qaResources[key].bad)}
+          </span>`,
+        ]),
+    ];
+  }
 
   return html`
     <btrix-data-table
@@ -92,7 +103,33 @@ function renderDiff(
   `;
 }
 
-export function renderResources(crawlData: ReplayData, qaData: ReplayData) {
+function resourceLegend() {
+  return html`
+    <dl>
+      <div class="flex gap-1">
+        <dt class="font-semibold">${msg("Good:")}</dt>
+        <dd>${msg("Success (2xx) and Redirection (3xx) status codes")}</dd>
+      </div>
+      <div class="flex gap-1">
+        <dt class="font-semibold">${msg("Bad:")}</dt>
+        <dd>
+          ${msg("Client error (4xx) and Server error (5xx) status codes")}
+        </dd>
+      </div>
+    </dl>
+  `;
+}
+
+export function renderResources(data: ReplayData) {
+  return html`<div class="flex h-full flex-col">
+    <div class="flex-1 overflow-auto overscroll-contain">
+      ${data?.resources ? resourceTable(data.resources) : renderSpinner()}
+    </div>
+    <footer class="pt-2 text-xs text-neutral-600">${resourceLegend()}</footer>
+  </div>`;
+}
+
+export function renderResourceDiff(crawlData: ReplayData, qaData: ReplayData) {
   // const noData = html`<div
   //   class="m-4 flex flex-col items-center justify-center gap-2 text-xs text-neutral-500"
   // >
@@ -104,23 +141,10 @@ export function renderResources(crawlData: ReplayData, qaData: ReplayData) {
     <div class="flex h-full flex-col">
       <div class="flex-1 overflow-auto overscroll-contain">
         ${crawlData?.resources && qaData?.resources
-          ? renderDiff(crawlData.resources, qaData.resources)
+          ? resourceTable(crawlData.resources, qaData.resources)
           : renderSpinner()}
       </div>
-      <footer class="pt-2 text-xs text-neutral-600">
-        <dl>
-          <div class="flex gap-1">
-            <dt class="font-semibold">${msg("Good:")}</dt>
-            <dd>${msg("Success (2xx) and Redirection (3xx) status codes")}</dd>
-          </div>
-          <div class="flex gap-1">
-            <dt class="font-semibold">${msg("Bad:")}</dt>
-            <dd>
-              ${msg("Client error (4xx) and Server error (5xx) status codes")}
-            </dd>
-          </div>
-        </dl>
-      </footer>
+      <footer class="pt-2 text-xs text-neutral-600">${resourceLegend()}</footer>
     </div>
   `;
 }
