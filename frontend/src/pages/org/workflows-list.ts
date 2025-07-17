f7d5c086ef1cbfa5ae0d87db4449f69891e2e191
sua yoo <sua@suayoo.com>
2025-07-16 19:12:06 -0700

    duplicate from detail

--- frontend/src/pages/org/workflows-list.ts
+++ frontend/src/pages/org/workflows-list.ts
@@ -1073,7 +1073,7 @@ export class WorkflowsList extends BtrixElement {
       });
     } else {
       this.notify.toast({
-        message: msg(str`Copied Workflow to new template.`),
+        message: msg("Copied settings to new workflow."),
         variant: "success",
         icon: "check2-circle",
         id: "workflow-copied-status",
