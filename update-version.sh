649f99f6fc23caa6f86648290e52db8b84c9c46c
Ilya Kreymer <ikreymer@gmail.com>
2025-07-23 00:18:20 -0700

    update browsertrix-emails in update-version.sh

--- update-version.sh
+++ update-version.sh
@@ -12,3 +12,4 @@ sed -E -i "" "s/^version:.*$/version: v$version/" chart/Chart.yaml
 
 sed -E -i "" "s/\/browsertrix-backend:[[:alnum:].-]+/\/browsertrix-backend:$version/" chart/values.yaml
 sed -E -i "" "s/\/browsertrix-frontend:[[:alnum:].-]+/\/browsertrix-frontend:$version/" chart/values.yaml
+sed -E -i "" "s/\/browsertrix-emails:[[:alnum:].-]+/\/browsertrix-emails:$version/" chart/values.yaml
