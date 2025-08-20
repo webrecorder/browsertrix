75bc39e8f5560afb7d4439c195c346293bcb36fc
emma <hi@emma.cafe>
2025-08-20 15:08:27 -0400

    clean up & refactor delete confirmation string template

--- frontend/src/strings/ui.ts
+++ frontend/src/strings/ui.ts
@@ -12,10 +12,21 @@ export const noData = stringFor.noData;
 export const notApplicable = stringFor.notApplicable;
 
 // TODO Refactor all generic confirmation messages to use utility
-export const deleteConfirmation = (name: string | TemplateResult) =>
-  msg(html`
-    Are you sure you want to delete
-    <span class="inline-flex max-w-full">
-      <strong class="inline-flex max-w-full font-semibold">${name}</strong>?
-    </span>
+export const deleteConfirmation = (name: string | TemplateResult) => {
+  const itemName = html`<strong class="inline-flex max-w-full font-semibold"
+    >${name}</strong
+  >`;
+
+  // This is necessary because we need to wrap the item name + the question mark
+  // in a span element for correct layout, which makes translating it tricky.
+  // This simplifies the markup in the translated template, while hopefully still
+  // allowing for enough variation in e.g. punctuation placement when translating.
+  const wrapItemName = (strings: TemplateStringsArray, ...items: unknown[]) =>
+    html`<span class="inline-flex max-w-full"
+      >${html(strings, ...items)}</span
+    >`;
+
+  return msg(html`
+    Are you sure you want to delete ${wrapItemName`${itemName}?`}
   `);
+};
