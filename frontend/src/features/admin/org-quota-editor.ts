80cb91d8ad17837ca53b03be925fabd52dc6a64d
emma <hi@emma.cafe>
2025-09-15 20:00:13 -0400

    add change tracking

--- frontend/src/features/admin/org-quota-editor.ts
+++ frontend/src/features/admin/org-quota-editor.ts
@@ -1,5 +1,5 @@
 import { localized, msg, str } from "@lit/localize";
-import { type SlDialog, type SlInput } from "@shoelace-style/shoelace";
+import { type SlDialog } from "@shoelace-style/shoelace";
 import { html, type PropertyValues } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import { createRef, ref, type Ref } from "lit/directives/ref.js";
@@ -14,6 +14,81 @@ import {
   type GridColumn,
 } from "@/components/ui/data-grid/types";
 import { type OrgData, type OrgQuotas } from "@/utils/orgs";
+import { pluralOf } from "@/utils/pluralize";
+
+const PRESETS = {
+  Starter: {
+    quotas: {
+      maxConcurrentCrawls: 1,
+      maxPagesPerCrawl: 2000,
+      storageQuota: 100_000_000_000,
+      maxExecMinutesPerMonth: 180,
+    },
+    subscriptionIds: ["starter", "starterTest"],
+  },
+  Standard: {
+    quotas: {
+      maxConcurrentCrawls: 2,
+      maxPagesPerCrawl: 5000,
+      storageQuota: 220_000_000_000,
+      maxExecMinutesPerMonth: 360,
+    },
+    subscriptionIds: ["standard", "standardTest"],
+  },
+  Plus: {
+    quotas: {
+      maxConcurrentCrawls: 3,
+      maxPagesPerCrawl: 10000,
+      storageQuota: 500_000_000_000,
+      maxExecMinutesPerMonth: 720,
+    },
+    subscriptionIds: ["plus", "plusTest"],
+  },
+  "Pro Standard": {
+    quotas: {
+      maxConcurrentCrawls: 4,
+      maxPagesPerCrawl: 50_000,
+      storageQuota: 1_000_000_000_000,
+      maxExecMinutesPerMonth: 50 * 60,
+    },
+    subscriptionIds: ["pro-standard-monthly", "pro-standard-yearly"],
+  },
+  "Pro Teams": {
+    quotas: {
+      maxConcurrentCrawls: 5,
+      maxPagesPerCrawl: 100_000,
+      storageQuota: 3_000_000_000_000,
+      maxExecMinutesPerMonth: 80 * 60,
+    },
+    subscriptionIds: ["pro-teams-monthly", "pro-teams-yearly"],
+  },
+  "Pro Plus": {
+    quotas: {
+      maxConcurrentCrawls: 10,
+      maxPagesPerCrawl: 400_000,
+      storageQuota: 5_000_000_000_000,
+      maxExecMinutesPerMonth: 150 * 60,
+    },
+    subscriptionIds: ["pro-plus-monthly", "pro-plus-yearly"],
+  },
+  Unset: {
+    quotas: {
+      maxConcurrentCrawls: 0,
+      maxPagesPerCrawl: 0,
+      storageQuota: 0,
+      maxExecMinutesPerMonth: 0,
+    },
+    subscriptionIds: [],
+  },
+} as const satisfies Record<
+  string,
+  {
+    quotas: {
+      [key in keyof OrgQuotas]?: number;
+    };
+    subscriptionIds?: string[];
+  }
+>;
 
 const LABELS = {
   maxConcurrentCrawls: {
@@ -76,6 +151,9 @@ export class OrgQuotaEditor extends BtrixElement {
   }
 
   render() {
+    const changeCount = Object.values(this.orgQuotaAdjustments).filter(
+      (value) => !!value,
+    ).length;
     return html` <btrix-dialog
       ${ref(this.dialog)}
       .label=${msg(str`Quotas for: ${this.activeOrg?.name || ""}`)}
@@ -103,14 +181,15 @@ export class OrgQuotaEditor extends BtrixElement {
         type Item = (typeof items)[number];
         const columns: GridColumn<Item>[] = [
           {
-            label: msg("Key"),
+            label: msg("Quota"),
             field: "key",
             editable: false,
             width: "2fr",
             renderCell: ({ item }) => html`${LABELS[item.key].label}`,
+            align: "start",
           },
           {
-            label: msg("Current Value"),
+            label: msg("Value"),
             field: "current",
             editable: false,
             width: "1fr",
@@ -118,39 +197,26 @@ export class OrgQuotaEditor extends BtrixElement {
               const key = item.key;
               const value = item.current;
               const labelConfig = LABELS[key];
-              const format = (v: number, isInitialValue = true) => {
-                if (v <= 0)
-                  return isInitialValue
-                    ? html`<span class="text-sm text-neutral-400"
-                        >${msg("Unlimited")}</span
-                      >`
-                    : html`<span class="text-sm text-neutral-400"
-                        >${msg("Unset")}</span
-                      >`;
-                const fn =
-                  labelConfig.type === "bytes"
-                    ? this.localize.bytes
-                    : this.localize.number;
-                return fn(v);
-              };
+              const format = (v: number, isInitialValue = true) =>
+                this.format(v, labelConfig.type, isInitialValue);
 
               return this.orgQuotaAdjustments[key]
                 ? Math.sign(this.orgQuotaAdjustments[key]) === 1
-                  ? html`<span>
+                  ? html`<span class="tabular-nums">
                       <b>${format(value + this.orgQuotaAdjustments[key])}</b>
-                      <span class="inline-block"
-                        >(${format(value, false)}
-                        <span class="text-green-600">
-                          + ${format(this.orgQuotaAdjustments[key])}</span
+                      <span class="inline-block">
+                        (${format(value, false)}
+                        <span class="whitespace-nowrap text-green-600">
+                          +&nbsp;${format(this.orgQuotaAdjustments[key])}</span
                         >)</span
                       ></span
                     >`
-                  : html`<span>
+                  : html`<span class="tabular-nums">
                       <b>${format(value + this.orgQuotaAdjustments[key])}</b>
-                      <span class="inline-block"
-                        >(${format(value, false)}
-                        <span class="text-red-600">
-                          - ${format(-this.orgQuotaAdjustments[key])}</span
+                      <span class="inline-block">
+                        (${format(value, false)}
+                        <span class="whitespace-nowrap text-red-600">
+                          -&nbsp;${format(-this.orgQuotaAdjustments[key])}</span
                         >)</span
                       ></span
                     >`
@@ -167,7 +233,78 @@ export class OrgQuotaEditor extends BtrixElement {
             step: 1,
           },
         ];
+
         return html`
+          <label
+            class="font-sm font-medium text-neutral-500"
+            for="org-quota-presets"
+            >${msg("Presets")}</label
+          >
+          <btrix-overflow-scroll class="-mx-4 part-[content]:px-4">
+            <sl-button-group id="org-quota-presets">
+              ${(Object.entries(PRESETS) as Entries<typeof PRESETS>).map(
+                ([key, value]) => {
+                  const isCurrentSubscription = (
+                    value.subscriptionIds as string[]
+                  ).includes(this.activeOrg?.subscription?.planId ?? "");
+                  return html`<btrix-popover placement="top">
+                    <sl-button
+                      @click=${() => {
+                        const newQuota: Partial<OrgQuotas> = {};
+                        (
+                          Object.entries(value.quotas) as Entries<
+                            typeof value.quotas
+                          >
+                        ).forEach(([k, v]) => {
+                          newQuota[k] = v - quotas[k];
+                        });
+                        this.orgQuotaAdjustments = { ...newQuota };
+                      }}
+                      variant=${isCurrentSubscription ? "primary" : "default"}
+                    >
+                      ${key}
+                      ${isCurrentSubscription
+                        ? html`<sl-icon
+                            name="credit-card-fill"
+                            slot="suffix"
+                          ></sl-icon>`
+                        : null}
+                    </sl-button>
+                    <div slot="content">
+                      <header class="mb-2 font-medium">
+                        ${key}${isCurrentSubscription
+                          ? html` -
+                              <b class="text-primary-600"
+                                >${msg("This is the current subscription.")}</b
+                              >`
+                          : null}
+                      </header>
+
+                      <hr class="my-2" />
+                      <table>
+                        <tbody>
+                          ${(
+                            Object.entries(value.quotas) as Entries<
+                              typeof value.quotas
+                            >
+                          ).map(
+                            ([key, value]) => html`
+                              <tr>
+                                <td class="pr-2">${LABELS[key].label}</td>
+                                <td class="pr-2">
+                                  ${this.format(value, LABELS[key].type)}
+                                </td>
+                              </tr>
+                            `,
+                          )}
+                        </tbody>
+                      </table>
+                    </div>
+                  </btrix-popover>`;
+                },
+              )}
+            </sl-button-group>
+          </btrix-overflow-scroll>
           <btrix-data-grid
             editCells
             .columns=${columns}
@@ -190,69 +327,41 @@ export class OrgQuotaEditor extends BtrixElement {
       })}
 
       <div slot="footer" class="flex justify-end">
+        <div class="px-4 py-2 text-xs text-neutral-700">
+          ${this.localize.number(changeCount)}
+          ${pluralOf("changes", changeCount)}
+        </div>
         <sl-button
           size="small"
           @click="${this.onSubmitQuotas}"
+          ?disabled=${changeCount === 0}
           variant="primary"
           >${msg("Update Quotas")}
         </sl-button>
       </div>
     </btrix-dialog>`;
-
-    // (Object.entries(quotas) as [keyof OrgQuotas, number][]).map(
-    //             ([key, value]) => {
-    //               let label: string;
-    //               switch (key) {
-    //                 case "maxConcurrentCrawls":
-    //                   label = msg("Max Concurrent Crawls");
-    //                   break;
-    //                 case "maxPagesPerCrawl":
-    //                   label = msg("Max Pages Per Crawl");
-    //                   break;
-    //                 case "storageQuota":
-    //                   label = msg("Org Storage Quota (GB)");
-    //                   value = Math.floor(value / 1e9);
-    //                   break;
-    //                 case "maxExecMinutesPerMonth":
-    //                   label = msg("Max Execution Minutes Per Month");
-    //                   break;
-    //                 case "extraExecMinutes":
-    //                   label = msg("Extra Execution Minutes");
-    //                   break;
-    //                 case "giftedExecMinutes":
-    //                   label = msg("Gifted Execution Minutes");
-    //                   break;
-    //                 default:
-    //                   label = msg("Unlabeled");
-    //               }
-    //               return html` ${msg("Current")}: ${value}
-    //                 <sl-input
-    //                   class="mb-3 last:mb-0"
-    //                   name=${key}
-    //                   label=${label}
-    //                   value="0"
-    //                   type="number"
-    //                   @sl-input="${this.onUpdateQuota}"
-    //                 ></sl-input>`;
-    //             },
-    //           ),
   }
 
-  private onUpdateQuota(e: CustomEvent) {
-    const inputEl = e.target as SlInput;
-    const name = inputEl.name as keyof OrgData["quotas"];
-    const quotas = this.activeOrg?.quotas;
-    if (quotas) {
-      if (name === "storageQuota") {
-        quotas[name] = Number(inputEl.value) * 1e9;
-      } else {
-        quotas[name] = Number(inputEl.value);
-      }
-    }
+  private format(v: number, type: "bytes" | "number", isInitialValue = true) {
+    if (v <= 0)
+      return isInitialValue
+        ? html`<span class="text-xs text-neutral-600">${msg("Unset")}</span>`
+        : html`<span class="text-xs text-neutral-400">${msg("0")}</span>`;
+    const fn = type === "bytes" ? this.localize.bytes : this.localize.number;
+    return fn(v);
   }
 
   private onSubmitQuotas() {
     if (this.activeOrg) {
+      (
+        Object.entries(this.orgQuotaAdjustments) as Entries<
+          typeof this.orgQuotaAdjustments
+        >
+      )
+        .filter(Boolean)
+        .forEach(([key, value]) => {
+          this.activeOrg!.quotas[key] += value ?? 0;
+        });
       this.dispatchEvent(
         new CustomEvent("update-quotas", {
           detail: this.activeOrg,
