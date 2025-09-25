85c8691420839ba5cd37331a30e75b7dd11d8c66
Ilya Kreymer <ikreymer@gmail.com>
2025-09-24 22:05:19 -0700

    add import job, minimally working update all models add dedupCollIndex on Crawl and CrawlConfig update crds

--- backend/btrixcloud/operator/collindexes.py
+++ backend/btrixcloud/operator/collindexes.py
@@ -1,16 +1,18 @@
 """Operator handler for CollIndexes"""
 
 from typing import Literal, get_args
+import re
+
 from uuid import UUID
 from pydantic import BaseModel
 
 from btrixcloud.utils import str_to_date
 
-from .models import MCSyncData, POD
+from .models import MCSyncData, POD, JOB, CMAP
 from .baseoperator import BaseOperator
 
 
-TYPE_INDEX_STATES = Literal["initing", "updating", "ready"]
+TYPE_INDEX_STATES = Literal["initing", "importing", "ready"]
 INDEX_STATES = get_args(TYPE_INDEX_STATES)
 
 
@@ -20,7 +22,7 @@ class CollIndexStatus(BaseModel):
 
     state: TYPE_INDEX_STATES = "initing"
 
-    lastCollUpdated: str
+    lastCollUpdated: str = ""
 
 
 # ============================================================================
@@ -30,7 +32,7 @@ class CollIndexSpec(BaseModel):
     id: UUID
     oid: UUID
 
-    collItemsUpdatedAt: str
+    collItemsUpdatedAt: str = ""
 
 
 # ============================================================================
@@ -42,12 +44,11 @@ class CollIndexOperator(BaseOperator):
     def __init__(self, *args):
         super().__init__(*args)
         self.shared_params.update(self.k8s.shared_params)
-        self.shared_params["redis_storage"] = self.shared_params[
-            "redis_coll_index_storage"
-        ]
-        self.shared_params["memory"] = "3Gi"
+        self.shared_params["redis_storage"] = "5Gi"
+        self.shared_params["memory"] = "1Gi"
         self.shared_params["cpu"] = self.shared_params["redis_cpu"]
         self.shared_params["init_redis"] = True
+        self.shared_params["obj_type"] = "coll"
 
     def init_routes(self, app):
         """init routes for this operator"""
@@ -66,34 +67,103 @@ class CollIndexOperator(BaseOperator):
         status = CollIndexStatus(**data.parent.get("status", {}))
 
         index_id = str(spec.id)
-        redis_name = "coll-redis-" + index_id
-        new_children = self._load_redis(index_id, redis_name)
+        redis_name = "redis-coll-" + index_id
+        new_children = self.load_redis(index_id, redis_name)
 
-        redis_exists = redis_name in data.children[POD]
-        if not redis_exists:
+        redis = None
+        if redis_name in data.children[POD]:
+            redis = await self.k8s.get_redis_connected("coll-" + index_id)
+        else:
             status.state = "initing"
-        # elif self.is_import_needed(spec, status):
-        #    import_job_name "coll-import-job-" + index_id
-        #    new_children.extend(self._load_import_job(index_id, import_job_name))
+
+        import_ts = self.get_import_ts(spec, status)
+        if import_ts:
+            import_job_name = f"import-{index_id}-{import_ts}"
+            new_children.extend(await self.load_import_job(index_id, import_job_name))
+            new_children.extend(
+                await self.load_import_configmap(
+                    index_id, import_job_name, spec.oid, data.children
+                )
+            )
+            status.state = "importing"
+
+        if redis:
+            # attempt to set the last updated from redis when done
+            try:
+                last_update_ts = await redis.get("last_update_ts")
+                if last_update_ts:
+                    status.lastCollUpdated = last_update_ts
+
+                # index is ready!
+                if not data.children[JOB]:
+                    status.state = "ready"
+
+            # pylint: disable=broad-exception-caught
+            except Exception as e:
+                print(e)
 
         return {
             "status": status.dict(exclude_none=True),
             "children": new_children,
         }
 
-    def is_import_needed(self, spec: CollIndexSpec, status: CollIndexStatus):
+    def get_import_ts(self, spec: CollIndexSpec, status: CollIndexStatus):
         """returnt rue if a reimport is needed based on last import date"""
         coll_update_date = str_to_date(spec.collItemsUpdatedAt)
+        if not coll_update_date:
+            return None
+
         last_import_date = str_to_date(status.lastCollUpdated)
-        if not coll_update_date or not last_import_date:
-            return True
+        # do update from 'coll_update_date' timestamp
+        if not last_import_date or coll_update_date >= last_import_date:
+            return re.sub(r"[^0-9]", "", spec.collItemsUpdatedAt)
 
-        return coll_update_date >= last_import_date
+        return None
 
-    def _load_redis(self, index_id: str, name: str):
+    def load_redis(self, index_id: str, name: str):
+        """create redis pods from yaml template"""
         params = {}
         params.update(self.shared_params)
         params["name"] = name
         params["id"] = index_id
 
         return self.load_from_yaml("redis.yaml", params)
+
+    async def load_import_job(self, index_id: str, name: str):
+        """create indexer pods from yaml template"""
+        params = {}
+        params.update(self.shared_params)
+        params["name"] = name
+        params["id"] = index_id
+        params["crawler_image"] = self.crawl_config_ops.get_channel_crawler_image(
+            "default"
+        )
+
+        params["redis_url"] = self.k8s.get_redis_url("coll-" + index_id)
+
+        return self.load_from_yaml("index-import-job.yaml", params)
+
+    async def load_import_configmap(
+        self, index_id: str, name: str, oid: UUID, children
+    ):
+        """create configmap for import job, lookup resources only on first init"""
+        configmap = children[CMAP].get(name)
+        # pylint: disable=duplicate-code
+        if configmap:
+            metadata = configmap["metadata"]
+            configmap["metadata"] = {
+                "name": metadata["name"],
+                "namespace": metadata["namespace"],
+                "labels": metadata["labels"],
+            }
+            return [configmap]
+
+        replay_list = await self.coll_ops.get_internal_replay_list(UUID(index_id), oid)
+
+        params = {}
+        params.update(self.shared_params)
+        params["name"] = name
+        params["id"] = index_id
+        params["config"] = replay_list.json()
+
+        return self.load_from_yaml("index-import-configmap.yaml", params)
