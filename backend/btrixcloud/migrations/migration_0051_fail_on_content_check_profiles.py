40c7b6b5944a0f37400ab5eb903a67990eb97e6a
Tessa Walsh <tessa@bitarchivist.net>
2025-09-26 17:28:25 -0400

    Add backend validation and migration for failOnContentCheck (#2844)
    Fixes #2838 
    
    - Ensure `failOnContentCheck` cannot be set when a browser profile is
    not set in crawlconfig POST and PATCH endpoints, else return 400 with
    detail `fail_on_content_check_requires_profile`
    - Add migration to unset `failOnContentCheck` for any workflows in the
    database that currently have that flag enabled without a profile
    configured

---
+++ backend/btrixcloud/migrations/migration_0051_fail_on_content_check_profiles.py
@@ -0,0 +1,37 @@
+"""
+Migration 0051 - Ensure failOnContentCheck is not set for workflows without profiles
+"""
+
+from btrixcloud.migrations import BaseMigration
+
+
+MIGRATION_VERSION = "0051"
+
+
+# pylint: disable=duplicate-code
+class Migration(BaseMigration):
+    """Migration class."""
+
+    # pylint: disable=unused-argument
+    def __init__(self, mdb, **kwargs):
+        super().__init__(mdb, migration_version=MIGRATION_VERSION)
+
+    async def migrate_up(self) -> None:
+        """Perform migration up.
+
+        Unset failOnContentCheck for workflows that don't have a profile set
+        """
+        crawl_configs_mdb = self.mdb["crawl_configs"]
+
+        try:
+            await crawl_configs_mdb.update_many(
+                {"profileid": None, "config.failOnContentCheck": True},
+                {"$set": {"config.failOnContentCheck": False}},
+            )
+
+        # pylint: disable=broad-exception-caught
+        except Exception as err:
+            print(
+                f"Error unsetting failOnContentCheck for configs without profiles: {err}",
+                flush=True,
+            )
