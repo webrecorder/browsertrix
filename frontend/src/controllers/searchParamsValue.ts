a318f103c3ee43127646d80f523ab5ac78a42027
emma <hi@emma.cafe>
2025-09-29 02:19:54 -0400

    add info on how to use `SearchParamsValue`

--- frontend/src/controllers/searchParamsValue.ts
+++ frontend/src/controllers/searchParamsValue.ts
@@ -2,6 +2,54 @@ import { type ReactiveController, type ReactiveControllerHost } from "lit";
 
 import { SearchParamsController } from "@/controllers/searchParams";
 
+/**
+ * Persists arbitrary state to the URL search params via an encoder and decoder.
+ *
+ * Automatically updates the URL search params when the value changes, and
+ * updates the host when the URL search params change. Updates to the value push
+ * state to the history stack, and so browser history navigation can be used to
+ * navigate between states.
+ *
+ * ## Usage
+ *
+ * As a reactive controller, the class must be passed a host as the first
+ * argument. The second argument is the encoder, which receives the current
+ * (incoming) value and the current URLSearchParams object, and returns a
+ * modified URLSearchParams object that represents the new value. The third
+ * argument is the decoder, which receives the current URLSearchParams object
+ * and converts this into a valid value.
+ *
+ * To access the value, use the `value` property. Setting the value will update
+ * the URL search params and push state to the history stack, and reading it is
+ * cached, so performance should be good no matter what.
+ *
+ * There is currently one option available via a third parameter, `initial`,
+ * which can be used to modify the initial value based on the URL search params
+ * at the time of initialization. This is useful if there's a default value that
+ * should be used if the URL search params are empty, but otherwise the value
+ * from the URL should be prioritized, for example.
+ *
+ * Example usage:
+ * ```ts
+ * class MyComponent extends LitElement {
+ *   query = new SearchParamsValue<string>(
+ *     this,
+ *     (value, params) => {
+ *       if (value) {
+ *         params.set("q", value);
+ *       } else {
+ *         params.delete("q");
+ *       }
+ *       return params;
+ *     },
+ *     (params) => params.get("q") ?? ""
+ *   );
+ *   render() {
+ *     return html`<input .value=${this.query.value} @input=${(e) => {this.query.value = e.target.value}} />`;
+ *   }
+ * }
+ * ```
+ */
 export class SearchParamsValue<T> implements ReactiveController {
   host: ReactiveControllerHost;
 
