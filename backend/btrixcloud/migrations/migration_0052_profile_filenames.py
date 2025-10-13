a091cfcfd0aee936133a3204e99ef8dcaaeda967
Tessa Walsh <tessa@bitarchivist.net>
2025-10-13 15:32:04 -0400

    Migration: delete replicas in db for profiles, spawn replica jobs

--- backend/btrixcloud/migrations/migration_0052_profile_filenames.py
+++ backend/btrixcloud/migrations/migration_0052_profile_filenames.py
@@ -17,14 +17,25 @@ class Migration(BaseMigration):
     def __init__(self, mdb, **kwargs):
         super().__init__(mdb, migration_version=MIGRATION_VERSION)
 
+        self.background_job_ops = kwargs.get("background_job_ops")
+
     async def migrate_up(self) -> None:
         """Perform migration up.
 
-        Add oid prefix to profile resource filenames that don't already have it
+        Add oid prefix to profile resource filenames that don't already have it.
+        For any profiles that match, also delete the database record for any
+        existing replicas and then spawn new replication jobs.
         """
         profiles_mdb = self.mdb["profiles"]
 
         match_query = {"resource.filename": {"$regex": r"^profiles"}}
+
+        if self.background_job_ops is None:
+            print(
+                f"Unable to start migration {MIGRATION_VERSION}, ops class missing",
+                flush=True,
+            )
+            return
 
         async for profile_res in profiles_mdb.find(match_query):
             profile = Profile.from_dict(profile_res)
@@ -33,16 +44,33 @@ class Migration(BaseMigration):
 
             existing_filename = profile.resource.filename
             oid = str(profile.oid)
+            new_filename = f"{oid}/{existing_filename}"
 
             if not existing_filename.startswith(oid):
                 try:
                     await profiles_mdb.find_one_and_update(
                         {"_id": profile.id},
-                        {"$set": {"resource.filename": f"{oid}/{existing_filename}"}},
+                        {
+                            "$set": {
+                                "resource.filename": new_filename,
+                                "resource.replicas": None,
+                            }
+                        },
+                    )
+
+                    profile.resource.filename = new_filename
+                    profile.resource.replicas = None
+
+                    print(
+                        f"Starting background jobs to replicate profile {profile.id}",
+                        flush=True,
+                    )
+                    await self.background_job_ops.create_replica_jobs(
+                        profile.oid, profile.resource, str(profile.id), "profile"
                     )
                 # pylint: disable=broad-exception-caught
                 except Exception as err:
                     print(
-                        f"Error updating filename for profile {profile.name} ({profile.id}): {err}",
+                        f"Error fixing filename and replicas for profile {profile.id}: {err}",
                         flush=True,
                     )
