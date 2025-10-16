58b0edfd14c0f45cb55e0697ede8576e919d5098
Tessa Walsh <tessa@bitarchivist.net>
2025-10-16 11:47:59 -0400

    Fix profile filenames in database and re-replicate (#2892)
    Fixes #2891 
    
    - Fix profile filenames in database to be full path in bucket (including
    org id prefix) for new profiles
    - Add migration to fix profile filenames that don't already have org id
    prefix, and for each found, delete existing replicas from database
    (files were never pushed to s3) and then run background jobs to
    replicate
    - Add `--error-on-no-transfer` flag to rclone replication job to fail
    with non-0 exit code if source file isn't found or another issue
    prevents copy from being successful
    
    ---------
    
    Co-authored-by: Ilya Kreymer <ikreymer@gmail.com>

---
+++ backend/btrixcloud/migrations/migration_0052_profile_filenames.py
@@ -0,0 +1,76 @@
+"""
+Migration 0052 - Fix profile filenames in db to be full path from bucket
+"""
+
+from btrixcloud.migrations import BaseMigration
+from btrixcloud.models import Profile
+
+
+MIGRATION_VERSION = "0052"
+
+
+# pylint: disable=duplicate-code
+class Migration(BaseMigration):
+    """Migration class."""
+
+    # pylint: disable=unused-argument
+    def __init__(self, mdb, **kwargs):
+        super().__init__(mdb, migration_version=MIGRATION_VERSION)
+
+        self.background_job_ops = kwargs.get("background_job_ops")
+
+    async def migrate_up(self) -> None:
+        """Perform migration up.
+
+        Add oid prefix to profile resource filenames that don't already have it.
+        For any profiles that match, also delete the database record for any
+        existing replicas and then spawn new replication jobs.
+        """
+        profiles_mdb = self.mdb["profiles"]
+
+        match_query = {"resource.filename": {"$regex": r"^profiles"}}
+
+        if self.background_job_ops is None:
+            print(
+                f"Unable to start migration {MIGRATION_VERSION}, ops class missing",
+                flush=True,
+            )
+            return
+
+        async for profile_res in profiles_mdb.find(match_query):
+            profile = Profile.from_dict(profile_res)
+            if not profile.resource:
+                continue
+
+            existing_filename = profile.resource.filename
+            oid = str(profile.oid)
+            new_filename = f"{oid}/{existing_filename}"
+
+            if not existing_filename.startswith(oid):
+                try:
+                    await profiles_mdb.find_one_and_update(
+                        {"_id": profile.id},
+                        {
+                            "$set": {
+                                "resource.filename": new_filename,
+                                "resource.replicas": [],
+                            }
+                        },
+                    )
+
+                    profile.resource.filename = new_filename
+                    profile.resource.replicas = []
+
+                    print(
+                        f"Starting background jobs to replicate profile {profile.id}",
+                        flush=True,
+                    )
+                    await self.background_job_ops.create_replica_jobs(
+                        profile.oid, profile.resource, str(profile.id), "profile"
+                    )
+                # pylint: disable=broad-exception-caught
+                except Exception as err:
+                    print(
+                        f"Error fixing filename and replicas for profile {profile.id}: {err}",
+                        flush=True,
+                    )
