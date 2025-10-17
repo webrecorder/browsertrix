90d055f7879d42d2e9ff48ab5f9137120b1f68a7
Ilya Kreymer <ikreymer@users.noreply.github.com>
2025-10-16 18:25:33 -0700

    Fix crawl running filter being passed incorrectly to backend (#2904) (#2905)
    Quick fix for an issue where the default state of the crawl running
    filter on the workflows list page was incorrectly filtering out running
    crawls. (1.19.4 patch release)
    
    Co-authored-by: Emma Segal-Grossman <hi@emma.cafe>

--- frontend/src/pages/org/workflows-list.ts
+++ frontend/src/pages/org/workflows-list.ts
@@ -101,7 +101,7 @@ type FilterBy = {
   name?: string;
   firstSeed?: string;
   schedule?: boolean;
-  isCrawlRunning?: boolean;
+  isCrawlRunning?: true;
 };
 
 /**
@@ -190,7 +190,7 @@ export class WorkflowsList extends BtrixElement {
               params.set(key, value[key] ? "true" : "false");
               break;
             case "isCrawlRunning":
-              if (value[key]) {
+              if (value[key] as true | undefined) {
                 params.set(key, "true");
               } else {
                 params.delete(key);
@@ -208,7 +208,7 @@ export class WorkflowsList extends BtrixElement {
         schedule: params.has("schedule")
           ? params.get("schedule") === "true"
           : undefined,
-        isCrawlRunning: params.get("isCrawlRunning") === "true",
+        isCrawlRunning: params.get("isCrawlRunning") === "true" || undefined,
       };
     },
   );
