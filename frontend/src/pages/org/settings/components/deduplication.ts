431df44dd7f305dea5a183f7d5c55b824f76530d
sua yoo <sua@suayoo.com>
2025-10-27 19:26:15 -0700

    add section

---
+++ frontend/src/pages/org/settings/components/deduplication.ts
@@ -0,0 +1,13 @@
+import { localized } from "@lit/localize";
+import { html } from "lit";
+import { customElement } from "lit/decorators.js";
+
+import { BtrixElement } from "@/classes/BtrixElement";
+
+@customElement("btrix-org-settings-deduplication")
+@localized()
+export class OrgSettingsDeduplication extends BtrixElement {
+  render() {
+    return html`TODO`;
+  }
+}
