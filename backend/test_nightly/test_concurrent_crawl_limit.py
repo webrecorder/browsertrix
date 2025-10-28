fc9cbb7e4ce2d18376197cd723426d677c7d2803
Ilya Kreymer <ikreymer@gmail.com>
2025-10-28 14:06:05 -0700

    fix tests?

--- backend/test_nightly/test_concurrent_crawl_limit.py
+++ backend/test_nightly/test_concurrent_crawl_limit.py
@@ -1,6 +1,5 @@
 import requests
 import time
-import pytest
 
 from .conftest import API_PREFIX
 from .utils import get_crawl_status
@@ -21,11 +20,11 @@ def test_set_concurrent_crawl_limit(org_with_quotas, admin_auth_headers):
 
 def test_run_two_only_one_concurrent(org_with_quotas, admin_auth_headers):
     global crawl_id_a
-    crawl_id_a = run_crawl(org_with_quotas, admin_auth_headers)
+    crawl_id_a, _ = run_crawl(org_with_quotas, admin_auth_headers)
     time.sleep(1)
 
     global crawl_id_b
-    crawl_id_b = run_crawl(org_with_quotas, admin_auth_headers)
+    crawl_id_b, _ = run_crawl(org_with_quotas, admin_auth_headers)
 
     while get_crawl_status(org_with_quotas, crawl_id_a, admin_auth_headers) in (
         "starting",
@@ -50,9 +49,18 @@ def test_run_two_only_one_concurrent(org_with_quotas, admin_auth_headers):
         == "waiting_org_limit"
     )
 
-    # additional waiting crawls not allowed due to limit
-    with pytest.raises(requests.exceptions.HTTPError):
-        run_crawl(org_with_quotas, admin_auth_headers)
+    crawl_id, res = run_crawl(org_with_quotas, admin_auth_headers)
+    assert not crawl_id
+    assert res["errorDetail"] == "slow_down_too_many_crawls_queued"
+
+    config_id = res["id"]
+
+    r = requests.post(
+        f"{API_PREFIX}/orgs/{org_with_quotas}/crawlconfigs/{config_id}/run",
+        headers=admin_auth_headers,
+    )
+    assert r.status_code == 429
+
 
 def test_cancel_and_run_other(org_with_quotas, admin_auth_headers):
     r = requests.post(
@@ -107,5 +115,4 @@ def run_crawl(org_id, headers):
     )
     r.raise_for_status()
     data = r.json()
-
-    return data["run_now_job"]
+    return data["run_now_job"], data
