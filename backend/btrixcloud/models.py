aab6616ec8d0c03ce7f1c2b9b1438ffccb9fa4f2
emma <hi@emma.cafe>
2025-10-30 00:13:10 -0400

    add endpoint for additional minute pricing

--- backend/btrixcloud/models.py
+++ backend/btrixcloud/models.py
@@ -1981,6 +1981,14 @@ class SubscriptionPortalUrlResponse(BaseModel):
 
 
 # ============================================================================
+class AddonMinutesPricing(BaseModel):
+    """Addon minutes pricing"""
+
+    price: float
+    currency: str
+
+
+# ============================================================================
 class CheckoutAddonMinutesRequest(BaseModel):
     """Request for additional minutes checkout session"""
 
