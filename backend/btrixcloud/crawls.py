webrecorder/browsertrix

commit 9ab038a2755bb0ca67465b02094c7b392180f552

Optimize Concurrent Crawl Check (#2945)
Use DB and query first N non-completed crawls (by start time), instead
of querying all k8s crawljob objects.
Add additional index on crawls optimized for this query.

Follow-up to #2940 to make concurrent crawler queue efficient even when
there is no limit set.

backend/btrixcloud/crawls.py
@@ -121,7 +121,13 @@ class CrawlOps(BaseCrawlOps):
         await self.crawls.create_index(
             [("type", pymongo.HASHED), ("fileSize", pymongo.DESCENDING)]
         )
-
+        await self.crawls.create_index(
+            [
+                ("state", pymongo.ASCENDING),
+                ("oid", pymongo.ASCENDING),
+                ("started", pymongo.ASCENDING),
+            ]
+        )
         await self.crawls.create_index([("finished", pymongo.DESCENDING)])
         await self.crawls.create_index([("oid", pymongo.HASHED)])
         await self.crawls.create_index([("cid", pymongo.HASHED)])
@@ -335,6 +341,18 @@ class CrawlOps(BaseCrawlOps):
             crawls.append(crawl)
 
         return crawls, total
+
+    async def get_active_crawls(self, oid: UUID, limit: int) -> list[str]:
+        """get list of waiting crawls, sorted from earliest to latest"""
+        res = (
+            self.crawls.find(
+                {"state": {"$in": RUNNING_AND_WAITING_STATES}, "oid": oid}, {"_id": 1}
+            )
+            .sort({"started": 1})
+            .limit(limit)
+        )
+        res_list = await res.to_list()
+        return [res["_id"] for res in res_list]
 
     async def delete_crawls(
         self,

backend/btrixcloud/operator/crawls.py
@@ -21,7 +21,6 @@ from btrixcloud.models import (
     TYPE_NON_RUNNING_STATES,
     TYPE_RUNNING_STATES,
     TYPE_ALL_CRAWL_STATES,
-    NON_RUNNING_STATES,
     RUNNING_STATES,
     WAITING_STATES,
     RUNNING_AND_STARTING_ONLY,
@@ -53,7 +52,6 @@ from .models import (
     CMAP,
     PVC,
     CJS,
-    BTRIX_API,
 )
 
 
@@ -258,7 +256,7 @@ class CrawlOperator(BaseOperator):
                     return self._empty_response(status)
 
         if status.state in ("starting", "waiting_org_limit"):
-            if not await self.can_start_new(crawl, data, status):
+            if not await self.can_start_new(crawl, status):
                 return self._empty_response(status)
 
             await self.set_state(
@@ -738,20 +736,10 @@ class CrawlOperator(BaseOperator):
 
     def get_related(self, data: MCBaseRequest):
         """return objects related to crawl pods"""
-        spec = data.parent.get("spec", {})
-        crawl_id = spec["id"]
-        oid = spec.get("oid")
-        # filter by role as well (job vs qa-job)
-        role = data.parent.get("metadata", {}).get("labels", {}).get("role")
-        related_resources = [
-            {
-                "apiVersion": BTRIX_API,
-                "resource": "crawljobs",
-                "labelSelector": {"matchLabels": {"btrix.org": oid, "role": role}},
-            },
-        ]
-
+        related_resources = []
         if self.k8s.enable_auto_resize:
+            spec = data.parent.get("spec", {})
+            crawl_id = spec["id"]
             related_resources.append(
                 {
                     "apiVersion": METRICS_API,
@@ -765,7 +753,6 @@ class CrawlOperator(BaseOperator):
     async def can_start_new(
         self,
         crawl: CrawlSpec,
-        data: MCSyncData,
         status: CrawlStatus,
     ):
         """return true if crawl can start, otherwise set crawl to 'queued' state
@@ -774,27 +761,17 @@ class CrawlOperator(BaseOperator):
         if not max_crawls:
             return True
 
+        next_active_crawls = await self.crawl_ops.get_active_crawls(
+            crawl.oid, max_crawls
+        )
+
         # if total crawls < concurrent, always allow, no need to check further
-        if len(data.related[CJS]) <= max_crawls:
+        if len(next_active_crawls) < max_crawls:
             return True
 
-        name = data.parent.get("metadata", {}).get("name")
-
-        # assume crawls already sorted from oldest to newest
-        # (seems to be the case always)
-        i = 0
-        for crawl_sorted in data.related[CJS].values():
-            # if crawl not running, don't count
-            if crawl_sorted.get("status", {}).get("state") in NON_RUNNING_STATES:
-                continue
-
-            # if reached current crawl, if did not reach crawl quota, allow current crawl to run
-            if crawl_sorted.get("metadata").get("name") == name:
-                if i < max_crawls:
-                    return True
-
-                break
-            i += 1
+        # allow crawl if within first list of active crawls
+        if crawl.id in next_active_crawls:
+            return True
 
         await self.set_state(
             "waiting_org_limit", status, crawl, allowed_from=["starting"]

commit 90e59e5ef69ea3212ca27d2b1cc03ccf3df6aa4f

storage quota size check fix: - add get_active_crawls_size() from db to check size of current crawls, instead of relying on removed related object - fixes regression caused by #2945 bump version to 1.19.7

backend/btrixcloud/crawls.py
@@ -354,6 +354,20 @@ class CrawlOps(BaseCrawlOps):
         res_list = await res.to_list()
         return [res["_id"] for res in res_list]
 
+    async def get_active_crawls_size(self, oid: UUID) -> int:
+        """get size of all running crawls"""
+        cursor = self.crawls.aggregate(
+            [
+                {"$match": {"state": {"$in": RUNNING_AND_WAITING_STATES}, "oid": oid}},
+                {"$group": {"_id": None, "totalSum": {"$sum": "$stats.size"}}},
+            ]
+        )
+        results = await cursor.to_list(length=1)
+        if not results:
+            return 0
+
+        return results[0].get("totalSum") or 0
+
     async def delete_crawls(
         self,
         org: Organization,

backend/btrixcloud/operator/crawls.py
@@ -51,7 +51,6 @@ from .models import (
     POD,
     CMAP,
     PVC,
-    CJS,
 )
 
 
@@ -1031,7 +1030,7 @@ class CrawlOperator(BaseOperator):
 
             # update stats and get status
             return await self.update_crawl_state(
-                redis, crawl, status, pods, pod_done_count, data
+                redis, crawl, status, pods, pod_done_count
             )
 
         # pylint: disable=broad-except
@@ -1397,7 +1396,7 @@ class CrawlOperator(BaseOperator):
         return True
 
     async def is_crawl_stopping(
-        self, crawl: CrawlSpec, status: CrawlStatus, data: MCSyncData
+        self, crawl: CrawlSpec, status: CrawlStatus
     ) -> Optional[StopReason]:
         """check if crawl is stopping and set reason"""
         # if user requested stop, then enter stopping phase
@@ -1428,19 +1427,12 @@ class CrawlOperator(BaseOperator):
             return "stopped_org_readonly"
 
         if org.quotas.storageQuota:
-            running_crawls_total_size = status.size
-            for crawl_job in data.related[CJS].values():
-                # if the job id matches current crawl job, then skip
-                # this job to avoid double-counting
-                # using the more up-to-date 'status.size' for this job
-                if crawl_job.get("spec", {}).get("id") == crawl.id:
-                    continue
+            active_crawls_total_size = await self.crawl_ops.get_active_crawls_size(
+                crawl.oid
+            )
+            print("SIZE", active_crawls_total_size)
 
-                crawl_status = crawl_job.get("status", {})
-                if crawl_status:
-                    running_crawls_total_size += crawl_status.get("size", 0)
-
-            if self.org_ops.storage_quota_reached(org, running_crawls_total_size):
+            if self.org_ops.storage_quota_reached(org, active_crawls_total_size):
                 return "stopped_storage_quota_reached"
 
         # gracefully stop crawl is execution time quota is reached
@@ -1479,7 +1471,6 @@ class CrawlOperator(BaseOperator):
         status: CrawlStatus,
         pods: dict[str, dict],
         pod_done_count: int,
-        data: MCSyncData,
     ) -> CrawlStatus:
         """update crawl state and check if crawl is now done"""
         results = await redis.hgetall(f"{crawl.id}:status")
@@ -1536,7 +1527,7 @@ class CrawlOperator(BaseOperator):
             await redis.delete(f"{crawl.id}:paused")
 
         if not status.stopping:
-            status.stopReason = await self.is_crawl_stopping(crawl, status, data)
+            status.stopReason = await self.is_crawl_stopping(crawl, status)
             status.stopping = status.stopReason is not None
 
             # mark crawl as stopping

backend/btrixcloud/version.py
@@ -1,3 +1,3 @@
 """current version"""
 
-__version__ = "1.19.6"
+__version__ = "1.19.7"

chart/Chart.yaml
@@ -5,7 +5,7 @@ type: application
 icon: https://webrecorder.net/assets/icon.png
 
 # Browsertrix and Chart Version
-version: v1.19.6
+version: v1.19.7
 
 dependencies:
   - name: btrix-admin-logging

chart/values.yaml
@@ -106,7 +106,7 @@ replica_deletion_delay_days: 0
 
 # API Image
 # =========================================
-backend_image: "docker.io/webrecorder/browsertrix-backend:1.19.6"
+backend_image: "docker.io/webrecorder/browsertrix-backend:1.19.7"
 backend_pull_policy: "IfNotPresent"
 
 backend_password_secret: "PASSWORD!"
@@ -171,7 +171,7 @@ cleanup_files_after_minutes: 1440
 
 # Emails Image
 # =========================================
-emails_image: "docker.io/webrecorder/browsertrix-emails:1.19.6"
+emails_image: "docker.io/webrecorder/browsertrix-emails:1.19.7"
 emails_pull_policy: "IfNotPresent"
 
 emails_cpu: "10m"
@@ -185,7 +185,7 @@ local_emails_port: 30872
 
 # Nginx Image
 # =========================================
-frontend_image: "docker.io/webrecorder/browsertrix-frontend:1.19.6"
+frontend_image: "docker.io/webrecorder/browsertrix-frontend:1.19.7"
 frontend_pull_policy: "IfNotPresent"
 
 frontend_cpu: "10m"