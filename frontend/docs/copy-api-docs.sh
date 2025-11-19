673b8388f8c4d56ff0b7e527b1f8441c4820d9ad
Ilya Kreymer <ikreymer@gmail.com>
2025-11-18 18:27:37 -0800

    remove docs prefix

--- frontend/docs/copy-api-docs.sh
+++ frontend/docs/copy-api-docs.sh
@@ -5,6 +5,8 @@ TARGET=$CURR/docs/api/
 mkdir $TARGET
 curl "$DOCS_SOURCE_URL/api/openapi.json" > $TARGET/openapi.json
 curl "$DOCS_SOURCE_URL/api/redoc" > $TARGET/index.html
+#remove /docs prefix for now
+awk '{ gsub(/\/docs\//, "/"); print }' $TARGET/index.html
 curl "$DOCS_SOURCE_URL/docs-logo.svg" > $TARGET/../docs-logo.svg
 
 if [ -n $ENABLE_ANALYTICS ]; then
