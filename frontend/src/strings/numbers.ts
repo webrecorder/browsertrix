1363990cc50bbc8cf8f69891b804f89644c9443b
emma <hi@emma.cafe>
2025-12-01 16:31:36 -0500

    return "0%" when value is actually 0

--- frontend/src/strings/numbers.ts
+++ frontend/src/strings/numbers.ts
@@ -1,5 +1,6 @@
 export function renderPercentage(ratio: number) {
   const percent = ratio * 100;
+  if (percent === 0) return `0%`;
   if (percent < 1) return `<1%`;
   return `${percent.toFixed(2)}%`;
 }
