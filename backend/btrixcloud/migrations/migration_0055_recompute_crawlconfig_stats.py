754517ac7ff3acdaf70d57472bf6e22c68417a0a
Tessa Walsh <tessa@bitarchivist.net>
2025-12-01 16:34:15 -0500

    Add migration to recompute workflow stats

---
+++ backend/btrixcloud/migrations/migration_0055_recompute_crawlconfig_stats.py
@@ -0,0 +1,35 @@
+"""
+Migration 0055 - Recompute workflow crawl stats
+"""
+
+from btrixcloud.crawlconfigs import stats_recompute_all
+from btrixcloud.migrations import BaseMigration
+
+
+MIGRATION_VERSION = "0055"
+
+
+class Migration(BaseMigration):
+    """Migration class."""
+
+    # pylint: disable=unused-argument
+    def __init__(self, mdb, **kwargs):
+        super().__init__(mdb, migration_version=MIGRATION_VERSION)
+
+    async def migrate_up(self):
+        """Perform migration up.
+
+        Recompute crawl workflow stats to fix issue with failed crawls
+        being added to successfulCrawlCount and workflow size totals.
+        """
+        # pylint: disable=duplicate-code
+        crawl_configs = self.mdb["crawl_configs"]
+        crawls = self.mdb["crawls"]
+
+        async for config in crawl_configs.find({"inactive": {"$ne": True}}):
+            config_id = config["_id"]
+            try:
+                await stats_recompute_all(crawl_configs, crawls, config_id)
+            # pylint: disable=broad-exception-caught
+            except Exception as err:
+                print(f"Unable to update workflow {config_id}: {err}", flush=True)
