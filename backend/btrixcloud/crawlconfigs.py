8a90888bdde3ac05184053d9d8b288b0664146cc
Ilya Kreymer <ikreymer@gmail.com>
2025-12-16 16:15:13 -0800

    fix merge

--- backend/btrixcloud/crawlconfigs.py
+++ backend/btrixcloud/crawlconfigs.py
@@ -576,10 +576,8 @@ class CrawlConfigOps:
 
         merged_raw_config_dict = None
         if update.config:
-            merged_raw_config_dict = RawCrawlConfig(
-                **orig_crawl_config.config.dict(),
-                **update.config.dict(exclude_unset=True),
-            ).dict()
+            merged_raw_config_dict = orig_crawl_config.config.model_dump()
+            merged_raw_config_dict.update(update.config.dict(exclude_unset=True))
 
         # indicates if any k8s crawl config settings changed
         changed = False
