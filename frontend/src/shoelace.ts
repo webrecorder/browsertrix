f58a5583f95787796aa31e6d787ce3b8e604f272
sua yoo <sua@webrecorder.org>
2026-01-12 13:12:53 -0800

    feat: Display dedupe stats in collection (#3073)
    Displays dedupe stats in collection detail and org settings.
    
    ---------
    
    Co-authored-by: Ilya Kreymer <ikreymer@users.noreply.github.com>

--- frontend/src/shoelace.ts
+++ frontend/src/shoelace.ts
@@ -31,6 +31,8 @@ import "@shoelace-style/shoelace/dist/components/progress-bar/progress-bar";
 import "@shoelace-style/shoelace/dist/components/progress-ring/progress-ring";
 import "@shoelace-style/shoelace/dist/components/visually-hidden/visually-hidden";
 
+import { APP_ICON_LIBRARY } from "./types/shoelace";
+
 import(
   /* webpackChunkName: "shoelace" */ "@shoelace-style/shoelace/dist/components/breadcrumb/breadcrumb.js"
 );
@@ -84,6 +86,6 @@ import(
 );
 
 setBasePath("/shoelace");
-registerIconLibrary("app", {
+registerIconLibrary(APP_ICON_LIBRARY, {
   resolver: (name) => `/assets/icons/${name}.svg`,
 });
