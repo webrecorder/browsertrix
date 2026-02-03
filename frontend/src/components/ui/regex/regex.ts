f0047548bb777175fbb1b6a8f4873ea0c6a50405
sua yoo <sua@suayoo.com>
2026-02-03 10:20:13 -0800

    add new component

---
+++ frontend/src/components/ui/regex/regex.ts
@@ -0,0 +1,30 @@
+import { unsafeCSS } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { html as staticHtml, unsafeStatic } from "lit/static-html.js";
+import RegexColorize from "regex-colorize";
+
+import stylesheet from "./regex.stylesheet.css";
+
+import { TailwindElement } from "@/classes/TailwindElement";
+import { tw } from "@/utils/tailwind";
+
+const styles = unsafeCSS(stylesheet);
+
+/**
+ * Syntax-highlighted regular expression pattern
+ */
+@customElement("btrix-regex")
+export class Component extends TailwindElement {
+  static styles = styles;
+
+  @property({ type: String })
+  value = "";
+
+  render() {
+    if (!this.value) return;
+
+    return staticHtml`<span class="regex ${tw`font-mono`}">${unsafeStatic(
+      new RegexColorize().colorizeText(this.value) as string,
+    )}</span>`;
+  }
+}
