6cd72426307e07c5bfa0c6524e8e907e2c3ffce0
Ilya Kreymer <ikreymer@gmail.com>
2026-02-09 00:01:26 -0800

    always use fixed memory_limit

--- backend/btrixcloud/operator/crawls.py
+++ backend/btrixcloud/operator/crawls.py
@@ -451,10 +451,10 @@ class CrawlOperator(BaseOperator):
         params["obj_type"] = "crawl"
         params["cpu"] = pod_info.newCpu or params.get("redis_cpu")
         params["memory"] = pod_info.newMemory or params.get("redis_memory")
-        if self.k8s.enable_auto_resize:
-            params["memory_limit"] = int(float(params["memory"]) * MEM_LIMIT_PADDING)
-        else:
-            params["memory_limit"] = self.k8s.max_crawler_memory_size
+        # if self.k8s.enable_auto_resize:
+        #    params["memory_limit"] = int(float(params["memory"]) * MEM_LIMIT_PADDING)
+        # else:
+        params["memory_limit"] = self.k8s.max_crawler_memory_size
         params["no_pvc"] = crawl.is_single_page
 
         restart_reason = None
@@ -616,10 +616,10 @@ class CrawlOperator(BaseOperator):
         params["memory"] = pod_info.newMemory or memory
         params["workers"] = workers
         params["save_profile"] = has_profile and (i == 0)
-        if self.k8s.enable_auto_resize:
-            params["memory_limit"] = int(float(params["memory"]) * MEM_LIMIT_PADDING)
-        else:
-            params["memory_limit"] = self.k8s.max_crawler_memory_size
+        # if self.k8s.enable_auto_resize:
+        #    params["memory_limit"] = int(float(params["memory"]) * MEM_LIMIT_PADDING)
+        # else:
+        params["memory_limit"] = self.k8s.max_crawler_memory_size
         params["storage"] = pod_info.newStorage or params.get("crawler_storage")
 
         params["init_crawler"] = not is_paused
