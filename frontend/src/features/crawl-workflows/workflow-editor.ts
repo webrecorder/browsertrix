b308d6cf45868037175dcc54e1bfd40b18eea955
sua yoo <sua@suayoo.com>
2026-02-11 22:23:56 -0800

    use default

--- frontend/src/features/crawl-workflows/workflow-editor.ts
+++ frontend/src/features/crawl-workflows/workflow-editor.ts
@@ -2638,7 +2638,9 @@ https://archiveweb.page/images/${"logo.svg"}`}
       this.formState.dedupeType === DedupeType.Collection &&
       (dedupeId
         ? !isEqual(
-            this.initialWorkflow?.autoAddCollections,
+            this.initialWorkflow?.autoAddCollections.filter(
+              (id) => id !== dedupeId,
+            ),
             this.formState.autoAddCollections.filter((id) => id !== dedupeId),
           )
         : newDedupeCollectionName &&
