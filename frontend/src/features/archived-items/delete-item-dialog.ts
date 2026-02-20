9757ea292fdd01299872a4d4d8dd6aaaac6ca339
sua yoo <sua@suayoo.com>
2026-02-19 16:09:17 -0800

    update delete dialog

--- frontend/src/features/archived-items/delete-item-dialog.ts
+++ frontend/src/features/archived-items/delete-item-dialog.ts
@@ -1,11 +1,14 @@
 import { localized, msg, str } from "@lit/localize";
 import { Task } from "@lit/task";
-import { html } from "lit";
+import type { SlChangeEvent, SlCheckbox } from "@shoelace-style/shoelace";
+import { html, type PropertyValues } from "lit";
 import { customElement, property, query } from "lit/decorators.js";
 import { when } from "lit/directives/when.js";
 
 import { BtrixElement } from "@/classes/BtrixElement";
 import type { Dialog } from "@/components/ui/dialog";
+import type { ArchivedItemSectionName } from "@/pages/org/archived-item-detail/archived-item-detail";
+import { CommonTab, OrgTab, WorkflowTab } from "@/routes";
 import type { ArchivedItem } from "@/types/crawler";
 import { isCrawl, renderName } from "@/utils/crawler";
 import { pluralOf } from "@/utils/pluralize";
@@ -25,6 +28,9 @@ export class DeleteItemDialog extends BtrixElement {
   @property({ type: Boolean })
   open = false;
 
+  @property({ type: Boolean })
+  disabled = false;
+
   @query("btrix-dialog")
   readonly dialog?: Dialog | null;
 
@@ -40,6 +46,12 @@ export class DeleteItemDialog extends BtrixElement {
     },
     args: () => [this.open, this.item] as const,
   });
+
+  protected willUpdate(changedProperties: PropertyValues): void {
+    if (changedProperties.has("item") && this.item) {
+      this.disabled = Boolean(this.item.requiredByCrawls.length);
+    }
+  }
 
   render() {
     return html`<btrix-dialog
@@ -65,10 +77,7 @@ export class DeleteItemDialog extends BtrixElement {
     >`;
 
     return html`
-      <p>
-        ${msg(html`Are you sure you want to delete ${item_name}?`)}
-        ${msg("All associated files and logs will be deleted.")}
-      </p>
+      <p>${msg(html`Are you sure you want to delete ${item_name}?`)}</p>
 
       ${this.renderDependents()} ${this.renderCollections()}
 
@@ -82,16 +91,22 @@ export class DeleteItemDialog extends BtrixElement {
           }}
           >${msg("Cancel")}</sl-button
         >
-        <sl-button
-          size="small"
-          variant="danger"
-          @click=${() => {
-            this.dispatchEvent(new CustomEvent("btrix-confirm"));
-          }}
-          >${crawl
-            ? msg("Delete Crawl")
-            : msg("Delete Archived Item")}</sl-button
+        <btrix-popover
+          content=${msg("Please confirm impact on dependents to continue.")}
+          ?disabled=${!this.disabled}
+          hoist
         >
+          <sl-button
+            size="small"
+            variant="danger"
+            ?disabled=${this.disabled}
+            @click=${() => {
+              this.dispatchEvent(new CustomEvent("btrix-confirm"));
+            }}
+          >
+            ${crawl ? msg("Delete Crawl") : msg("Delete Archived Item")}
+          </sl-button>
+        </btrix-popover>
       </div>
     `;
   }
@@ -106,29 +121,35 @@ export class DeleteItemDialog extends BtrixElement {
     const plural_of_collections = pluralOf("collections", count);
 
     return html`
-      <p class="my-2">
-        ${msg(
-          str`The archived item will be removed from ${number_of_collections} ${plural_of_collections}:`,
-        )}
-      </p>
-      ${this.collectionsTask.render({
-        pending: () =>
-          html`<btrix-linked-collections-list
-            .collections=${collectionIds.map((id) => ({ id }))}
-            baseUrl="${this.navigate.orgBasePath}/collections/view"
-          >
-          </btrix-linked-collections-list>`,
-        complete: (res) =>
-          when(
-            res,
-            (collections) =>
-              html`<btrix-linked-collections-list
-                .collections=${collections}
-                baseUrl="${this.navigate.orgBasePath}/collections/view"
-              >
-              </btrix-linked-collections-list>`,
-          ),
-      })}
+      <btrix-details class="mt-2" ?open=${!this.item.requiredByCrawls.length}>
+        <span slot="title">${msg("Impact on Collections")}</span>
+
+        <p class="my-2">
+          ${msg(
+            str`This item will be removed from ${number_of_collections} ${plural_of_collections}:`,
+          )}
+        </p>
+        ${this.collectionsTask.render({
+          pending: () =>
+            html`<btrix-linked-collections-list
+              .collections=${collectionIds.map((id) => ({ id }))}
+              baseUrl="${this.navigate
+                .orgBasePath}/${OrgTab.Collections}/${CommonTab.View}"
+            >
+            </btrix-linked-collections-list>`,
+          complete: (res) =>
+            when(
+              res,
+              (collections) =>
+                html`<btrix-linked-collections-list
+                  .collections=${collections}
+                  baseUrl="${this.navigate
+                    .orgBasePath}/${OrgTab.Collections}/${CommonTab.View}"
+                >
+                </btrix-linked-collections-list>`,
+            ),
+        })}
+      </btrix-details>
     `;
   }
 
@@ -138,19 +159,55 @@ export class DeleteItemDialog extends BtrixElement {
     const { requiredByCrawls } = this.item;
     const count = requiredByCrawls.length;
 
-    const number_of_items = this.localize.number(count);
-    const plural_of_items = pluralOf("items", count);
+    const number_of_dependents = this.localize.number(count);
+    const plural_of_dependents = pluralOf("dependents", count);
+
+    const itemPath = `${this.navigate.orgBasePath}/${OrgTab.Workflows}/${this.item.cid}/${WorkflowTab.Crawls}/${this.item.id}`;
 
     return html`
-      <p class="my-2">
-        ${msg(
-          str`${number_of_items} ${plural_of_items} depend on this
-                    item.`,
-        )}
-        ${msg(
-          "Deleting this item will result in incomplete replay and downloads.",
-        )}
-      </p>
+      <btrix-details class="mt-2" open>
+        <span slot="title">${msg("Impact on Dependents")}</span>
+
+        <p class="my-2">
+          ${msg(
+            str`This item has ${number_of_dependents} ${plural_of_dependents}.`,
+          )}
+        </p>
+        <p class="my-2 font-medium text-danger">
+          ${msg(
+            "Deleting this item may result in missing or incomplete content.",
+          )}
+        </p>
+        <p class="my-2">
+          ${msg(
+            "To prevent permanent data loss, downloading the item first is highly recommended.",
+          )}
+          <btrix-link
+            href="${this.navigate.orgBasePath}/${OrgTab.Workflows}/${this.item
+              .cid}/${WorkflowTab.Crawls}/${this.item
+              .id}#${"files" satisfies ArchivedItemSectionName}"
+            @click=${() => {
+              if (new URL(window.location.href).pathname === itemPath) {
+                void this.dialog?.hide();
+                this.dispatchEvent(new CustomEvent("btrix-cancel"));
+              }
+            }}
+            >${msg("Go to downloads")}</btrix-link
+          >
+        </p>
+      </btrix-details>
+
+      <sl-checkbox
+        @sl-change=${(e: SlChangeEvent) => {
+          const { checked } = e.target as SlCheckbox;
+
+          this.disabled = !checked;
+        }}
+        ?checked=${!this.disabled}
+        >${msg(
+          "I understand the impact of item deletion on dependents.",
+        )}</sl-checkbox
+      >
     `;
   }
 
