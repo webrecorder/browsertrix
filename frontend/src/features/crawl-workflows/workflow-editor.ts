73ebad20c6aaf798c2ce928f8276236f1716d1f4
sua yoo <sua@suayoo.com>
2026-05-05 15:06:09 -0700

    update user guide

--- frontend/src/features/crawl-workflows/workflow-editor.ts
+++ frontend/src/features/crawl-workflows/workflow-editor.ts
@@ -2068,9 +2068,9 @@ https://archiveweb.page/images/${"logo.svg"}`}
       ${this.renderHelpTextCol(html`
         ${infoTextFor["browserProfile"]}
         ${this.renderUserGuideLink({
-          hash: "browser-profile-best-practices",
-          content: msg("Read best practices"),
-        })}.
+          hash: "browser-profile",
+          content: msg("More details"),
+        })}
       `)}
       ${when(
         this.formState.browserProfile,
@@ -2096,13 +2096,13 @@ https://archiveweb.page/images/${"logo.svg"}`}
       )}
       ${inputCol(html`
         <sl-checkbox name="saveStorage" ?checked=${this.formState.saveStorage}>
-          ${msg("Include browser storage data")}
+          ${labelFor["saveStorage"]}
         </sl-checkbox>
       `)}
       ${this.renderHelpTextCol(
         html`${infoTextFor["saveStorage"]}
         ${this.renderUserGuideLink({
-          hash: "save-local-and-session-storage",
+          hash: "include-browser-storage-data",
           content: msg("More details"),
         })}.`,
         false,
