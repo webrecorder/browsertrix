6fb43dfbf54c010aff28d50958e20d24a7286eee
sua yoo <sua@suayoo.com>
2026-05-12 15:29:50 -0700

    copy additional fields

--- frontend/src/pages/org/workflow-detail.ts
+++ frontend/src/pages/org/workflow-detail.ts
@@ -14,7 +14,13 @@ import isNil from "lodash/isNil";
 import queryString from "query-string";
 
 import type { ArchivedItemSectionName } from "./archived-item-detail/archived-item-detail";
-import type { Crawl, CrawlLog, Seed, Workflow } from "./types";
+import {
+  workflowParamsSchema,
+  type Crawl,
+  type CrawlLog,
+  type Seed,
+  type Workflow,
+} from "./types";
 
 import { BtrixElement } from "@/classes/BtrixElement";
 import type { Alert } from "@/components/ui/alert";
@@ -800,14 +806,7 @@ export class WorkflowDetail extends BtrixElement {
               value=${ifDefined(
                 this.workflow &&
                   this.seeds &&
-                  JSON.stringify(
-                    {
-                      ...omitNil(this.workflow.config),
-                      seeds: this.seeds.items.map(omitNil),
-                    },
-                    null,
-                    2,
-                  ),
+                  JSON.stringify(this.settingsToJson(), null, 2),
               )}
               content=${msg("Copy as JSON")}
               size="medium"
@@ -2159,6 +2158,42 @@ export class WorkflowDetail extends BtrixElement {
       errors: errors.total,
       behaviors: behaviors.total,
     };
+  }
+
+  /**
+   * Copy workflow settings to clipboard in JSON format
+   * compatible with POST/PATCH API
+   */
+  private settingsToJson() {
+    if (!this.workflow || !this.seeds) return;
+
+    const params = {
+      name: this.workflow.name,
+      schedule: this.workflow.schedule,
+      browserWindows: this.workflow.browserWindows,
+      profileid: this.workflow.profileid,
+      tags: this.workflow.tags,
+      crawlTimeout: this.workflow.crawlTimeout,
+      maxCrawlSize: this.workflow.maxCrawlSize,
+      description: this.workflow.description,
+      autoAddCollections: this.workflow.autoAddCollections,
+      crawlerChannel: this.workflow.crawlerChannel,
+      proxyId: this.workflow.proxyId,
+      config: this.workflow.config,
+      seeds: this.seeds.items.map(omitNil),
+    };
+
+    const { error } = workflowParamsSchema.safeParse(params);
+
+    if (error) {
+      console.debug(error);
+    }
+
+    // Omit null and undefined values
+    return omitNil({
+      ...params,
+      config: omitNil(params.config),
+    });
   }
 
   /**
