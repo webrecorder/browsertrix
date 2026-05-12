c88f107a3b8fef7ca2d91a487e51ef9dc0e30221
Misty De Meo <mistydemeo@gmail.com>
2026-05-12 10:01:59 -0700

    backend: swap to a dependency lockfile
    I chose to go with uv's lockfile format for convenience since that lets
    us track the dependency tree a little more easily.

--- update-version.sh
+++ update-version.sh
@@ -10,6 +10,10 @@ echo "__version__ = \"$version\"" >> ./backend/btrixcloud/version.py
 
 sed -E -i "" "s/^version:.*$/version: v$version/" chart/Chart.yaml
 
+sed -E -i "" "s/^version =.*$/version = \"$version\"/" backend/pyproject.toml
+# updates the version in the lockfile
+uv --directory backend lock
+
 sed -E -i "" "s/\/browsertrix-backend:[[:alnum:].-]+/\/browsertrix-backend:$version/" chart/values.yaml
 sed -E -i "" "s/\/browsertrix-frontend:[[:alnum:].-]+/\/browsertrix-frontend:$version/" chart/values.yaml
 sed -E -i "" "s/\/browsertrix-emails:[[:alnum:].-]+/\/browsertrix-emails:$version/" chart/values.yaml
