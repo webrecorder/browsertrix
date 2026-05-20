f7a22acfac92ed3a43b3b8356a2b71b38c060b6e
Emma Segal-Grossman <hi@emma.cafe>
2026-05-20 17:18:06 -0400

    Fix safari hang by refactoring crawl list item name (#3322)

--- frontend/src/features/crawls/crawl-list/crawl-list-item.ts
+++ frontend/src/features/crawls/crawl-list/crawl-list-item.ts
@@ -1,7 +1,6 @@
 import { localized, msg } from "@lit/localize";
 import { css, html, nothing, type TemplateResult } from "lit";
 import { customElement, property, query } from "lit/decorators.js";
-import { ifDefined } from "lit/directives/if-defined.js";
 
 import { BtrixElement } from "@/classes/BtrixElement";
 import type { OverflowDropdown } from "@/components/ui/overflow-dropdown";
@@ -48,52 +47,21 @@ export class CrawlListItem extends BtrixElement {
 
   render() {
     if (!this.crawl) return;
-    let idCell: TemplateResult;
 
-    if (this.workflowId) {
-      const label = html`
-        <div>
-          ${this.safeRender(
-            (crawl) => html`
-              <btrix-format-date
-                date=${crawl.started}
-                month="2-digit"
-                day="2-digit"
-                year="numeric"
-                hour="2-digit"
-                minute="2-digit"
-              ></btrix-format-date>
-            `,
-          )}
-        </div>
-      `;
-      idCell = html`
-        <btrix-table-cell
-          rowClickTarget=${ifDefined(this.href ? "a" : undefined)}
-        >
-          ${this.href
-            ? html`<a href=${this.href} @click=${this.navigate.link}>
-                ${label}
-              </a>`
-            : html`<div>${label}</div> `}
-        </btrix-table-cell>
-      `;
-    } else {
-      const label = html`
-        <btrix-table-cell class="clickLabel pl-0" role="generic">
-          ${this.safeRender((workflow) => renderName(workflow))}
-        </btrix-table-cell>
-      `;
-      idCell = html`
-        <btrix-table-cell rowClickTarget="a">
-          ${this.href
-            ? html`<a href=${this.href} @click=${this.navigate.link}>
-                ${label}
-              </a>`
-            : html`<div>${label}</div> `}
-        </btrix-table-cell>
-      `;
-    }
+    const label = this.workflowId
+      ? this.safeRender(
+          (crawl) => html`
+            <btrix-format-date
+              date=${crawl.started}
+              month="2-digit"
+              day="2-digit"
+              year="numeric"
+              hour="2-digit"
+              minute="2-digit"
+            ></btrix-format-date>
+          `,
+        )
+      : this.safeRender((workflow) => renderName(workflow));
 
     const skipped = isSkipped(this.crawl);
     const hasExec = Boolean(this.crawl.crawlExecSeconds);
@@ -126,7 +94,13 @@ export class CrawlListItem extends BtrixElement {
             `,
           )}
         </btrix-table-cell>
-        ${idCell}
+        <btrix-table-cell rowClickTarget=${this.href ? "a" : nothing}>
+          ${this.href
+            ? html`<a href=${this.href} @click=${this.navigate.link}>
+                ${label}
+              </a>`
+            : label}
+        </btrix-table-cell>
         ${this.workflowId
           ? nothing
           : html`
