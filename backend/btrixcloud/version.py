ac9410a5ad1b16f23e9761c96317e01476f1e729
emma <hi@emma.cafe>
2026-06-05 02:46:30 -0400

    add version/commit/branch to all logs

--- backend/btrixcloud/version.py
+++ backend/btrixcloud/version.py
@@ -1,3 +1,7 @@
-"""current version"""
+"""Build version info from environment, set at Docker build time."""
+
+import os
 
 __version__ = "1.23.0-beta.0"
+__commit_hash__ = os.environ["GIT_COMMIT_HASH"]
+__branch__ = os.environ["GIT_BRANCH_NAME"]
