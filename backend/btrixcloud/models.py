c365b604b5564ff446c1744a140b34ad63c98ce9
emma <hi@emma.cafe>
2026-06-24 12:45:38 -0400

    lint/ruff check

--- backend/btrixcloud/models.py
+++ backend/btrixcloud/models.py
@@ -3345,18 +3345,16 @@ class PostProcessUploadJob(BackgroundJob):
 # Union of all job types, for response model
 
 AnyJob = RootModel[
-    Union[
-        CreateReplicaJob,
-        DeleteReplicaJob,
-        BackgroundJob,
-        DeleteOrgJob,
-        RecalculateOrgStatsJob,
-        ReAddOrgPagesJob,
-        OptimizePagesJob,
-        CleanupSeedFilesJob,
-        UpdateCollStatsJob,
-        PostProcessUploadJob,
-    ]
+    CreateReplicaJob
+    | DeleteReplicaJob
+    | BackgroundJob
+    | DeleteOrgJob
+    | RecalculateOrgStatsJob
+    | ReAddOrgPagesJob
+    | OptimizePagesJob
+    | CleanupSeedFilesJob
+    | UpdateCollStatsJob
+    | PostProcessUploadJob
 ]
 
 
