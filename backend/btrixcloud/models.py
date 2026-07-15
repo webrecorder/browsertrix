aff0c21afd589be99da78ec8811f82e0c4c36cb1
emma <hi@emma.cafe>
2026-07-15 16:22:56 -0400

    mark uploads as deleted in database and abort post-processing when deleting
    when an upload is deleted during post-processing, it gets marked as
    deleted in the db and aborts further processing
    also removes unnecessary `upsert=True` on status updates where the
    document should already exist

--- backend/btrixcloud/models.py
+++ backend/btrixcloud/models.py
@@ -1217,6 +1217,7 @@ class UploadedCrawl(BaseCrawl):
 
     type: Literal["upload"] = "upload"
     image: None = None
+    deleted: bool = False
 
 
 # ============================================================================
