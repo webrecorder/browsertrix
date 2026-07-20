75b71d250c3210f7d130cb459dae2aa8a0db9baf
sua yoo <sua@suayoo.com>
2026-07-20 12:30:53 -0700

    add component from details

--- frontend/src/components/ui/url-list.ts
+++ frontend/src/components/ui/url-list.ts
@@ -1,27 +1,67 @@
-import { localized } from "@lit/localize";
-import { html } from "lit";
+import { localized, msg } from "@lit/localize";
+import { css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 import { TailwindElement } from "@/classes/TailwindElement";
+import type { Seed } from "@/types/crawler";
 
 @customElement("btrix-url-list")
 @localized()
 export class UrlList extends TailwindElement {
+  static styles = css`
+    btrix-table {
+      --btrix-table-grid-template-columns: 1fr min-content min-content;
+      --btrix-table-column-gap: var(--sl-spacing-x-small);
+    }
+
+    btrix-table-cell,
+    btrix-code {
+      overflow: hidden;
+    }
+
+    btrix-table-cell {
+      white-space: nowrap;
+    }
+  `;
+
   @property({ type: Array })
-  urls?: string[] = [];
+  urls?: (string | Seed)[] = [];
 
   render() {
     if (!this.urls?.length) return;
 
-    return html`<btrix-table>
-      <btrix-table-body>
-        ${this.urls.map(
-          (url) =>
-            html`<btrix-table-row>
-              <btrix-table-cell>${url}</btrix-table-cell>
-            </btrix-table-row>`,
-        )}
-      </btrix-table-body>
+    return html`<btrix-table class="grid-cols-[1fr_auto]">
+      ${this.urls.map((seedOrUrl: string | Seed) => {
+        const url = typeof seedOrUrl === "string" ? seedOrUrl : seedOrUrl.url;
+
+        return html`
+          <btrix-table-row
+            class="rounded transition-colors duration-x-fast even:bg-neutral-50 even:[--btrix-overflow-scroll-scrim-color:--sl-color-neutral-50] has-[btrix-copy-button:hover]:!bg-cyan-50/80 has-[sl-icon-button:hover]:!bg-cyan-50/80"
+          >
+            <btrix-table-cell class="pl-2">
+              <btrix-overflow-scroll
+                class="-ml-5 w-[calc(100%+theme(spacing.5))] contain-inline-size part-[content]:px-5 part-[content]:[scrollbar-width:thin]"
+              >
+                <btrix-code
+                  language="url"
+                  class="block w-max whitespace-nowrap"
+                  .value=${url}
+                ></btrix-code>
+              </btrix-overflow-scroll>
+            </btrix-table-cell>
+            <btrix-table-cell>
+              <btrix-copy-button .value=${url}></btrix-copy-button>
+              <sl-icon-button
+                name="arrow-up-right"
+                href="${url}"
+                target="_blank"
+                label=${msg("Open in New Tab")}
+              >
+              </sl-icon-button>
+            </btrix-table-cell>
+          </btrix-table-row>
+        `;
+      })}
     </btrix-table>`;
   }
 }
--- frontend/src/stories/components/UrlList.stories.ts
+++ frontend/src/stories/components/UrlList.stories.ts
@@ -5,9 +5,14 @@ import { renderComponent, type RenderProps } from "./UrlList";
 
 // Fixed seed for reproducibility
 faker.seed(0);
+
 const data = Array.from({ length: 95 }).map(
   () =>
-    `${faker.internet.url({ appendSlash: true })}${faker.word.words({ count: { min: 0, max: 20 } }).replace(/\s/g, "/")}`,
+    `${faker.internet.url({ appendSlash: true })}${
+      // FIXME Replace workaround with `urlPath`
+      // https://github.com/faker-js/faker/issues/3790
+      faker.word.words({ count: { min: 0, max: 15 } }).replace(/\s/g, "/")
+    }`,
 );
 
 const meta = {
@@ -30,8 +35,28 @@ export const Empty: Story = {
   args: {},
 };
 
-export const WithData: Story = {
+export const WithUrls: Story = {
   args: {
-    urls: data.slice(0, 20),
+    urls: [
+      "https://webrecorder.net/",
+      "https://webrecorder.net/resources/",
+      "https://webrecorder.net/resources/what-is-web-archiving/",
+      "https://webrecorder.net/resources/glossary/",
+      "https://forum.webrecorder.net/",
+      "https://github.com/webrecorder/",
+      "https://specs.webrecorder.net/wacz/1.1.1/",
+      "https://specs.webrecorder.net/wacz-auth/0.1.0/",
+      "https://specs.webrecorder.net/cdxj/0.1.0/",
+      "https://specs.webrecorder.net/use-cases/0.1.0/",
+      "https://specs.webrecorder.net/wacz-ipfs/latest/",
+      "https://docs.google.com/presentation/d/1UwXDOcRA8zg5CExXru9o_Ml6oQZxThkA_neQ8PrlpOI/edit?usp=sharing",
+      "https://docs.google.com/presentation/d/12jLMPYpLR3s7Ucq2Hf_n4qOzjzv7pZrOgEN69VPq0jY/edit?usp=sharing",
+    ],
+  },
+};
+
+export const WithManyUrls: Story = {
+  args: {
+    urls: data,
   },
 };
