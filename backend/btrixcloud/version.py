0b72ecc47225d15fc777d935d92f893b77c2ac1e
Tessa Walsh <tessa@bitarchivist.net>
2026-07-21 16:40:29 -0400

    Use environ.get for version env vars to be more permissive

--- backend/btrixcloud/version.py
+++ backend/btrixcloud/version.py
@@ -3,5 +3,5 @@
 import os
 
 __version__ = "1.24.0-beta.2"
-__commit_hash__ = os.environ["GIT_COMMIT_HASH"]
-__branch__ = os.environ["GIT_BRANCH_NAME"]
+__commit_hash__ = os.environ.get("GIT_COMMIT_HASH")
+__branch__ = os.environ.get("GIT_BRANCH_NAME")
