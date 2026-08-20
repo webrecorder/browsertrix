7484235945334a16d790a3f9cd50cf68476bca21
Tessa Walsh <tessa@bitarchivist.net>
2026-08-20 16:08:33 -0400

    Improve logging of edge cases
    Remove if not job condition, as the getter will already raise if
    the job can't be found.

--- backend/btrixcloud/background_jobs.py
+++ backend/btrixcloud/background_jobs.py
@@ -680,7 +680,15 @@ class BackgroundJobOps:
             await self.crawl_manager.delete_all_k8s_resources_for_org(str(oid))
 
         job = await self.get_background_job(job_id)
-        if not job or job.finished:
+        if job.finished:
+            logger.debug(
+                "job_already_updated",
+                job_id=job.id,
+                oid=oid,
+                job_type=job_type,
+                success=job.success,
+                finished=job.finished,
+            )
             return
 
         if job.type != job_type:
@@ -746,6 +754,7 @@ class BackgroundJobOps:
 
         res = await self.jobs.find_one(query)
         if not res:
+            logger.error("job_not_found", job_id=job_id, oid=oid)
             raise HTTPException(status_code=404, detail="job_not_found")
 
         return self._get_job_by_type_from_data(res)
