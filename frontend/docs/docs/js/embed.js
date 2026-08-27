3fcd9390109848c4a8a8cd1d6d26f26631c8336a
sua yoo <sua@webrecorder.org>
2026-08-26 21:43:04 -0700

    docs: Update theme & improve embed (#3582)
    - Updates theme to closer match Browsertrix application and marketing
    site
    - Adds dark theme
    - Adds sign up/sign in buttons to header
    - When embedded, prevents navigation back to irrelevant docs (like
    self-hosting)
    
    <sub>Stack created with <a
    href="https://github.com/github/gh-stack">GitHub Stacks CLI</a> • <a
    href="https://gh.io/stacks-feedback">Give Feedback 💬</a></sub>

--- frontend/docs/docs/js/embed.js
+++ frontend/docs/docs/js/embed.js
@@ -1,9 +1,37 @@
 if (window.self !== window.top) {
   // Within iframe--assume this is an iframe embedded in the Browsertrix app.
-  const style = document.createElement("style");
+  window.document.documentElement.classList.add("btrix-app-embed");
 
-  // Decrease text size without decreasing element size and overall spacing
-  style.innerText = `.md-typeset { font-size: 0.7rem; }`;
+  replaceDocsTitle();
+}
 
-  window.document.body.appendChild(style);
+/**
+ * Replace the "Browsertrix Docs" title with the title of the current page,
+ * since the embed is already labeled by the Browsertrix panel as "User Guide".
+ */
+function replaceDocsTitle() {
+  const docsTitle = document.querySelector(
+    ".md-header__title .md-header__topic:first-child .md-ellipsis",
+  );
+  const pageTitle = document.querySelector(
+    ".md-header__title .md-header__topic:last-child .md-ellipsis",
+  );
+
+  if (!docsTitle) {
+    console.debug(
+      "no docsTitle `.md-header__title md-header__topic:first-child .md-ellipsis`",
+    );
+    return;
+  }
+
+  if (!pageTitle) {
+    console.debug(
+      "no pageTitle `.md-header__title md-header__topic:last-child .md-ellipsis`",
+    );
+    return;
+  }
+
+  if (pageTitle.textContent) {
+    docsTitle.textContent = pageTitle.textContent.trim();
+  }
 }
