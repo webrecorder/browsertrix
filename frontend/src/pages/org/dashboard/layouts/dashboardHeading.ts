6bef1891bbd6ab3c73fa779551bafca7ff5af6c9
sua yoo <sua@suayoo.com>
2026-08-26 20:36:25 -0700

    switch to container queries

---
+++ frontend/src/pages/org/dashboard/layouts/dashboardHeading.ts
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+import { html } from "lit";
+
+import { tw } from "@/utils/tailwind";
+
+export function dashboardHeading(
+  content: string,
+  { aside } = { aside: false },
+) {
+  return html`<header class="mb-2">
+    <h2
+      class=${clsx(
+        tw`text-base font-medium leading-6`,
+        aside && tw`@5xl:text-sm @5xl:leading-5`,
+      )}
+    >
+      ${content}
+    </h2>
+  </header>`;
+}
