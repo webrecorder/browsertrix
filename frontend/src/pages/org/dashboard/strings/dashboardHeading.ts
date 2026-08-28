b9f7dd12d9c6f2eabb0d3a7314de712df0753a49
sua yoo <sua@suayoo.com>
2026-08-27 17:47:37 -0700

    share titles

---
+++ frontend/src/pages/org/dashboard/strings/dashboardHeading.ts
@@ -0,0 +1,7 @@
+import { msg } from "@lit/localize";
+
+export const dashboardHeadingFor = {
+  crawlingGuides: msg("Crawling Guides"),
+  newUserCrawlingGuides: msg("What would you like to archive?"),
+  exploreMoreGuides: msg("Explore More Guides"),
+};
