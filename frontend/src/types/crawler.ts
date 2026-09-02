e63ee10dc8eeeacfac68f09c02114d5f6785ef6b
sua yoo <sua@suayoo.com>
2026-09-02 12:07:03 -0700

    add crawl mock

--- frontend/src/types/crawler.ts
+++ frontend/src/types/crawler.ts
@@ -187,7 +187,12 @@ type ArchivedItemBase = {
   fileSize?: number;
   collectionIds: string[];
   collections: { id: string; name: string }[];
-  stats: { done: string; found: string; size: string } | null;
+  // TODO Check if this is still ever a string
+  stats: {
+    done: string | number;
+    found: string | number;
+    size: string | number;
+  } | null;
   firstSeed: string | null;
   seedCount: number | null;
   tags: string[];
@@ -268,6 +273,7 @@ export type CrawlerChannelsAPIResponse = {
 export type ArchivedItem = Crawl | Upload;
 
 export type ListArchivedItem = Omit<ArchivedItem, "collections">;
+export type ListCrawl = Omit<Crawl, "collections">;
 
 export type ArchivedItemPageComment = {
   id: string;
