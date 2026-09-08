7fcab1bc9f8c06cf30a947f272885b3d3be846d7
sua yoo <sua@suayoo.com>
2026-09-08 09:52:06 -0700

    migrate service

--- frontend/src/utils/PasswordService.ts
+++ frontend/src/utils/PasswordService.ts
@@ -1,21 +1,37 @@
-import { zxcvbn, zxcvbnOptions, type OptionsType } from "@zxcvbn-ts/core";
+import {
+  ZxcvbnFactory,
+  type OptionsType,
+  type ZxcvbnResult,
+} from "@zxcvbn-ts/core";
+import * as zxcvbnCommonPackage from "@zxcvbn-ts/language-common";
+import { mergeDeep } from "immutable";
 
-const loadOptions = async (): Promise<OptionsType> => {
-  const zxcvbnCommonPackage = await import(
-    /* webpackChunkName: "zxcvbnCommonPackage" */ "@zxcvbn-ts/language-common"
-  );
+enum PasswordServiceLanguage {
+  English = "en",
+}
+
+const DEFAULT_LANGUAGE = PasswordServiceLanguage.English;
+const DEFAULT_OPTIONS = {
+  dictionary: {
+    ...zxcvbnCommonPackage.dictionary,
+  },
+  graphs: zxcvbnCommonPackage.adjacencyGraphs,
+} as const;
+
+const loadOptions = async (
+  lang: PasswordServiceLanguage,
+): Promise<OptionsType> => {
   const zxcvbnEnPackage = await import(
-    /* webpackChunkName: "zxcvbnEnPackage" */ "@zxcvbn-ts/language-en"
+    /* webpackChunkName: "zxcvbnEnPackage" */ `@zxcvbn-ts/language-${lang}`
   );
 
-  return {
+  return mergeDeep(DEFAULT_OPTIONS, {
+    ...DEFAULT_OPTIONS,
     dictionary: {
-      ...zxcvbnCommonPackage.dictionary,
       ...zxcvbnEnPackage.dictionary,
     },
-    graphs: zxcvbnCommonPackage.adjacencyGraphs,
     translations: zxcvbnEnPackage.translations,
-  };
+  });
 };
 
 /**
@@ -26,25 +42,14 @@ export default class PasswordService {
   static readonly PASSWORD_MAXLENGTH = 64 as const;
   static readonly PASSWORD_MIN_SCORE = 3 as const;
 
-  static options?: OptionsType;
+  static options?: Promise<OptionsType> = Promise.resolve(DEFAULT_OPTIONS);
+  static lang?: PasswordServiceLanguage;
+  static zxcvbn = new ZxcvbnFactory(DEFAULT_OPTIONS);
 
-  /**
-   * Update zxcvbn options asynchronously
-   * @TODO Localize by loading different translations
-   * @param opts See https://zxcvbn-ts.github.io/zxcvbn/guide/options/
-   */
-  static async setOptions(opts?: OptionsType) {
-    if (!PasswordService.options) {
-      PasswordService.options = await loadOptions();
-    }
-    if (opts) {
-      zxcvbnOptions.setOptions({
-        ...PasswordService.options,
-        ...opts,
-      });
-    } else {
-      zxcvbnOptions.setOptions(PasswordService.options);
-    }
+  static async setLanguage(lang: PasswordServiceLanguage) {
+    this.lang = lang;
+    this.options = loadOptions(lang);
+    this.zxcvbn = new ZxcvbnFactory(await this.options);
   }
 
   /**
@@ -55,8 +60,13 @@ export default class PasswordService {
   static async checkStrength(
     password: string,
     // User input to check, e.g. emails
-    userInputs?: string[],
-  ) {
-    return zxcvbn(password, userInputs);
+    userInputs?: (string | number)[],
+  ): Promise<ZxcvbnResult> {
+    if (!this.lang) {
+      await this.setLanguage(DEFAULT_LANGUAGE);
+    } else {
+      await this.options;
+    }
+    return this.zxcvbn.check(password, userInputs);
   }
 }
