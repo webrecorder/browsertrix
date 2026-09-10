d2dca56df96715c8514f82d449440dddcbcd0693
sua yoo <sua@suayoo.com>
2026-09-10 15:00:46 -0700

    track finish

--- frontend/src/trackEvents.ts
+++ frontend/src/trackEvents.ts
@@ -19,11 +19,14 @@ export enum AnalyticsTrackEvent {
   /**
    * Onboarding
    */
-  CompleteOnboardingStep = "Complete Onboarding Step",
+  CompleteOnboardingStep = "Completed onboarding step",
+  FinishSetUp = "Finished set up",
+  UndoFinishSetUp = "Undo finished set up",
   /**
    * User Guide
    */
   OpenedCrawlingOnePageGuide = "Opened crawling one page guide",
   OpenedCrawlingSocialMediaGuide = "Opened crawling social media guide",
   OpenedCrawlingWebsiteGuide = "Opened crawling website guide",
+  ClickSearchGuides = "Clicked search guides",
 }
